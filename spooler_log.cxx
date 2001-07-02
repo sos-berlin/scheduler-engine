0e346fae957cbb9589e14a1330ddd61c1c57de1a
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2001-07-02 11:13:43 +0000

    Factory: Fehler beim Include einer Aufzählung behoben
    git-svn-id: http://subversion.sos/svn/sos@9360 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_log.cxx
+++ spooler_log.cxx
@@ -1,4 +1,4 @@
-// $Id: spooler_log.cxx,v 1.12 2001/02/21 10:57:36 jz Exp $
+/ $Id: spooler_log.cxx,v 1.13 2001/07/02 21:38:26 jz Exp $
 
 #include "../kram/sos.h"
 #include "spooler.h"
@@ -124,6 +124,125 @@ void Prefix_log::log( Log_kind kind, const string& line )
     _log->log( kind, _prefix, line );
 }
 
+//----------------------------------------------------------------------------------Stdout_collector
+/*
+struct Stdout_collector
+{
+    void                        close                       ();
+  //void                        collect_stdout              ();
+    void                        collect_stderr              ();
+  //virtual void                write_stdout                ( const Const_area& data );
+    virtual void                write_stderr                ( const Const_area& data );
+
+    HANDLE                     _original_stderr;
+    HANDLE                     _stdout_write;
+    HANDLE                     _stdout_read;
+};
+
+//------------------------------------------------------------------Stdout_collector::collect_stderr
+
+void Stdout_collector::collect_stderr()
+{
+    BOOL    ok; 
+
+    _original_stderr = GetStdHandle( STD_ERROR_HANDLE ); 
+
+    HANDLE  stdin_read     = (HANDLE)0;
+    HANDLE  stdin_write    = (HANDLE)0;
+    HANDLE  stdout_read    = (HANDLE)0;
+    HANDLE  stdout_write   = (HANDLE)0;
+    DWORD   thread_id;
+
+    try
+    {
+        {
+            SECURITY_ATTRIBUTES security_attributes; 
+
+            security_attributes.nLength              = sizeof security_attributes; 
+            security_attributes.bInheritHandle       = TRUE;    // pipe handles are inherited. 
+            security_attributes.lpSecurityDescriptor = NULL; 
+ 
+            // The steps for redirecting child process's STDIN: 
+            //     1.  Save current STDIN, to be restored later. 
+            //     2.  Create anonymous pipe to be STDIN for child process. 
+            //     3.  Set STDIN of the parent to be the read handle to the 
+            //         pipe, so it is inherited by the child process. 
+            //     4.  Create a noninheritable duplicate of the write handle, 
+            //         and close the inheritable write handle. 
+ 
+            ok = CreatePipe( &stdout_read, &stdout_write, &security_attributes, 0 );        if(!ok) throw_mswin_error("CreatePipe");
+
+            ok = SetStdHandle( STD_INPUT_HANDLE, stdin_read );                              if(!ok) throw_mswin_error("SetStdHandle");
+            ok = SetStdHandle( STD_OUTPUT_HANDLE, stdout_write );                           if(!ok) throw_mswin_error("SetStdHandle"); 
+
+            ok = DuplicateHandle( GetCurrentProcess(), stdin_write, 
+                                  GetCurrentProcess(), &_stdin_write, 0, 
+                                  FALSE,   // not inherited 
+                                  DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS );         if(!ok) throw_mswin_error("DuplicateHandle");
+
+            ok = DuplicateHandle( GetCurrentProcess(), stdout_read,
+                                  GetCurrentProcess(), &_stdout_read, 0,
+                                  FALSE,   // not inherited 
+                                  DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS );         if(!ok) throw_mswin_error("DuplicateHandle");
+        }
+
+        // Now create the child process. 
+        {
+            PROCESS_INFORMATION process_info; 
+            STARTUPINFO         startup_info; 
+ 
+            memset( &process_info, 0, sizeof process_info );
+ 
+            memset( &startup_info, 0, sizeof startup_info );
+            startup_info.cb = sizeof startup_info; 
+ 
+            ok = CreateProcess( _program_path.c_str(),       // application name
+                                (char*)_command_line.c_str(),       // command line 
+                                NULL,          // process security attributes 
+                                NULL,          // primary thread security attributes 
+                                TRUE,          // handles are inherited 
+                                0,             // creation flags 
+                                NULL,          // use parent's environment 
+                                NULL,          // use parent's current directory 
+                                &startup_info, // STARTUPINFO pointer 
+                                &process_info ); // receives PROCESS_INFORMATION 
+
+            if( !ok )  throw_mswin_error("CreateProcess");
+
+            CloseHandle( process_info.hThread );
+            _pid = process_info.hProcess;
+        } 
+     
+        // After process creation, restore the saved STDIN and STDOUT. 
+ 
+        ok = SetStdHandle( STD_INPUT_HANDLE , parents_stdin  );                             if(!ok) throw_mswin_error("SetStdHandle");
+        ok = SetStdHandle( STD_OUTPUT_HANDLE, parents_stdout );                             if(!ok) throw_mswin_error("SetStdHandle");
+
+        CloseHandle( stdin_read );
+        CloseHandle( stdout_write );    
+
+        // Vorbereiten der Schreib- und Lesefäden:
+
+        _write_event        = CreateEvent( NULL, FALSE, FALSE, NULL );                      if(!_write_event       ) throw_mswin_error("CreateEvent");
+        _data_written_event = CreateEvent( NULL, FALSE, FALSE, NULL );                      if(!_data_written_event) throw_mswin_error("CreateEvent");
+        _data_read_event    = CreateEvent( NULL, FALSE, FALSE, NULL );                      if(!_data_read_event   ) throw_mswin_error("CreateEvent");
+
+        _write_thread_handle = CreateThread( NULL, 0, write_thread_function, this, 0, &thread_id );  
+        if(!_write_thread_handle) throw_mswin_error("CreateThread");
+
+        _read_thread_handle  = CreateThread( NULL, 0, read_thread_function , this, 0, &thread_id );  
+        if(!_read_thread_handle) throw_mswin_error("CreateThread");
+    }
+    catch( const Xc& )
+    {
+        CloseHandle( stdin_read  ); 
+        CloseHandle( stdin_write ); 
+        CloseHandle( stdout_read  ); 
+        CloseHandle( stdout_write ); 
+        throw;
+    }
+} 
+*/
 //-------------------------------------------------------------------------------------------------
 
 } //namespace spooler
