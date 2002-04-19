8c18c047eff949f66f8854f7ffaeec147941f158
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2002-04-19 05:51:22 +0000

    sosopt
    git-svn-id: http://subversion.sos/svn/sos@9591 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_version.h
+++ spooler_version.h
@@ -1,9 +1,9 @@
-// $Id: spooler_version.h,v 1.41 2002/04/17 21:37:19 jz Exp $
+// $Id: spooler_version.h,v 1.42 2002/04/19 05:51:22 jz Exp $
 
 #define VER_FILEDESC_STR        "Spooler\0"
 
-#define VER_PRODUCTVERSION       1,1,23
-#define VER_PRODUCTVERSION_STR  "1.1.23,  2002-04-17\0"
+#define VER_PRODUCTVERSION       1,1,24
+#define VER_PRODUCTVERSION_STR  "1.1.24,  2002-04-18\0"
 
 #define VER_NAME_STR            "Spooler\0"
 #define VER_FILENAME_STR        "spooler.exe\0"
