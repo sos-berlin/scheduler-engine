80379e9e4aedef8be9d0ad7d1957322a6ec91ed5
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2002-10-26 16:56:31 +0000

    Portierung nach Linux
    git-svn-id: http://subversion.sos/svn/sos@9722 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_version.h
+++ spooler_version.h
@@ -1,9 +1,9 @@
-// $Id: spooler_version.h,v 1.75 2002/10/24 09:12:25 jz Exp $
+// $Id: spooler_version.h,v 1.76 2002/10/26 16:56:31 jz Exp $
 
 #define VER_FILEDESC_STR        "Spooler\0"
 
-#define VER_PRODUCTVERSION       1,2,30
-#define VER_PRODUCTVERSION_STR  "1.2.30, 2002-10-24\0"
+#define VER_PRODUCTVERSION       1,2,31
+#define VER_PRODUCTVERSION_STR  "1.2.31, 2002-10-26\0"
 
 #define VER_NAME_STR            "Spooler\0"
 #define VER_FILENAME_STR        "spooler.exe\0"
