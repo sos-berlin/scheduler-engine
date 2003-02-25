590bc3242d8536e584d602ee722cf94225b5bff7
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2003-02-25 09:25:58 +0000

    Spooler libxml2 utf8
    git-svn-id: http://subversion.sos/svn/sos@9876 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_version.h
+++ spooler_version.h
@@ -1,9 +1,9 @@
-// $Id: spooler_version.h,v 1.94 2003/02/19 07:50:43 jz Exp $
+// $Id: spooler_version.h,v 1.95 2003/02/25 09:25:57 jz Exp $
 
 #define VER_FILEDESC_STR        "Spooler\0"
 
-#define VER_PRODUCTVERSION       1,3,1
-#define VER_PRODUCTVERSION_STR  "1.3.1, 2002-02-19\0"
+#define VER_PRODUCTVERSION       1,3,2
+#define VER_PRODUCTVERSION_STR  "1.3.2, 2002-02-25\0"
 
 #define VER_NAME_STR            "Spooler\0"
 #define VER_FILENAME_STR        "spooler.exe\0"
