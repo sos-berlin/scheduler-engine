d088681d3fd28ad261bdddbf091759e0cf47b101
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2003-04-01 10:29:40 +0000

    *** empty log message ***
    git-svn-id: http://subversion.sos/svn/sos@9916 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- sos/spooler/Variable_set.java
+++ sos/spooler/Variable_set.java
@@ -1,4 +1,4 @@
-// $Id: Variable_set.java,v 1.4 2003/03/31 11:32:54 jz Exp $
+// $Id: Variable_set.java,v 1.5 2003/04/01 10:29:39 jz Exp $
 
 package sos.spooler;
 
@@ -6,7 +6,7 @@ package sos.spooler;
  * Variablenmenge.
  *
  * @author Joacim Zschimmer, Zschimmer GmbH
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 
 public class Variable_set extends Idispatch
@@ -15,6 +15,10 @@ public class Variable_set extends Idispatch
 
     public void         set_var                 ( String name, String value )       {                       com_call( ">var", name, value       ); }
     public String           var                 ( String name )                     { return (String)       com_call( "<var", name              ); }
+
+
+    /** @return Liefert die Anzahl der Variablen.
+      */
 
     public int              count               ()                                  { return            int_com_call( "<count"                  ); }
   
@@ -25,6 +29,23 @@ public class Variable_set extends Idispatch
     
     public void             merge               ( Variable_set vars )               {                       com_call( "merge", vars             ); }
 
+
+    /** Parameter ist ein XML-Dokument folgender DTD:
+      * <!ELEMENT variable_set ( variable* )>
+      * <!ELEMENT variable EMPTY>
+      * <!ATTLIST variable name CDATA #REQUIRED>
+      * <!ATTLIST variable value CDATA #REQUIRED>
+      *
+      * Die Variablen im XML-Dokument werden dem Variable_set hinzugefügt. 
+      * Vorhandene Variablen werden überschrieben.
+      */
+
     public void         set_xml                 ( String xml_text )                 {                       com_call( ">xml", xml_text          ); }
+
+
+    /** Liefert die Variablenmenge als XML-Dokument, wie in {@link #set_xml()} beschrieben.
+      * Das XML-Dokument kann {@link #set_xml()} übergeben werden.
+      */
+
     public String           xml                 ()                                  { return (String)       com_call( "<xml"                    ); }
 }
