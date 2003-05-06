59af05a74d23e66f72fd7800d79a846ab1f5278a
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2003-05-06 10:35:04 +0000

    *** empty log message ***
    git-svn-id: http://subversion.sos/svn/sos@9948 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_version.h
+++ spooler_version.h
@@ -1,9 +1,9 @@
-// $Id: spooler_version.h,v 1.114 2003/05/05 16:51:21 jz Exp $
+// $Id: spooler_version.h,v 1.115 2003/05/06 10:35:04 jz Exp $
 
 #define VER_FILEDESC_STR        "Spooler\0"
 
 #define VER_PRODUCTVERSION       1,3,21
-#define VER_PRODUCTVERSION_STR  "1.3.21, 2002-05-05\0"
+#define VER_PRODUCTVERSION_STR  "1.3.21, 2003-05-05\0"
 
 #define VER_NAME_STR            "Spooler\0"
 #define VER_FILENAME_STR        "spooler.exe\0"
