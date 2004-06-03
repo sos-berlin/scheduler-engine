329833f73c821b619f1de66d8edcb3a9cc3f73b8
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2004-06-03 15:40:59 +0000

    *** empty log message ***
    git-svn-id: http://subversion.sos/svn/sos@10738 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_version.h
+++ spooler_version.h
@@ -1,9 +1,9 @@
-// $Id: spooler_version.h,v 1.224 2004/05/31 16:16:02 jz Exp $
+// $Id: spooler_version.h,v 1.225 2004/06/03 15:40:59 jz Exp $
 
 #define VER_FILEDESC_STR        "Scheduler\0"
 
-#define VER_PRODUCTVERSION       2,0,72
-#define VER_PRODUCTVERSION_STR  "2.0.72, 2004-05-31\0"
+#define VER_PRODUCTVERSION       2,0,73
+#define VER_PRODUCTVERSION_STR  "2.0.73, 2004-06-03\0"
 
 #define VER_NAME_STR            "Scheduler\0"
 #define VER_FILENAME_STR        "scheduler.exe\0"
