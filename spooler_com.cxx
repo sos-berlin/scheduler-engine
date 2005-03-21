ffed356b626c8335a72a89836931751372e39a8c
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2005-03-21 13:45:55 +0000

    git-svn-id: http://subversion.sos/svn/sos@11345 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_com.cxx
+++ spooler_com.cxx
@@ -2482,7 +2482,7 @@ const Com_method Com_spooler::_methods[] =
     { DISPATCH_PROPERTYGET, 27, "Db_history_table_name"     , (Com_method_ptr)&Com_spooler::get_Db_history_table_name  , VT_BSTR      },
     { DISPATCH_PROPERTYGET, 28, "Db_order_history_table_name", (Com_method_ptr)&Com_spooler::get_Db_order_history_table_name, VT_BSTR      },
     { DISPATCH_PROPERTYGET, 29, "Ini_path"                  , (Com_method_ptr)&Com_spooler::get_Ini_path         , VT_BSTR      },
-    { DISPATCH_METHOD     , 30, "Execute_xml"               , (Com_method_ptr)&Com_spooler::Execute_xml          , VT_BSTR      },
+    { DISPATCH_METHOD     , 30, "Execute_xml"               , (Com_method_ptr)&Com_spooler::Execute_xml          , VT_BSTR     , { VT_BSTR } },
     {}
 };
 
