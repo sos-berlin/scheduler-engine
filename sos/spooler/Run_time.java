77a0ab59af01cda8a83fed2148cd281c6542e06a
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2005-03-31 12:38:45 +0000

    git-svn-id: http://subversion.sos/svn/sos@11364 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ sos/spooler/Run_time.java
@@ -0,0 +1,25 @@
+// $Id: Order.java 3481 2005-03-24 10:00:11Z jz $
+
+package sos.spooler;
+
+/** &lt;run_time>-Element.
+ * 
+ * @see Order#run_time()
+ *
+ * @author Joacim Zschimmer
+ * @version $Revision: 1.5 $
+ */
+
+public class Run_time  extends Idispatch
+{
+    private                 Run_time            ( long idispatch )                  { super(idispatch); }
+
+    
+    
+    /** Setzt die Run_time oder erweitert sie.
+     *  
+     * @param xml Ein &lt;run_time>-Dokument.
+     */
+    
+    public void         set_xml                 ( String xml )                      {                   com_call( ">xml", xml           ); }
+}
