09dea18670a852bd4ebfab1c8542b44eddc7577b
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2005-04-19 11:54:46 +0000

    git-svn-id: http://subversion.sos/svn/sos@11425 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_communication.cxx
+++ spooler_communication.cxx
@@ -586,12 +586,12 @@ int Communication::bind_socket( SOCKET socket, struct sockaddr_in* sa )
     int ret;
     int true_ = 1;
 
-#   ifdef Z_UNIX
+/*  War für Suse 8 nötig:
         LOG( "setsockopt(" << socket << ",SOL_SOCKET,SO_REUSEADDR,1)  " );
         ret = setsockopt( socket, SOL_SOCKET, SO_REUSEADDR, (const char*)&true_, sizeof true_ );
         LOG( "ret=" << ret );  if( ret == SOCKET_ERROR )  LOG( "errno=" << errno << "  "  << strerror(errno) );
         LOG( "\n" );
-#   endif
+*/
 
     ret = ::bind( socket, (struct sockaddr*)sa, sizeof (struct sockaddr_in) );
 
