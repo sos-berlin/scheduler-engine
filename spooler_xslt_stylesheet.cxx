2e1c98844c4528f434b1ffa16160f731b8bb6c7c
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2005-06-23 12:32:06 +0000

    git-svn-id: http://subversion.sos/svn/sos@11532 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ spooler_xslt_stylesheet.cxx
@@ -0,0 +1,77 @@
+// $Id: spooler_subprocess.cxx 3668 2005-05-17 10:47:25Z jz $
+
+#include "spooler.h"
+
+
+namespace sos {
+namespace spooler {
+
+
+//-------------------------------------------------------------------------------------------------
+
+Xslt_stylesheet::Class_descriptor    Xslt_stylesheet::class_descriptor ( &typelib, "Spooler.Xslt_stylesheet", Xslt_stylesheet::_methods );
+
+//------------------------------------------------------------------------Xslt_stylesheet::_methods
+
+const Com_method Xslt_stylesheet::_methods[] =
+{ 
+#ifdef COM_METHOD
+    COM_PROPERTY_GET( Xslt_stylesheet,  1, Java_class_name       , VT_BSTR    , 0 ),
+    COM_METHOD      ( Xslt_stylesheet,  2, Close                 , VT_EMPTY   ),
+    COM_METHOD      ( Xslt_stylesheet,  3, Load                  , VT_EMPTY   , 0, VT_BSTR ),
+    COM_METHOD      ( Xslt_stylesheet,  4, Transform_xml         , VT_EMPTY   , 0, VT_BSTR  ),
+    COM_METHOD      ( Xslt_stylesheet,  5, Transform_xml_to_file , VT_EMPTY   , 0, VT_BSTR, VT_BSTR  ),
+#endif
+    {}
+};
+
+//-----------------------------------------------------------------Xslt_stylesheet::Xslt_stylesheet
+
+Xslt_stylesheet::Xslt_stylesheet() 
+: 
+    Idispatch_implementation( &class_descriptor ),
+    _zero_(this+1)
+{
+}
+
+//-----------------------------------------------------------------Xslt_stylesheet::Xslt_stylesheet
+
+Xslt_stylesheet::Xslt_stylesheet( const string& xml_or_filename ) 
+: 
+    Idispatch_implementation( &class_descriptor ),
+    xml::Xslt_stylesheet( xml_or_filename ),
+    _zero_(this+1)
+{
+}
+
+//-----------------------------------------------------------------Xslt_stylesheet::Xslt_stylesheet
+
+Xslt_stylesheet::Xslt_stylesheet( const BSTR xml_or_filename_bstr ) 
+: 
+    Idispatch_implementation( &class_descriptor ),
+    xml::Xslt_stylesheet( xml_or_filename_bstr ),
+    _zero_(this+1)
+{
+}
+
+//----------------------------------------------------------------Xslt_stylesheet::~Xslt_stylesheet
+
+Xslt_stylesheet::~Xslt_stylesheet() 
+{
+}
+
+//------------------------------------------------------------------Xslt_stylesheet::QueryInterface
+
+STDMETHODIMP Xslt_stylesheet::QueryInterface( const IID& iid, void** result )
+{
+    Z_IMPLEMENT_QUERY_INTERFACE( this, iid, spooler_com::Ixslt_stylesheet    , result );
+    Z_IMPLEMENT_QUERY_INTERFACE( this, iid, spooler_com::Ihas_java_class_name, result );
+    Z_IMPLEMENT_QUERY_INTERFACE( this, iid, IDispatch                        , result );
+
+    return Idispatch_implementation::QueryInterface( iid, result );
+}
+
+//-------------------------------------------------------------------------------------------------
+
+} //namespace spoooler
+} //namespace sos
---
+++ spooler_xslt_stylesheet.h
@@ -0,0 +1,61 @@
+// $Id: spooler_subprocess.h 3668 2005-05-17 10:47:25Z jz $
+
+#ifndef __SPOOLER_XSLT_STYLESHEET_H
+#define __SPOOLER_XSLT_STYLESHEET_H
+
+#include "../zschimmer/xslt_libxslt.h"
+
+
+namespace sos {
+namespace spooler {
+
+
+
+//----------------------------------------------------------------------------------Xslt_stylesheet
+
+struct Xslt_stylesheet : idispatch_implementation< Xslt_stylesheet, spooler_com::Ixslt_stylesheet >, 
+                         xml::Xslt_stylesheet,
+                         spooler_com::Ihas_java_class_name
+{
+    static Class_descriptor        class_descriptor;
+    static const com::Com_method  _methods[];
+
+
+                                Xslt_stylesheet             ();
+                                Xslt_stylesheet             ( const string& xml_or_filename );
+                                Xslt_stylesheet             ( const BSTR xml_or_filename );
+                               ~Xslt_stylesheet             ();
+
+    STDMETHODIMP_(ULONG)        AddRef                      ()                                      { return Idispatch_implementation::AddRef(); }
+    STDMETHODIMP_(ULONG)        Release                     ()                                      { return Idispatch_implementation::Release(); }
+    STDMETHODIMP                QueryInterface              ( const IID&, void** );
+
+    // interface Ihas_java_class_name
+    STDMETHODIMP            get_Java_class_name             ( BSTR* result )                        { return String_to_bstr( const_java_class_name(), result ); }
+    STDMETHODIMP_(char*)  const_java_class_name             ()                                      { return (char*)"sos.spooler.Xslt_stylesheet"; }
+
+
+    // interface Ixslt_stylesheet
+    STDMETHODIMP                Close                       ()                                      { return xml::Xslt_stylesheet::Close(); }
+    STDMETHODIMP                Load_xml                    ( BSTR xml )                            { return xml::Xslt_stylesheet::Load_xml( xml ); }
+    STDMETHODIMP                Load_file                   ( BSTR filename )                       { return xml::Xslt_stylesheet::Load_file( filename ); }
+    STDMETHODIMP                Transform_xml               ( BSTR xml, BSTR* result )              { return xml::Xslt_stylesheet::Transform_xml( xml, result ); }
+  //STDMETHODIMP                Transform_xml_to_file       ( BSTR xml, BSTR filename )             { return xml::Xslt_stylesheet::Transform_xml_to_file( xml, filename ); }
+
+
+    bool                        is_xml                      ( const string& );
+    void                        close                       ();
+    void                        load                        ( const string& );
+    string                      Transform_xml               ( const string& );
+    void                        Transform_xml_to_file       ( const string& xml, const string& filename );
+
+  private:
+    Fill_zero                  _zero_;
+};
+
+//-------------------------------------------------------------------------------------------------
+
+} //namespace spooler
+} //namespace sos
+
+#endif
