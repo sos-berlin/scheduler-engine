09ed83409ee34d607b4efe3967f0e9a46f177ad8
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2007-01-23 23:03:56 +0000

    git-svn-id: http://subversion.sos/svn/sos@12566 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_task.h
+++ spooler_task.h
@@ -216,8 +216,7 @@ struct Task : Object,
     friend struct               Job;
     friend struct               Job::Task_queue;
     friend struct               Object_set;
-    friend struct               Com_task;
-
+    friend struct               com_objects::Com_task;
     friend struct               Task_history;
 
     virtual bool                do_load                     ()                                      { return true; }
