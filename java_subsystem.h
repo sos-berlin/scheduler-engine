8260200a21b9e808eb61f2346263ca9ec458e64e
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2007-01-29 11:24:04 +0000

    git-svn-id: http://subversion.sos/svn/sos@12584 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ java_subsystem.h
@@ -0,0 +1,27 @@
+// $Id: scheduler_script.h 4773 2007-01-20 23:15:39Z jz $        Joacim Zschimmer, Zschimmer GmbH, http://www.zschimmer.com
+
+#ifndef __SCHEDULER_JAVA_SUBSYSTEM_H
+#define __SCHEDULER_JAVA_SUBSYSTEM_H
+
+namespace sos {
+namespace scheduler {
+
+//-----------------------------------------------------------------------Java_subsystem_interface
+
+struct Java_subsystem_interface : Subsystem
+{
+                                Java_subsystem_interface    ( Scheduler* scheduler, Type_code t )   : Subsystem( scheduler, t ) {}
+
+    virtual java::Vm*           java_vm                     ()                                      = 0;
+};
+
+//-------------------------------------------------------------------------------------------------
+
+ptr<Java_subsystem_interface>   new_java_subsystem          ( Scheduler* );
+
+//-------------------------------------------------------------------------------------------------
+
+} //namespace scheduler
+} //namespace sos
+
+#endif
