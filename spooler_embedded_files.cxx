b8ede5ca3d7768042705eb24410d70414a0d605c
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2007-05-04 15:35:34 +0000

    git-svn-id: http://subversion.sos/svn/sos@12804 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_embedded_files.cxx
+++ spooler_embedded_files.cxx
@@ -1580,7 +1580,7 @@ namespace scheduler {
 
 static const Embedded_file embedded_files_array[] = 
 {
-    { "scheduler.xsd", file_scheduler_xsd, sizeof file_scheduler_xsd - 1, 1178286153 },
+    { "scheduler.xsd", file_scheduler_xsd, sizeof file_scheduler_xsd - 1, 1178292357 },
     { NULL, NULL, 0 }
 };
 
