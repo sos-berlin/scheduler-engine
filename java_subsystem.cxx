8b67beaac5ec7e1a8000bfbdcb35f26204b490e0
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2010-09-01 07:41:53 +0000

    Maven
    git-svn-id: http://subversion.sos/svn/sos@13990 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- java_subsystem.cxx
+++ java_subsystem.cxx
@@ -133,6 +133,7 @@ void Java_subsystem::initialize_java_sister()
     _java_objects = Z_NEW( Java_objects );
     _java_objects->_schedulerJ = SchedulerJ::new_instance(_spooler->j());
     _java_objects->_schedulerJ.test("Hallo, hier ist C++");
+    _java_objects->_schedulerJ.test2("Hallo, hier ist nochmal C++");
 }
 
 //-----------------------------------------------------------Java_subsystem::register_proxy_classes
