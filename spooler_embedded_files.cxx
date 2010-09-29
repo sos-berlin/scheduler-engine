3ed99a01e01d7d822dfa59a739986da361854800
rb <rb@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2010-09-29 11:22:59 +0000

    JS-544 [smtp] Einstellung nicht "löschen", wenn smtp in sos.ini und factory.ini leer sind oder fehlen. Doku mit Hinweis auf die Möglichkeit den SMTP Port einstellen zu können, ergänzt.
    git-svn-id: http://subversion.sos/svn/sos@14051 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- spooler_embedded_files.cxx
+++ spooler_embedded_files.cxx
@@ -1964,7 +1964,7 @@ namespace scheduler {
 
 static const Embedded_file embedded_files_array[] = 
 {
-    { "scheduler.xsd", file_scheduler_xsd, sizeof file_scheduler_xsd - 1, 1285587311 },
+    { "scheduler.xsd", file_scheduler_xsd, sizeof file_scheduler_xsd - 1, 1285659962 },
     { "doc.en/log_categories.xml", file_doc_en_log_categories_xml, sizeof file_doc_en_log_categories_xml - 1, 1284714349 },
     { NULL, NULL, 0 }
 };
