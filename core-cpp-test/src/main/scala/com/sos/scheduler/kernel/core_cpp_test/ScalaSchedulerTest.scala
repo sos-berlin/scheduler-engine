e87aefd2042b043ceb63f914b4eca8e109d223af
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2010-12-30 13:01:58 +0000

    Umbennung kernel -> engine
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@7469 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- core-cpp-test/src/main/scala/com/sos/scheduler/kernel/core_cpp_test/ScalaSchedulerTest.scala
+++
@@ -1,9 +0,0 @@
-package com.sos.scheduler.engine.kernelcpptest
-
-import org.junit._
-
-
-abstract class ScalaSchedulerTest extends SchedulerTest
-{
-    def shortTimeout = SchedulerTest.shortTimeout       // Zur komfortableren Benutzung
-}
