12661c404f5b8a7fa36d8a94e3aa6e2fbb850ebe
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-05-21 10:08:58 +0000

    JS-647 Test im neuen Paket jira abgelegt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@14422 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel-cpp-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/jira/js647/UnixBlockedSignalTest.java
+++ kernel-cpp-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/jira/js647/UnixBlockedSignalTest.java
@@ -1,7 +1,5 @@
-package com.sos.scheduler.engine.kernelcpptest.blockedsignal;
+package com.sos.scheduler.engine.kernelcpptest.jira.js647;
 
-import java.io.File;
-import com.google.common.base.Splitter;
 import com.sos.scheduler.engine.kernelcpptest.OperatingSystem;
 import com.sos.scheduler.engine.kernel.event.Event;
 import com.sos.scheduler.engine.kernel.event.EventSubscriber;
@@ -9,10 +7,12 @@ import com.sos.scheduler.engine.kernel.util.Time;
 import com.sos.scheduler.engine.kernelcpptest.SchedulerTest;
 import org.apache.log4j.*;
 import org.junit.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 
+/** Stellt sicher, dass ein Shell-Prozess einen Kind-Prozess mit kill abbrechen kann.
+ *
+ * @author Joacim Zschimmer
+ */
 public class UnixBlockedSignalTest extends SchedulerTest {
     private static final Logger logger = Logger.getLogger(UnixBlockedSignalTest.class);
     private static final Time myTimeout = Time.of(10+5);   // Länger als der Job im Fehlerfall läuft. Siehe blockedSignal.job.xml
