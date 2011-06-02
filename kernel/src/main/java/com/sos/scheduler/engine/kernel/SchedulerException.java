2b94abb4f45adf4094ad47f3ee1102d2ddf257ba
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-06-02 20:39:50 +0000

    JS-504: Plugins mit Kommandoschnittelle (ermöglicht besondere GUI-Kommandos mit eigenem Datenbankzugriff) Kommandoschnittelle für Java
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@14544 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel/src/main/java/com/sos/scheduler/engine/kernel/SchedulerException.java
+++ kernel/src/main/java/com/sos/scheduler/engine/kernel/SchedulerException.java
@@ -1,10 +1,13 @@
 package com.sos.scheduler.engine.kernel;
 
+import com.sos.scheduler.engine.kernel.event.Message;
+
 
 //TODO Meldungscodes erzwingen
 public class SchedulerException extends RuntimeException {
     private static final long serialVersionUID = 2011040301;
 
+    public SchedulerException(Message m) { super(m.toString()); }
     public SchedulerException(String message) { super(message); }
     public SchedulerException(String message, Throwable t) { super(message, t); }
     public SchedulerException(Throwable t) { super(t); }
