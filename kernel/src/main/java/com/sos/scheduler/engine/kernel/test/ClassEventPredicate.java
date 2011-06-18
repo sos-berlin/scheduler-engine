ff78cb71ad8dda37ae66ee5634625853c0c47ded
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-06-18 11:55:49 +0000

    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@14625 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ kernel/src/main/java/com/sos/scheduler/engine/kernel/test/ClassEventPredicate.java
@@ -0,0 +1,24 @@
+package com.sos.scheduler.engine.kernel.test;
+
+import com.sos.scheduler.engine.kernel.event.Event;
+import com.sos.scheduler.engine.kernel.event.EventPredicate;
+
+
+public class ClassEventPredicate implements EventPredicate {
+    private final Class<? extends Event> eventClass;
+
+
+    public ClassEventPredicate(Class<? extends Event> eventClass) {
+        this.eventClass = eventClass;
+    }
+
+
+    @Override public boolean apply(Event e) {
+        return eventClass.isAssignableFrom(e.getClass());
+    }
+
+
+    @Override public String toString() {
+        return eventClass.getName();
+    }
+}
