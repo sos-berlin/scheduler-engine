a57757cd9701b14afd54cff0f7274626306adda3
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-06-21 15:12:25 +0000

    Erstellungsparameter (version, Platform) per command line
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@14644 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ setup/prepare-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# != 1 ]
+then
+  echo "usage: prepare-setup.sh <version>"
+  exit 99
+fi
+
+version=$1
+mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease -Dsetup.version=$version -Dsetup.platform=windows-x86
+mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease -Dsetup.version=$version -Dsetup.platform=linux-i386
--- setup/setup.bat
+++
@@ -1,2 +0,0 @@
-call mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease -Pwindows
-call mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease -Plinux
