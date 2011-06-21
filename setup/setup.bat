4921fef00e173f941f6288452d983b175a7260a4
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-06-21 13:49:26 +0000

    2. Version
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@14641 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,1 +1,2 @@
-mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease
+call mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease -Pwindows
+call mvn dependency:copy dependency:copy-dependencies antrun:run -Prelease -Plinux
