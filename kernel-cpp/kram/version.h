6cff987da52e1147b8ead049d800a1449797f53c
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-07-08 07:21:01 +0000

    mavenSettings.h wird jetzt nach "generated" generiert und in SVN eingecheckt (damit mit einfachem svn export kompiliert werden kann)
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@14774 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel-cpp/kram/version.h
+++ kernel-cpp/kram/version.h
@@ -1,7 +1,8 @@
 // $Id$
 // Für Windows-Dateiinfo
 
-#include "../target/cpp/mavenSettings.h"
+// #include "../target/cpp/mavenSettings.h"
+#include "../generated/mavenSettings.h"
 
 // Die Hostware-Versionsnummer wird nicht von Maven verwaltet, die ist fest (hoffentlich für alle Zeit)
 #undef VER_PRODUCTVERSION
