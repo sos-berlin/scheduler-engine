b1c7732eca648231e158f9ce6587a1e5b43b9dc5
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-10-10 06:57:30 +0000

    ClassResource auf URL umgestellt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15275 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel/src/main/java/com/sos/scheduler/engine/kernel/util/ClassResource.java
+++ kernel/src/main/java/com/sos/scheduler/engine/kernel/util/ClassResource.java
@@ -1,8 +1,14 @@
 package com.sos.scheduler.engine.kernel.util;
 
 //import com.google.common.io.InputSupplier;
-import java.io.InputStream;
+import static com.google.common.collect.Maps.fromProperties;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+import com.google.common.collect.ImmutableMap;
 
 public class ClassResource /*implements InputSupplier<InputStream>*/ {
     private final Class<?> clas;
@@ -14,25 +20,36 @@ public class ClassResource /*implements InputSupplier<InputStream>*/ {
         subPath = path;
     }
 
-
     public final InputStream getInputStream() {
-        InputStream result = clas.getClassLoader().getResourceAsStream(getPath());
-        if (result == null)  throw new RuntimeException("Java ressource '" + getPath() + "' is missing" );
-        return result;
+        try {
+            //clas.getClassLoader().getResourceAsStream(getPath());
+            return url().openStream();
+        } catch (IOException x) {
+            throw new RuntimeException("Java ressource '" + url() + "' is missing", x);
+        }
     }
 
-
-//    @Override public InputStream getInput() {
-//        return getInputStream();
+//    private final String getPath() {
+//        return clas.getPackage().getName().replace(".", "/") + "/" + subPath;
 //    }
 
-
-    public final String getPath() {
-        return clas.getPackage().getName().replace(".", "/") + "/" + subPath;
+    public URL url() {
+        return clas.getResource(subPath);
     }
 
+    public ImmutableMap<String,String> properties() {
+        try {
+            Properties properties = new Properties();
+            properties.load(getInputStream());
+            return fromProperties(properties);
+        } catch (IOException e) { throw new RuntimeException(e); }
+    }
 
     @Override public final String toString() {
-        return "Java resource " + getPath();
+        return "Java resource " + url();
+    }
+
+    public static URL resourceUrl(Class<?> c, String subPath) {
+        return new ClassResource(c, subPath).url();
     }
 }
