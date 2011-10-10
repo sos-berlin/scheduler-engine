418f53969a1a97bac9f92a9cd73915c0de13f39c
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-10-10 06:56:28 +0000

    PropertiesTest: 10s Wartezeit ausgebaut
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15270 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/scheduler/properties/PropertiesTest.java
+++ kernel-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/scheduler/properties/PropertiesTest.java
@@ -3,22 +3,24 @@ package com.sos.scheduler.engine.kernelcpptest.scheduler.properties;
 import com.sos.scheduler.engine.kernel.test.SchedulerTest;
 import org.apache.log4j.*;
 import org.junit.*;
-import static org.junit.Assert.*;
 
+import static com.google.common.base.Strings.nullToEmpty;
 
 public class PropertiesTest extends SchedulerTest {
     private static final Logger logger = Logger.getLogger(PropertiesTest.class);
 
     @Test public void test1() throws Exception {
         try {
-            runScheduler(shortTimeout);
-            System.out.println("port=" + scheduler.getTcpPort());
-            System.out.println("host=" + scheduler.getHostname());
-            System.out.println("host_complete=" + scheduler.getHostnameLong());
+            startScheduler();
+            System.out.println("port=" + getScheduler().getTcpPort());
+            System.out.println("host=" + getScheduler().getHostname());
+            System.out.println("host_complete=" + getScheduler().getHostnameLong());
+            schedulerController.terminateAndWait();
 //            fail("Exception expected");
         }
         catch (Exception x) {
-            if (!x.getMessage().contains("SOS-1300"))  fail("Exception enthält nicht SOS-1300");
+            if (!nullToEmpty(x.getMessage()).contains("SOS-1300"))
+                throw new RuntimeException("Exception enthält nicht SOS-1300", x);
             logger.debug("OK: " + x);
         }
     }
