a6ac0423d24134611ac7fbea02d2a234547a1711
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-10-21 19:07:04 +0000

    - ShowStateStressTest aufgeräumt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15394 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel-test/src/test/scala/com/sos/scheduler/engine/kernelcpptest/stress/showstate/ShowStateStressTest.scala
+++ kernel-test/src/test/scala/com/sos/scheduler/engine/kernelcpptest/stress/showstate/ShowStateStressTest.scala
@@ -1,19 +1,18 @@
 package com.sos.scheduler.engine.kernelcpptest.stress.showstate
 
-import com.sos.scheduler.engine.kernel.test.SuperSchedulerTest
-import com.sos.scheduler.engine.cplusplus.scalautil.io.Util._
 import java.io._
 import java.net._
 import java.nio.charset.Charset
 import org.junit._
+import com.sos.scheduler.engine.cplusplus.scalautil.io.Util._
+import com.sos.scheduler.engine.kernelcpptest.ScalaSchedulerTest
 
-
-class ShowStateStressTest extends SuperSchedulerTest {
+final class ShowStateStressTest extends ScalaSchedulerTest {
     import ShowStateStressTest._
     
-    @Ignore @Test def test1() {
-        startScheduler("-e", "-log-level=warn")
-        waitUntilSchedulerIsRunning()
+    //@Ignore
+    @Test def test1() {
+        controller.startScheduler("-e", "-log-level=warn")
         closingFinally(new Connection(new InetSocketAddress("localhost", scheduler.getTcpPort))) { connection =>
             1 to 1000 foreach { i => connection.sendAndReceive(emptyCommand) }
         }
@@ -21,24 +20,26 @@ class ShowStateStressTest extends SuperSchedulerTest {
 }
 
 object ShowStateStressTest {
-    def emptyCommand = (  //<params/>.toString
-        <commands><subsystem.show what="statistics"/><show_state what="folders cluster remote_schedulers schedules" subsystems="lock schedule process_class folder"/></commands>
+    private def emptyCommand = (  //<params/>.toString
+        <commands>
+            <subsystem.show what="statistics"/>
+            <show_state what="folders cluster remote_schedulers schedules" subsystems="lock schedule process_class folder"/>
+        </commands>
     ).toString()
     
     def main(args: Array[String]) {
-        implicit def inetSocketAddress(a: String) = {
+        def inetSocketAddress(a: String) = {
             val b = a split ':'
             new InetSocketAddress(b(0), b(1).toInt)
         }
 
         val connection = new Connection(inetSocketAddress(args(0)))
-        1 to Int.MaxValue foreach { i => connection.sendAndReceive(emptyCommand) }
+        1 to Int.MaxValue foreach { _ => connection.sendAndReceive(emptyCommand) }
     }
 
-
-    class Connection(val address: InetSocketAddress) {
-        private def encoding = Charset.forName("UTF-8")     //TODO XML-Routinen verwenden, OutputStream statt Writer/Reader
-        private val socket = new Socket()
+    private class Connection(val address: InetSocketAddress) {
+        private def encoding = Charset.forName("UTF-8")     //TODO Statt XML.toString besser XML direkt nach OutputStream schreiben lassen
+        private val socket = new Socket
         socket.connect(address)
         private val writer = new OutputStreamWriter(socket.getOutputStream, encoding)
         private val reader = new SplitReader(new InputStreamReader(socket.getInputStream), '\0')
@@ -54,9 +55,10 @@ object ShowStateStressTest {
         }
     }
 
-
-    class SplitReader(reader: Reader, separator: Char) extends Closeable {
-        def close() { reader.close() }
+    private class SplitReader(reader: Reader, separator: Char) extends Closeable {
+        def close() {
+          reader.close()
+        }
 
         def read(): String = {
             val result = new StringBuilder
