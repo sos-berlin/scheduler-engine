a6f8250f0bb7acf23e02d9e64549c5b76de53a78
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-10-31 16:46:16 +0000

    JS-644: JS644PluginSchedulerTest
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15488 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ plugins/src/test/java/com/sos/scheduler/engine/plugins/js644/JS644PluginSchedulerTest.java
@@ -0,0 +1,69 @@
+package com.sos.scheduler.engine.plugins.js644;
+
+import static com.sos.scheduler.engine.plugins.js644.JS644PluginSchedulerTest.E.jobActivated;
+import static com.sos.scheduler.engine.plugins.js644.JS644PluginSchedulerTest.E.jobchainActivated;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import com.sos.scheduler.engine.kernel.event.Event;
+import com.sos.scheduler.engine.kernel.event.EventSubscriber;
+import com.sos.scheduler.engine.kernel.folder.AbsolutePath;
+import com.sos.scheduler.engine.kernel.folder.event.FileBasedActivatedEvent;
+import com.sos.scheduler.engine.kernel.job.Job;
+import com.sos.scheduler.engine.kernel.main.SchedulerState;
+import com.sos.scheduler.engine.kernel.test.SchedulerTest;
+import com.sos.scheduler.engine.kernel.util.Time;
+
+public class JS644PluginSchedulerTest extends SchedulerTest {
+    private static final Logger logger = Logger.getLogger(JS644PluginSchedulerTest.class);
+    private static final AbsolutePath jobPath = new AbsolutePath("/A");
+    private static final AbsolutePath jobchainPath = new AbsolutePath("/a");
+    private static final Time timeout = shortTimeout;
+    enum E { jobActivated, jobchainActivated };
+    private final Gate<E> gate = new Gate<E>();
+
+    @Test public void test() throws Exception {
+        controller().startScheduler("-e");
+        controller().waitUntilSchedulerState(SchedulerState.active);
+        controller().strictSubscribeEvents(new MyEventSubscriber());
+        modifyJobFile();
+        gate.expect(jobActivated, timeout);
+        gate.expect(jobchainActivated, timeout);
+    }
+
+    private void modifyJobFile() throws IOException {
+        File jobFile = fileBasedFile(scheduler().getJobSubsystem().job(jobPath));
+        assertThat(jobFile + " does not exist", jobFile.exists());
+        OutputStream out = new FileOutputStream(jobFile, true);
+        try {
+            out.write(' ');
+        } finally {
+            out.close();
+        }
+    }
+
+    private class MyEventSubscriber implements EventSubscriber {
+        @Override public void onEvent(Event event) throws InterruptedException {
+            if (event instanceof FileBasedActivatedEvent) {
+                FileBasedActivatedEvent e = (FileBasedActivatedEvent)event;
+                if (e.getObject() == scheduler().getJobSubsystem().job(jobPath)) {
+                    gate.put(jobActivated);
+                } else
+                if (e.getObject() == scheduler().getOrderSubsystem().jobChain(jobchainPath)) {
+                    gate.put(jobchainActivated);
+                }
+            }
+        }
+    }
+
+    private File fileBasedFile(Job o) {
+        return new File(controller().environment().configDirectory(), o.getPath() + ".job.xml");
+    }
+}
