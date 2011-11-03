0425c816e817454669864441451326336247356d
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-03 13:22:11 +0000

    - DatabaseQueryPlugTest: Debug-Meldungen eingebaut
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15541 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- plugins/src/test/java/com/sos/scheduler/engine/plugins/databasequery/DatabaseQueryPluginTest.java
+++ plugins/src/test/java/com/sos/scheduler/engine/plugins/databasequery/DatabaseQueryPluginTest.java
@@ -3,11 +3,14 @@ package com.sos.scheduler.engine.plugins.databasequery;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import com.sos.scheduler.engine.kernel.test.SchedulerTest;
 
 public final class DatabaseQueryPluginTest extends SchedulerTest {
+    private static final Logger logger = Logger.getLogger(DatabaseQueryPluginTest.class);
+
     public DatabaseQueryPluginTest() throws Exception {
         controller().startScheduler("-e");
         waitForTaskTermination();
@@ -31,6 +34,8 @@ public final class DatabaseQueryPluginTest extends SchedulerTest {
                 "<plugin.command plugin_class='" + DatabaseQueryPlugin.class.getName() + "'>" +
                     subcommandXml +
                 "</plugin.command>";
-        return scheduler().executeXml(commandXml);
+        String result = scheduler().executeXml(commandXml);
+        logger.debug(result);
+        return result;
     }
 }
