578cce3717a769d941409e08266cb44455038a19
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-05 20:58:38 +0000

    - JS611Test endet früher
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15561 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/jira/js611/JS611Test.java
+++ kernel-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/jira/js611/JS611Test.java
@@ -2,25 +2,24 @@ package com.sos.scheduler.engine.kernelcpptest.jira.js611;
 
 import org.junit.Test;
 
-import com.sos.scheduler.engine.kernel.event.Event;
-import com.sos.scheduler.engine.kernel.event.EventSubscriber;
+import com.sos.scheduler.engine.kernel.event.EventHandler;
+import com.sos.scheduler.engine.kernel.order.OrderFinishedEvent;
 import com.sos.scheduler.engine.kernel.test.SchedulerTest;
 import com.sos.scheduler.engine.kernel.util.Time;
 
 //TODO Wie prüfen wir das? Erstmal durch Blick ins scheduler.log: {JS-611}.
-/** Test, ob Scheduler Log-Dateien verübergehen schließt, wenn mehr Logs als festgelegt geschrieben werden. */
+/** Test, ob Scheduler Log-Dateien verübergehen schließt, wenn mehr Logs als festgelegt geschrieben werden.
+ * @see <a href="http://www.sos-berlin.com/jira/browse/JS-611">JS-611</a> */
 public class JS611Test extends SchedulerTest {
     //private static final Logger logger = Logger.getLogger(JS611Test.class);
-	private static final int orderCount = 10;
-    private static final Time myTimeout = Time.of(orderCount * 1.1);
-
+	private static final int maxOrderCount = 10;
+    private static final Time myTimeout = Time.of(maxOrderCount * 1.1);
+    private int finishedOrderCount = 0;
 
     @Test public void test() throws Exception {
-        MyEventSubscriber eventSubscriber = new MyEventSubscriber();
-        controller().strictSubscribeEvents(eventSubscriber);
-        controller().startScheduler("-e");
-        controller().scheduler().executeXml("<scheduler_log.log_categories.set category='JS-611'/>");
-        addOrders(orderCount);
+        controller().startScheduler();
+        scheduler().executeXml("<scheduler_log.log_categories.set category='JS-611'/>");
+        addOrders(maxOrderCount);
         controller().waitForTermination(myTimeout);
     }
     
@@ -29,9 +28,9 @@ public class JS611Test extends SchedulerTest {
     		scheduler().executeXml("<add_order job_chain='/A' id='" + i + "'/>");
     }
 
-
-    private class MyEventSubscriber implements EventSubscriber {
-        @Override public void onEvent(Event event) {
-        }
+    @EventHandler public void handle(OrderFinishedEvent e) {
+        finishedOrderCount += 1;
+        if (finishedOrderCount == maxOrderCount)
+            controller().terminateScheduler();
     }
 }
