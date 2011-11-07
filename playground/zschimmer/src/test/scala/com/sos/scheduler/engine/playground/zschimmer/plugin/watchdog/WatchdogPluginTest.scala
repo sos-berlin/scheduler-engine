91343a06464c216b5a2efa134c0b11bb019c9eb4
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-07 22:29:30 +0000

    - WatchdogPluginTest aufgeräumt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15588 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- playground/zschimmer/src/test/scala/com/sos/scheduler/engine/playground/zschimmer/plugin/watchdog/WatchdogPluginTest.scala
+++ playground/zschimmer/src/test/scala/com/sos/scheduler/engine/playground/zschimmer/plugin/watchdog/WatchdogPluginTest.scala
@@ -1,29 +1,23 @@
 package com.sos.scheduler.engine.playground.zschimmer.plugin.watchdog
 
-import com.sos.scheduler.engine.kernel.event.Event
-import com.sos.scheduler.engine.kernel.event.EventSubscriber
 import com.sos.scheduler.engine.kernel.order.OrderFinishedEvent
 import com.sos.scheduler.engine.kernel.util.Time
 import com.sos.scheduler.engine.kernelcpptest.ScalaSchedulerTest
 import org.junit._
+import com.sos.scheduler.engine.kernel.event.EventHandler
 
-
-class WatchdogPlugInTest extends ScalaSchedulerTest {
+class WatchdogPluginTest extends ScalaSchedulerTest {
     private val schedulerTimeout = Time.of(15)
     private val sleepTime = Time.of(11)
-    private val watchdogPeriod = Time.of(1)
 
     @Test def test() {
-        controller.strictSubscribeEvents(new MyEventSubscriber)
-        controller.runScheduler(schedulerTimeout, "-e")
+        controller.startScheduler("-e")
+        Thread.sleep(schedulerTimeout.getMillis)
+        //TODO SCHEDULER-721 soll gemeldet sein und Plugin soll Warnungen ausgegeben haben.   assertThat(scheduler.log().last(SchedulerLogLevel.warn), Matchers.stringStartsWith("SCHEDULER-721"));
+        controller.terminateScheduler()
     }
 
-    class MyEventSubscriber extends EventSubscriber {
-        override def onEvent(e: Event) {
-            e match {
-                case e: OrderFinishedEvent => Thread.sleep(sleepTime.getMillis)
-                case _ =>
-            }
-        }
+    @EventHandler def handleEvent(e: OrderFinishedEvent) {
+        Thread.sleep(sleepTime.getMillis)   // Wir  blockieren den Scheduler
     }
 }
