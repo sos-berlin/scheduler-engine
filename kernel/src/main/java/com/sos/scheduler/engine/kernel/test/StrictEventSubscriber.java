9cf804b2e58c199e083c7ea11109dfaa97bde1ad
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-07 22:28:24 +0000

    StrictEventSubscriber fängt alle Throwable ab
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15583 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel/src/main/java/com/sos/scheduler/engine/kernel/test/StrictEventSubscriber.java
+++ kernel/src/main/java/com/sos/scheduler/engine/kernel/test/StrictEventSubscriber.java
@@ -2,31 +2,22 @@ package com.sos.scheduler.engine.kernel.test;
 
 import com.sos.scheduler.engine.kernel.event.Event;
 import com.sos.scheduler.engine.kernel.event.EventSubscriber;
-import com.sos.scheduler.engine.kernel.log.ErrorLogEvent;
 import com.sos.scheduler.engine.kernel.main.SchedulerController;
-
 
 /** Beendet bei einer Exception oder einem ErrorLogEvent den Scheduler. */
 public class StrictEventSubscriber implements EventSubscriber {
     private final EventSubscriber eventSubscriber;
     private final SchedulerController schedulerController;
 
-
     public StrictEventSubscriber(EventSubscriber s, SchedulerController controller) {
         eventSubscriber = s;
         schedulerController = controller;
     }
 
-
     @Override public final void onEvent(Event e) {
         try {
             eventSubscriber.onEvent(e);
-        }
-        catch (Exception x) {
-            schedulerController.terminateAfterException(x);
-        }
-        catch (Error x) {
-            //logger.error(x);
+        } catch (Throwable x) {
             schedulerController.terminateAfterException(x);
         }
     }
