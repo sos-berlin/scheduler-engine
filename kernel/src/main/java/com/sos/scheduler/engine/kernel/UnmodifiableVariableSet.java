d95ccdad30daafadf43a9194b952051c30755e87
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-07 22:29:39 +0000

    JS-731: UnmodifiableVariableSet.getNames()
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15589 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel/src/main/java/com/sos/scheduler/engine/kernel/UnmodifiableVariableSet.java
+++ kernel/src/main/java/com/sos/scheduler/engine/kernel/UnmodifiableVariableSet.java
@@ -1,9 +1,11 @@
 package com.sos.scheduler.engine.kernel;
 
+import java.util.Collection;
+
 import javax.annotation.Nullable;
 
 public interface UnmodifiableVariableSet {
     @Nullable String tryGet(String name);
-
     String get(String name);
+    Collection<String> getNames();
 }
