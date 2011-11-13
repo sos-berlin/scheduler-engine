0522e9ac6ea15454c34e4ea372ef29e24a05dad3
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-13 22:07:18 +0000

    - MaxOrderTest auf @EventHandler umgestellt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15627 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- kernel-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/jobchain/max_order/MaxOrderTest.java
+++ kernel-test/src/test/java/com/sos/scheduler/engine/kernelcpptest/jobchain/max_order/MaxOrderTest.java
@@ -8,8 +8,7 @@ import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
-import com.sos.scheduler.engine.kernel.event.Event;
-import com.sos.scheduler.engine.kernel.event.EventSubscriber;
+import com.sos.scheduler.engine.kernel.event.EventHandler;
 import com.sos.scheduler.engine.kernel.order.OrderFinishedEvent;
 import com.sos.scheduler.engine.kernel.order.OrderTouchedEvent;
 import com.sos.scheduler.engine.kernel.test.SchedulerTest;
@@ -20,41 +19,34 @@ public class MaxOrderTest extends SchedulerTest {
     private static final int addedOrderCount = 9;   // Anzahl der <add_order>
     private static final Logger log = Logger.getLogger(MaxOrderTest.class);
 
+    private int touchedOrderCount = 0;
+    private int maxTouchedOrderCount = 0;
+    private int finishedOrderCount = 0;
 
     @Test public void test() throws Exception {
-        controller().strictSubscribeEvents(new MyEventSubscriber());
         controller().runScheduler(shortTimeout);
     }
 
+    @EventHandler public void handlEvent(OrderTouchedEvent e) {
+        touchedOrderCount++;
+        maxTouchedOrderCount = max(maxTouchedOrderCount, touchedOrderCount);
+    }
 
-    class MyEventSubscriber implements EventSubscriber {
-        private int touchedOrderCount = 0;
-        private int maxTouchedOrderCount = 0;
-        private int finishedOrderCount = 0;
-        
-        @Override public void onEvent(Event e) {
-            if (e instanceof OrderTouchedEvent) {
-                touchedOrderCount++;
-                maxTouchedOrderCount = max(maxTouchedOrderCount, touchedOrderCount);
-            }
-            else
-            if (e instanceof OrderFinishedEvent) {
-                touchedOrderCount--;
-                finishedOrderCount++;
-                if (finishedOrderCount == addedOrderCount)
-                    finish();
-            }
-        }
+    @EventHandler public void handleEvent(OrderFinishedEvent e) {
+        touchedOrderCount--;
+        finishedOrderCount++;
+        if (finishedOrderCount == addedOrderCount)
+            finish();
+    }
 
-        private void finish() {
-            check();
-            controller().terminateScheduler();
-        }
+    private void finish() {
+        check();
+        controller().terminateScheduler();
+    }
 
-        private void check() {
-            assertThat("touchedOrderCount != 0", touchedOrderCount, equalTo(0));
-            assertThat(maxTouchedOrderCount, lessThanOrEqualTo(maxOrders));
-            assertThat(maxTouchedOrderCount, equalTo(maxOrders));   // Strenger, das Maximum soll genutzt worden sein
-        }
+    private void check() {
+        assertThat("touchedOrderCount != 0", touchedOrderCount, equalTo(0));
+        assertThat(maxTouchedOrderCount, lessThanOrEqualTo(maxOrders));
+        assertThat(maxTouchedOrderCount, equalTo(maxOrders));   // Strenger, das Maximum soll genutzt worden sein
     }
 }
