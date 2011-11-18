6b76adb9b3e66966e7beac41552823b4f3293728
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-18 11:50:26 +0000

    Template für ein Java-Testprojket mit Scheduler-Anbindung
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15678 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ test-template/src/test/java/com/sos/scheduler/engine/test/events/OrderFinishedPlugin.java
@@ -0,0 +1,89 @@
+package com.sos.scheduler.engine.test.events;
+
+import static com.sos.scheduler.engine.kernel.util.XmlUtils.stringXPath;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+
+import com.sos.scheduler.engine.kernel.Scheduler;
+import com.sos.scheduler.engine.kernel.SchedulerException;
+import com.sos.scheduler.engine.kernel.event.AbstractEventPlugin;
+import com.sos.scheduler.engine.eventbus.Event;
+import com.sos.scheduler.engine.kernel.order.OrderEvent;
+import com.sos.scheduler.engine.kernel.order.OrderFinishedEvent;
+import com.sos.scheduler.engine.kernel.plugin.Plugin;
+import com.sos.scheduler.engine.kernel.plugin.PluginFactory;
+
+/**
+ * \file OrderFinishedPlugin.java 
+ * \brief Simple plugin to display all Order finished
+ * 
+ * \class OrderFinishedPlugin
+ * \brief Simple plugin to display all Order finished
+ * 
+ * \details
+ * \code 
+   \endcode
+ * 
+ * \version 1.0 - 18.08.2011 16:32:06
+ * <div class="sos_branding">
+ * <p>(c) 2011 SOS GmbH - Berlin (<a style='color:silver' href='http://www.sos-berlin.com'>http://www.sos-berlin.com</a>)</p>
+ * </div>
+ */
+
+/**
+ * The base class AbstractEventPlugin subscribes and unsubscribes the events of the JobScheduler.
+ * Additionaly the interface EventSubscriber was implemented. It expects the method onEvent (see below).
+ */
+public class OrderFinishedPlugin extends AbstractEventPlugin {
+	
+	private static Logger logger = Logger.getLogger(OrderFinishedPlugin.class);
+
+    /**
+     * The constructor of an event plugin always gets the instance of the Scheduler and a DOM element containing
+     * the configuration of the plugin.
+     * For test purposes some of the informations of the plugin configuration was read. To do this the class
+     * com.sos.scheduler.engine.kernel.util.XmlUtils contains some static methods such as stringXPath or
+     * elementXPath.
+     */
+	OrderFinishedPlugin(Scheduler scheduler, Element plugInElement) {
+		super(scheduler, plugInElement);
+		String server = stringXPath(plugInElement,	"myconfig/myserver/@host", "<unknown>");
+		String port = stringXPath(plugInElement, "myconfig/myserver/@port", "<unknown>");
+		logger.info("plugin configuration: server=" + server + ":" + port);
+	}
+
+	/**
+	 * The method deploys the event fired by the Scheduler
+	 */
+	@Override
+	public void onEvent(Event e) throws Exception {
+
+		try {
+			if (e instanceof OrderEvent) {
+				OrderEvent orderEvent = (OrderEvent)e;
+				if (e instanceof OrderFinishedEvent) {
+					logger.info("Order " + orderEvent.getOrder().getId() + ": " + orderEvent.getOrder().getTitle() + " finished");
+				}
+			}
+		} catch(SchedulerException ev) {
+			throw new SchedulerException(ev);
+		}
+
+	}
+	
+    /**
+     * Important!!
+     * Any plugin has to define a static method to return a PluginFactor object.
+     * This is the way to announce the plugin to the Scheduler.
+     */
+	public static PluginFactory factory() {
+		return new PluginFactory() {
+			@Override
+			public Plugin newInstance(Scheduler scheduler, Element plugInElement) {
+				return new OrderFinishedPlugin(scheduler, plugInElement);
+			}
+		};
+	}
+
+}
---
+++ test-template/src/test/java/com/sos/scheduler/engine/test/templates/PropertiesTest.java
@@ -0,0 +1,45 @@
+package com.sos.scheduler.engine.test.templates;
+
+import static org.junit.Assert.fail;
+
+import com.sos.scheduler.engine.kernel.test.SchedulerTest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * \file PropertiesTest.java
+ * \brief a simple test getting some properties of a running scheduler instance
+ *  
+ * \class PropertiesTest
+ * \brief a simple test getting some properties of a running scheduler instance
+ * 
+ * \details
+ *
+ * \code
+  \endcode
+ *
+ * \author ss
+ * \version 1.0 - 08.09.2011 11:21:33
+ * <div class="sos_branding">
+ *   <p>(c) 2011 SOS GmbH - Berlin (<a style='color:silver' href='http://www.sos-berlin.com'>http://www.sos-berlin.com</a>)</p>
+ * </div>
+ */
+public class PropertiesTest extends SchedulerTest {
+	
+    private static Logger logger = LoggerFactory.getLogger(PropertiesTest.class);
+
+    @Test public void test1() throws Exception {
+        try {
+        	logger.info("some properties of the running scheduler ...");
+            controller().runScheduler(shortTimeout);
+            logger.debug("port=" + scheduler().getTcpPort());
+            logger.debug("host=" + scheduler().getHostname());
+            logger.debug("host_complete=" + scheduler().getHostnameLong());
+//            fail("Exception expected");
+        }
+        catch (Exception x) {
+            logger.debug(x.getMessage());
+        }
+    }
+}
