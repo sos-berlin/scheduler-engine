f2301f88c33f03f0882b93beeb941431e0b9faa7
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-20 11:24:40 +0000

    kernel/MAINFEST.MF mit main() gelöscht
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15698 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/main/Main.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/main/Main.java
@@ -2,17 +2,17 @@ package com.sos.scheduler.engine.kernel.main;
 
 import com.sos.scheduler.engine.kernel.util.Time;
 
-public class Main {
+class Main {
     private final SchedulerController schedulerController = new SchedulerThreadController();
 
-    public final int apply(String[] args) {
+    private int apply(String[] args) {
         CppScheduler.loadModuleFromPath();  // TODO Methode nur provisorisch. Besser den genauen Pfad übergeben, als Kommandozeilenparameter.
         schedulerController.startScheduler(args);
         schedulerController.tryWaitForTermination(Time.eternal);
         return schedulerController.exitCode();
     }
 
-    public static void main(String[] args) throws Exception {
+    private static void main(String[] args) throws Exception {
         int exitCode = new Main().apply(args);
         if (exitCode != 0)  throw new ExitCodeException(exitCode);
     }
