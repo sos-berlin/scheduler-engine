275b1480f09929e47196bbcdfe413da8de85597e
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-11-23 11:51:37 +0000

    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15753 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel-cpp/generated/mavenSettings.h
+++ opensource/kernel-cpp/generated/mavenSettings.h
@@ -12,7 +12,7 @@
 // Die numerische Dateiversion einer Windows-Datei (bei uns VER_PRODUCTVERSION) hat vier Zahlen
 
 #define VER_PRODUCTVERSION       1,3,13
-#define VER_PRODUCTVERSION_STR  "1.3.13-beta-1a-SNAPSHOT" VER_PRODUCTVERSION_TAIL
+#define VER_PRODUCTVERSION_STR  "1.3.13-beta-2a-SNAPSHOT" VER_PRODUCTVERSION_TAIL
 
 #ifdef _DEBUG
 #   define VER_PRODUCTVERSION_DEBUG " (Debug)"
