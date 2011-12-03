9cbdd171d7f687e9e516d6bc1a10ef4b01899ae7
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-03 07:44:37 +0000

    JS653Test: Wartezeit verlängert
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15892 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js653/JS653Test.java
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js653/JS653Test.java
@@ -1,5 +1,7 @@
 package com.sos.scheduler.engine.tests.jira.js653;
 
+import static java.lang.System.currentTimeMillis;
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.fail;
 
 import java.util.HashSet;
@@ -14,15 +16,15 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.sos.scheduler.engine.eventbus.HotEventHandler;
 import com.sos.scheduler.engine.kernel.order.Order;
+import com.sos.scheduler.engine.kernel.order.OrderFinishedEvent;
 import com.sos.scheduler.engine.kernel.order.OrderId;
 import com.sos.scheduler.engine.kernel.order.OrderState;
 import com.sos.scheduler.engine.kernel.order.OrderTouchedEvent;
-import com.sos.scheduler.engine.kernel.util.Time;
 import com.sos.scheduler.engine.test.SchedulerTest;
 
 /** @see <a href='http://www.sos-berlin.com/jira/browse/JS-653'>JS-653</a> */
 public final class JS653Test extends SchedulerTest {
-    private static final Time timeout = Time.of(5);
+    private static final long idleTimeoutMs = 5*1000;
     private static final Joiner commaJoiner = Joiner.on(", ");
     private static final ImmutableSet<OrderIdAndState> expectedOrderStarts = ImmutableSet.of(
             new OrderIdAndState(new OrderId("simpleShouldRun"), new OrderState("state.job1")),
@@ -30,22 +32,32 @@ public final class JS653Test extends SchedulerTest {
             new OrderIdAndState(new OrderId("superWithStateBShouldRun"), new OrderState("state.nestedB.job1")));
 
     private final Set<OrderIdAndState> orderStarts = new HashSet<OrderIdAndState>();
+    private volatile long lastActivity;
 
     @Test public void test() throws InterruptedException {
-        controller().startScheduler();
+        controller().activateScheduler("-e");
         waitUntilOrdersAreNotStarted();
         controller().terminateScheduler();
         if (!orderStarts.equals(expectedOrderStarts))
             fail(differenceMessage());
     }
 
+    /* Warten bis wir einigermaßen sicher sind, dass Aufträge, die nicht starten sollen, nicht gestartet werden. **/
     private void waitUntilOrdersAreNotStarted() throws InterruptedException {
-        // Warten bis wir einigermaßen sicher sind, dass Aufträge, die nicht starten sollen, nicht gestartet werden.
-        Thread.sleep(timeout.getMillis());
+        lastActivity = currentTimeMillis();
+        while(true) {
+            long remaining = lastActivity + idleTimeoutMs - currentTimeMillis();
+            if (remaining < 0) break;
+            sleep(remaining);
+        }
     }
 
     @HotEventHandler public void handleEvent(OrderTouchedEvent e, Order o) {
         orderStarts.add(new OrderIdAndState(o.getId(), o.getState()));
+    }
+
+    @HotEventHandler public void handleEvent(OrderFinishedEvent e) {
+        lastActivity = currentTimeMillis();
     }
 
     private String differenceMessage() {
