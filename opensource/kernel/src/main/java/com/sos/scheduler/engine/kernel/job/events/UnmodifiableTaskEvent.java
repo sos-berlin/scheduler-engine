0de3c9a7774227968f80366b946e51318dba73cb
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-03 09:08:45 +0000

    UnmodifiableTaskEvent.getJobPath()
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15899 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/job/events/UnmodifiableTaskEvent.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/job/events/UnmodifiableTaskEvent.java
@@ -1,16 +1,24 @@
 package com.sos.scheduler.engine.kernel.job.events;
 
+import com.sos.scheduler.engine.kernel.folder.Path;
 import com.sos.scheduler.engine.kernel.job.UnmodifiableTask;
 import com.sos.scheduler.engine.kernel.scheduler.SchedulerObject;
 
 public class UnmodifiableTaskEvent extends TaskEvent {
     private final UnmodifiableTask task;
+    private final Path jobPath;
+
 
     protected UnmodifiableTaskEvent(UnmodifiableTask task) {
         this.task = task;
+        jobPath = task.getJob().getPath();
     }
 
     @Override public final SchedulerObject getObject() {
         return task;
+    }
+
+    public Path getJobPath() {
+        return jobPath;
     }
 }
