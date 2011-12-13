2e2579ce2043040433ae3817002ba16be8a4634a
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-13 22:36:13 +0000

    Time überarbeitet
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15995 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/util/Time.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/util/Time.java
@@ -1,7 +1,8 @@
 package com.sos.scheduler.engine.kernel.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
-
 
 public final class Time {
     public static final Time eternal = new Time(Long.MAX_VALUE, TimeUnit.DAYS);
@@ -9,33 +10,42 @@ public final class Time {
     public final long value;
     public final TimeUnit unit;
 
-
     public Time(long value, TimeUnit timeUnit) {
         this.value = value;
         this.unit = timeUnit;
     }
 
-
     public long getMillis() {
         return unit.toMillis(value);
     }
-
 
 //    public long getNanosWithoutMillis() {
 //        return unit.excessNanos(getMillis(), value);
 //    }
 
+    public final String toXml() {
+        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(new Date(getMillis()));
+    }
 
     @Override public String toString() {
         return (unit.toMillis(value) / 1000.0) + "s";
     }
 
 
-    public static Time of(long value, TimeUnit u) { return new Time(value, u); }
+    public static Time of(long value, TimeUnit u) {
+        return new Time(value, u);
+    }
 
-    public static Time of(double seconds) { return new Time((long)(seconds * 1000000000), TimeUnit.NANOSECONDS); }
+    public static Time of(double seconds) {
+        return new Time((long)(seconds * 1000000000), TimeUnit.NANOSECONDS);
+    }
 
+    public static Time of(Date o) {
+        return new Time(o.getTime(), TimeUnit.MILLISECONDS);
+    }
     //public static Time of(Date d) { return ofMillis(d.getTime()); }
 
-    public static Time ofMillis(long value) { return new Time(value, TimeUnit.MILLISECONDS); }
+    public static Time ofMillis(long value) {
+        return new Time(value, TimeUnit.MILLISECONDS);
+    }
 }
