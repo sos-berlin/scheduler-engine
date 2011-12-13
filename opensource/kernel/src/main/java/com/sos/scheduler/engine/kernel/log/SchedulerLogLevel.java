29112cdcbd440e1c524fe01c2f21f4f45e3a9f3b
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-13 22:28:23 +0000

    SchedulerLogLevel.getNumber()
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15981 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/log/SchedulerLogLevel.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/log/SchedulerLogLevel.java
@@ -1,15 +1,21 @@
 package com.sos.scheduler.engine.kernel.log;
 
 public enum SchedulerLogLevel {
-    debug("debug"),
-    info("info"),
-    warn("warn"),
-    error("error");
+    debug(-1, "debug"),
+    info(0, "info"),
+    warn(1, "warn"),
+    error(2, "error");
 
+    private final int number;
     private final String cppName;
 
-    SchedulerLogLevel(String cppName) {
+    SchedulerLogLevel(int n, String cppName) {
+        this.number = n;
         this.cppName = cppName;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     String getCppName() {
