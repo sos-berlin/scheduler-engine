f3de26b10a721218ccad1a85dd75b12731274ed5
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-13 22:06:42 +0000

    EventPipe zum Abfragen von Events im Haupt-Thread
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15978 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/EventPipe.scala
@@ -0,0 +1,45 @@
+package com.sos.scheduler.engine.test
+
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
+import com.sos.scheduler.engine.main.event.TerminatedEvent
+import com.sos.scheduler.engine.eventbus._
+import com.sos.scheduler.engine.kernel.util.Time
+
+class EventPipe extends EventHandlerAnnotated {
+    import EventPipe._
+    private final val queue: BlockingQueue[Event] = new LinkedBlockingQueue[Event]
+    @volatile private var lastEventAdded = false
+
+    @EventHandler def add(e: Event) {
+        lastEventAdded |= lastEventClass isAssignableFrom e.getClass
+        queue.add(e)
+    }
+
+    def expectEvent[E <: Event](t: Time)(predicate: E => Boolean)(implicit m: ClassManifest[E]): E = {
+        val eventClass = m.erasure
+        def className = eventClass.getSimpleName
+        while (true) {
+            val eventOption = tryPoll(t)
+            if (lastEventAdded) throw new RuntimeException("Expected Event '"+className+"' has not arrived before "+lastEventClass.getName+" has arrived")
+            eventOption match {
+                case None => throw new RuntimeException("Expected Event '"+className+"' has not arrived within "+t)
+                case Some(event: Event) if eventClass isAssignableFrom event.getClass =>
+                    val e = event.asInstanceOf[E]
+                    if (predicate(e)) return e
+                case _ =>
+            }
+        }
+        throw new RuntimeException()  // Für Intellij-Scala-Plugin 5.12.2011
+    }
+
+    def poll(t: Time): Event = tryPoll(t) getOrElse {throw new RuntimeException("Event has not arrived within "+t)}
+
+    def tryPoll(t: Time) = Option(queue.poll(t.getMillis, TimeUnit.MILLISECONDS))
+}
+
+object EventPipe {
+    private val lastEventClass = classOf[TerminatedEvent]
+}
+
