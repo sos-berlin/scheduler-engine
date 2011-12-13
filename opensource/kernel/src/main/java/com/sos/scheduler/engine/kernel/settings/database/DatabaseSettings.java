61e6675900c95d738d45fd279c2b3c6561f698b5
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-13 22:05:29 +0000

    - überflüssige Dateien gelöscht
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15974 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/settings/database/DatabaseSettings.java
+++
@@ -1,7 +0,0 @@
-package com.sos.scheduler.engine.kernel.settings.database;
-
-import javax.annotation.Nullable;
-
-public interface DatabaseSettings {
-    @Nullable String getHostwarePathOrNull();
-}
