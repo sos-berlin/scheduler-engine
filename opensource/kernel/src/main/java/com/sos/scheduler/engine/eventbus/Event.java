1ccab668ba6f784a2ed819e8780914ffcc7a2e66
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-13 22:05:49 +0000

    - Event aufgeräumt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@15975 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/eventbus/Event.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/eventbus/Event.java
@@ -2,6 +2,6 @@ package com.sos.scheduler.engine.eventbus;
 
 import com.sos.scheduler.engine.cplusplus.runtime.annotation.ForCpp;
 
-@ForCpp
 // Abstrakte Klasse statt Interface, damit C++/Java-Generator Event als Oberklasse akzeptiert.
+@ForCpp
 public interface Event {}
