5f9be0c31e1bb93cb3348a4fded30429945855c0
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-14 17:26:07 +0000

    ScalaJobTest prüft, ob alle Job-Methoden aufgerufen werden
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16011 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/spoolerapi/scala/ScalaJob.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/spoolerapi/scala/ScalaJob.scala
@@ -5,34 +5,48 @@ import sos.spooler.Job_impl
 final class ScalaJob extends Job_impl {
     private val myListIterator = Iterator("Step one", "Step two")
 
+    private lazy val logLevel = spooler_task.params.value("logLevel").toInt  // lazy, weil spooler_task verzögert gesetzt wird.
+
     override def spooler_init() = {
-        spooler_log.info("spooler_init")
+        trace("spooler_init")
         true
     }
 
     override def spooler_exit() {
-        spooler_log.info("spooler_exit")
+        trace("spooler_exit")
     }
 
     override def spooler_open() = {
-        spooler_log.info("spooler_open")
+        trace("spooler_open")
         myListIterator.hasNext
     }
 
     override def spooler_close() {
-        spooler_log.log(spooler_task.params.value("logLevel").toInt, "spooler_close")
+        trace("spooler_close")
+        spooler_log.log(logLevel, "spooler_close")
     }
 
     override def spooler_process() = {
-        spooler_log.info("spooler_process "+ myListIterator.next)
+        trace("spooler_process")
+        val a = myListIterator.next()
+        spooler_log.info(a)
         myListIterator.hasNext
     }
 
     override def spooler_on_success() {
-        spooler_log.info("spooler_on_success")
+        trace("spooler_on_success")
     }
 
     override def spooler_on_error() {
-        spooler_log.info("spooler_on_error")
+        trace("spooler_on_error")
+    }
+
+    private def trace(call: String) {
+        val v = "test."+ logLevel +"."+ call
+        val n = spooler.variables.value(v) match {
+            case "" => 0
+            case o => o.toInt
+        }
+        spooler.variables.set_value(v, (n+1).toString)
     }
 }
--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/spoolerapi/scala/ScalaJobTest.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/spoolerapi/scala/ScalaJobTest.scala
@@ -1,12 +1,16 @@
 package com.sos.scheduler.engine.tests.spoolerapi.scala
 
-import org.junit.Test
+import scala.collection.JavaConversions._
 import com.sos.scheduler.engine.kernel.folder.AbsolutePath
 import com.sos.scheduler.engine.kernel.job.events.TaskEndedEvent
 import com.sos.scheduler.engine.test.scala._
 import com.sos.scheduler.engine.test.scala.SchedulerTestImplicits._
 import com.sos.scheduler.engine.kernel.log.SchedulerLogLevel
 
+/** Prüft, ob alle Methoden eines Scala-Jobs aufgerufen werden.
+ * Der Scala-Job wird mit den Log-Levels info und error gerufen, um den Aufruf von spooler_on_success() und spooler_on_error()
+ * zu prüfen.
+ * Der Job schreibt in Spooler.variables für jeden Log-Level und jeden Aufruf eine Variable mit der Anzahl der Aufrufe.*/
 final class ScalaJobTest extends ScalaSchedulerTest {
     import ScalaJobTest._
     private val eventPipe = controller.newEventPipe()
@@ -14,7 +18,7 @@ final class ScalaJobTest extends ScalaSchedulerTest {
     controller.setTerminateOnError(false)
     controller.activateScheduler("-e")
 
-    @Test def testScalaJob() {
+    test("xx") {
         runJob(SchedulerLogLevel.info)
         runJob(SchedulerLogLevel.error)
     }
@@ -22,6 +26,7 @@ final class ScalaJobTest extends ScalaSchedulerTest {
     private def runJob(logLevel: SchedulerLogLevel) {
         scheduler.executeXml(startJobElem(logLevel))
         eventPipe.expectEvent(shortTimeout){e: TaskEndedEvent => e.getJobPath == jobPath}
+        checkSchedulerVariables(logLevel)
     }
 
     private def startJobElem(logLevel: SchedulerLogLevel) =
@@ -30,8 +35,36 @@ final class ScalaJobTest extends ScalaSchedulerTest {
                 <param name="logLevel" value={logLevel.getNumber.toString}/>
             </params>
         </start_job>
+
+    private def checkSchedulerVariables(logLevel: SchedulerLogLevel) {
+        val LevelString = logLevel.getNumber.toString
+        val result = scheduler.getVariables.toMap flatMap {_ match {
+            case (VariableNamePattern(LevelString, call), value) => Some(call -> value.toInt)
+            case _ => None
+        }}
+        assert(result === expectedCallFrequencies(logLevel))
+    }
 }
 
 object ScalaJobTest {
     private def jobPath = new AbsolutePath("/scala")
+    private val VariableNamePattern = """test[.](\d+)[.]([a-z_]+)""".r      // "test.0.spooler_process"
+
+    private val expectedCallFrequencies = Map(
+        SchedulerLogLevel.info -> Map(
+            "spooler_init" -> 1,
+            "spooler_exit" -> 1,
+            "spooler_open" -> 1,
+            "spooler_close" -> 1,
+            "spooler_process" -> 2,
+            "spooler_on_success" -> 1),
+        SchedulerLogLevel.error -> Map(
+            "spooler_init" -> 1,
+            "spooler_exit" -> 1,
+            "spooler_open" -> 1,
+            "spooler_close" -> 1,
+            "spooler_process" -> 2,
+            "spooler_on_error" -> 1))
+
+    private def v(l: SchedulerLogLevel, call: String) = "test."+ l.getNumber +"."+call
 }
