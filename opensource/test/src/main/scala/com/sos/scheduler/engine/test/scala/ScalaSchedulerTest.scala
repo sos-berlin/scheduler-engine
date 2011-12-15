150d0a8a76eeef9e4c15c38b60c7581356cb2211
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-15 13:45:04 +0000

    ScalaSchedulerTest läuft mit JUnit
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16025 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/ScalaSchedulerTest.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/ScalaSchedulerTest.scala
@@ -2,7 +2,10 @@ package com.sos.scheduler.engine.test.scala
 
 import com.sos.scheduler.engine.test.SchedulerTest
 import org.scalatest.{FunSuite, BeforeAndAfterAll}
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
 
+@RunWith(classOf[JUnitRunner])
 abstract class ScalaSchedulerTest extends SchedulerTest with FunSuite with BeforeAndAfterAll {
     def shortTimeout = SchedulerTest.shortTimeout       // Zur komfortableren Benutzung
 
