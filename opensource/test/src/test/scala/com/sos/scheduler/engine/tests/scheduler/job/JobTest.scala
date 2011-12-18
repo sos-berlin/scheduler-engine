5b5f783f7e046b15cb7eda3c6be3861283a545b8
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-18 22:20:41 +0000

    Test für Job
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16049 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/scheduler/job/JobTest.scala
@@ -0,0 +1,38 @@
+package com.sos.scheduler.engine.tests.scheduler.job
+
+import scala.collection.JavaConversions._
+import org.scalatest.matchers.ShouldMatchers._
+import com.sos.scheduler.engine.kernel.folder.AbsolutePath
+import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
+
+class JobTest extends ScalaSchedulerTest {
+  import JobTest._
+
+  private val job = scheduler.getJobSubsystem.job(jobPath)
+
+  test("job.name") {
+    assert(job.getName === "a")
+  }
+
+  test("job.path") {
+    assert(job.getPath.toString === "/a")
+  }
+
+  test("job.isFileBasedReread") {
+    assert(job.isFileBasedReread === false)
+  }
+
+  test("jobSubsystem.visibleNames") {
+    val list = scheduler.getJobSubsystem.getVisibleNames.toList
+    list.toSet should equal (Set("a", "b"))
+  }
+
+  test("jobSubsystem.names") {
+    val list = scheduler.getJobSubsystem.getNames.toList
+    list.toSet should equal (Set("scheduler_file_order_sink", "scheduler_service_forwarder", "a", "b"))
+  }
+}
+
+object JobTest {
+  private val jobPath = new AbsolutePath("/a")
+}
