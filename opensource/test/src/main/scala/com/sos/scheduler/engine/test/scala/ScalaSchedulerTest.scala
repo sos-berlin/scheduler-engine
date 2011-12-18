d3e61bd7351127a2003f574a85a9ec6f1da03df0
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2011-12-18 22:18:19 +0000

    - ScalaSchedulerTest ist trait
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16044 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/ScalaSchedulerTest.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/ScalaSchedulerTest.scala
@@ -1,16 +1,21 @@
 package com.sos.scheduler.engine.test.scala
 
-import com.sos.scheduler.engine.test.SchedulerTest
 import org.scalatest.{FunSuite, BeforeAndAfterAll}
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
+import com.sos.scheduler.engine.test.{TestSchedulerController, SchedulerTest}
 
 @RunWith(classOf[JUnitRunner])
-abstract class ScalaSchedulerTest extends SchedulerTest with FunSuite with BeforeAndAfterAll {
-    def shortTimeout = SchedulerTest.shortTimeout       // Zur komfortableren Benutzung
+trait ScalaSchedulerTest extends FunSuite with BeforeAndAfterAll {
+  val controller = TestSchedulerController.of(getClass)
 
-    override def afterAll(configMap: Map[String, Any]) {
-        try controller.close()
-        finally super.afterAll(configMap)
-    }
+  def shortTimeout = SchedulerTest.shortTimeout   // Zur komfortableren Benutzung
+
+  override def afterAll(configMap: Map[String, Any]) {
+    try controller.close()
+    finally super.afterAll(configMap)
+  }
+
+  /** Zur Bequemlichkeit; dasselbe wie {@link com.sos.scheduler.engine.test.TestSchedulerController#scheduler()}. */
+  protected final def scheduler = controller.scheduler
 }
