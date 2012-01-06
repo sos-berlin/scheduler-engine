31a14443c29b16af5081483dfe1e8f8da8d1ef18
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-06 15:36:14 +0000

    Separates Testprojekt wird nicht mehr benötigt. Tests können in das Artifakt Test integriert werden.
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16134 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- test-template/src/test/java/com/sos/scheduler/engine/test/excluded/_PackageDescription.java
+++
@@ -1,9 +0,0 @@
-package com.sos.scheduler.engine.test.excluded;
-/**
- * \package com.sos.scheduler.engine.test.excluded
- * 
- * \brief package with classes exluded from the maven test 
- * All classes contained in this package are excluded from the maven tests. You should use it for
- * developing of test cases.
- */
-
