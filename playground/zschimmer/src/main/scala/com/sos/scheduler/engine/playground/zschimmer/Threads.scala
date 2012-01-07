07012eb78aaa4da6d13f48134c537a3b18c2a3c1
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-07 20:53:02 +0000

    playground.zschimmer.Threads.untilInterrupted()
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16167 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- playground/zschimmer/src/main/scala/com/sos/scheduler/engine/playground/zschimmer/Threads.scala
+++ playground/zschimmer/src/main/scala/com/sos/scheduler/engine/playground/zschimmer/Threads.scala
@@ -1,19 +1,21 @@
 package com.sos.scheduler.engine.playground.zschimmer
 
-
 import com.sos.scheduler.engine.kernel.util.Time
 
 object Threads {
+  def untilInterruptedEvery(t: Time)(f: => Unit) {
+    val timer = new IntervalTimer(t.getMillis)
+    untilInterrupted {
+      f
+      Thread.sleep(timer.msUntilNextInterval())
+    }
+  }
+
     def untilInterrupted(f: => Unit) {
         try while (!Thread.interrupted) f
-        catch { case x: InterruptedException => }
-    }
-
-    def untilInterruptedEvery(t: Time)(f: => Unit) {
-        val timer = new IntervalTimer(t.getMillis)
-        untilInterrupted {
-            f
-            Thread.sleep(timer.msUntilNextInterval())
+        catch {
+          case x: InterruptedException =>
+          case x: RuntimeException if x.getCause.isInstanceOf[InterruptedException] =>
         }
     }
 
