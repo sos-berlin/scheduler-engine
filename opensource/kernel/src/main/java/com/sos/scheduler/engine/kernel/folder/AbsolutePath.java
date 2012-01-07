5a14248553a199750178526059efdb821a3ce759
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-07 19:46:34 +0000

    AbsolutePath.of()
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16137 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/folder/AbsolutePath.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/folder/AbsolutePath.java
@@ -10,4 +10,9 @@ public class AbsolutePath extends Path {
     public final String getName() {
         return getString().substring(getString().lastIndexOf('/') + 1);
     }
+
+    /** @param path ist absolut oder relativ zur Wurzel. */
+    public static AbsolutePath of(String path) {
+        return new AbsolutePath(path.startsWith("/")? path : "/" + path);
+    }
 }
