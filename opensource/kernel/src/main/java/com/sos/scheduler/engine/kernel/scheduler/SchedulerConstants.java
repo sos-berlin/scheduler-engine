2758246d92e7f2339df8e03b7238b6329b0671b4
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-07 20:37:36 +0000

    SchedulerConstants.schedulerEncoding = ISO_8859_1
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16141 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/scheduler/SchedulerConstants.java
@@ -0,0 +1,11 @@
+package com.sos.scheduler.engine.kernel.scheduler;
+
+import java.nio.charset.Charset;
+
+import static com.google.common.base.Charsets.ISO_8859_1;
+
+public class SchedulerConstants {
+    public static final Charset schedulerEncoding = ISO_8859_1;
+
+    private SchedulerConstants() {}
+}
