0d9601f61e615a90ffb3fa449cc59aca4cf10a41
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-07 20:55:33 +0000

    - JS655Test von Jetty-Client auf Jersey umgestellt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16172 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js655/JS655Test.java
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js655/JS655Test.java
@@ -1,24 +1,5 @@
 package com.sos.scheduler.engine.tests.jira.js655;
 
-import static com.google.common.base.Charsets.ISO_8859_1;
-import static com.sos.scheduler.engine.tests.jira.js655.JS655Test.M.jobChainActivated;
-import static com.sos.scheduler.engine.tests.jira.js655.JS655Test.M.jobChainRemoved;
-import static com.sos.scheduler.engine.tests.jira.js655.JS655Test.M.terminated;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.Charset;
-
-import org.apache.log4j.Logger;
-import org.eclipse.jetty.client.ContentExchange;
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.HttpExchange;
-import org.eclipse.jetty.io.ByteArrayBuffer;
-import org.junit.Test;
-
 import com.sos.scheduler.engine.eventbus.EventHandler;
 import com.sos.scheduler.engine.eventbus.HotEventHandler;
 import com.sos.scheduler.engine.kernel.folder.AbsolutePath;
@@ -30,23 +11,33 @@ import com.sos.scheduler.engine.kernel.util.Files;
 import com.sos.scheduler.engine.kernel.util.sync.Gate;
 import com.sos.scheduler.engine.main.event.TerminatedEvent;
 import com.sos.scheduler.engine.test.SchedulerTest;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URI;
+
+import static com.sos.scheduler.engine.kernel.util.Util.ignore;
+import static com.sos.scheduler.engine.tests.jira.js655.JS655Test.M.*;
+import static com.sun.jersey.api.client.ClientResponse.Status.INTERNAL_SERVER_ERROR;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 /** JS-655  "JobScheduler does not start when a webservice entry is assigned to a job chain coming from hot folder" */
 public class JS655Test extends SchedulerTest {
-    private static final Logger logger = Logger.getLogger(JS655Test.class);
     private static final Path initialJobChainPath = new AbsolutePath("/myJobChain");
     private static final Path rightJobChainPath = new AbsolutePath("/myLazyJobChain");
 
-    private final HttpClient httpClient = new HttpClient();
-    private final URI uri;
+    private final WebResource webResource;
     enum M { jobChainActivated, jobChainRemoved, terminated }
     private final Gate<M> gate = new Gate<M>();
 
     public JS655Test() throws Exception {
-        controller().activateScheduler();
-        uri = new URI("http://localhost:"+ scheduler().getTcpPort() +"/myService");
-        httpClient.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
-        httpClient.start();
+        controller().activateScheduler("-e");
+        URI uri = new URI("http://localhost:"+ scheduler().getTcpPort() +"/myService");
+        webResource = Client.create().resource(uri);
     }
 
     @Test public void test() throws Exception {
@@ -59,7 +50,7 @@ public class JS655Test extends SchedulerTest {
     private void testAddJobChain() throws Exception {
         renameJobChain(initialJobChainPath, rightJobChainPath);
         gate.expect(jobChainActivated, shortTimeout);
-        assertThat(webClient().post("Hello!"), equalTo("Bye!"));
+        assertThat(webResource.post(String.class, "Hello!"), equalTo("Bye!"));
     }
 
     private void testRemoveJobChain() throws Exception {
@@ -77,52 +68,25 @@ public class JS655Test extends SchedulerTest {
     }
 
     private void checkWebServiceIsNotReady() throws Exception {
-        WebClient webClient = webClient();
-        webClient.tryPost("Hej!");
-        assertThat(webClient.contentExchange.getResponseStatus(), equalTo(500));
-    }
-
-    private WebClient webClient() throws Exception {
-        return new WebClient(uri.toString());
+        ClientResponse response = webResource.post(ClientResponse.class, "Hej!");
+        assertThat(response.getClientResponseStatus(), equalTo(INTERNAL_SERVER_ERROR));
     }
 
     @HotEventHandler public void handleEvent(FileBasedActivatedEvent e, JobChain jobChain) throws InterruptedException {
+        ignore(e);
         if (jobChain.getPath().equals(rightJobChainPath))
             gate.put(jobChainActivated);
     }
 
     @HotEventHandler public void handleEvent(FileBasedRemovedEvent e, JobChain jobChain) throws InterruptedException {
+        ignore(e);
         if (jobChain.getPath().equals(rightJobChainPath))
             gate.put(jobChainRemoved);
     }
 
     @EventHandler public void handleEvent(TerminatedEvent e) throws InterruptedException {
         // Nur für Fehlerfall
+        ignore(e);
         gate.put(terminated);
-    }
-
-    private class WebClient {
-        private final Charset encoding = ISO_8859_1;
-        private final ContentExchange contentExchange = new ContentExchange();
-
-        WebClient(String url) throws Exception {
-            contentExchange.setURL(url);
-            contentExchange.setRequestContentType("text/xml;charset=" + encoding.name());
-        }
-
-        private String post(String request) throws IOException, InterruptedException {
-            String response = tryPost(request);
-            assertThat(contentExchange.getResponseStatus(), equalTo(200));
-            return response;
-        }
-
-        private String tryPost(String request) throws IOException, InterruptedException {
-            contentExchange.setMethod("POST");
-            contentExchange.setRequestContent(new ByteArrayBuffer(request, encoding.name()));
-            httpClient.send(contentExchange);
-            int sendStatus = contentExchange.waitForDone();
-            assertThat(sendStatus, equalTo(HttpExchange.STATUS_COMPLETED));
-            return contentExchange.getResponseContent();
-        }
     }
 }
