e9c092eee0bd81d7c2e4e09b027d3a422da8165e
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-07 20:47:00 +0000

    CppProxyImpl.checkIsNotReleased() erlaubt Rückgabe von null
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16159 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- cppjava/runtime/src/main/java/com/sos/scheduler/engine/cplusplus/runtime/CppProxyImpl.java
+++ cppjava/runtime/src/main/java/com/sos/scheduler/engine/cplusplus/runtime/CppProxyImpl.java
@@ -6,17 +6,14 @@ public class CppProxyImpl<SISTER extends Sister> implements CppProxyWithSister<S
     private volatile long cppReference = 0;
     private SISTER sister = null;
 
-
     protected final long cppReference() {
         return cppReference;
     }
 
-    
-    @Override public boolean cppReferenceIsValid() { 
+    @Override public boolean cppReferenceIsValid() {
         return cppReference != 0;
     }
 
-    
     /** Nur für JNI zugänglich. Wird vom C++-Destruktor ~Has_proxy aufgerufen. */
     @SuppressWarnings("unused")
     private void invalidateCppReference() {
@@ -25,11 +22,9 @@ public class CppProxyImpl<SISTER extends Sister> implements CppProxyWithSister<S
             sister.onCppProxyInvalidated();
     }
 
-
     @Override public final void setSister(SISTER o) {
         sister = o;
     }
-
 
     @Override public final SISTER getSister() {
         if (sister == null)
@@ -41,27 +36,23 @@ public class CppProxyImpl<SISTER extends Sister> implements CppProxyWithSister<S
         return sister != null;
     }
 
-
 //    Variable_setC zum Beispiel hat eine kontextfreie Schwester.
 //    protected void requireContext(Sister context) {
 //        if (context == null)  throw new RuntimeException("Missing Sister context in " + getClass().getName());
 //    }
-
 
     protected final void requireContextIsNull(Sister context) {
         if (context != null)
             throw new RuntimeException("Sister context != null, but interface has no sisterType, in " + getClass().getName());
     }
 
-
     /* Wenn C++ ein temporäres Objekt liefert und also den C++-Proxy über JNI sofort wieder freigibt,
      * dann haben wir nur ein (zerstörtes) Ding der Klasse Object. */
     protected static void checkIsNotReleased(Class<?> clas, Object o) {
-        if (!clas.isInstance(o))
+        if (o != null && !clas.isInstance(o))
             throw new CppProxyInvalidated(clas);
     }
 
-    
     private static class CppProxyInvalidated extends RuntimeException {
         private CppProxyInvalidated(Class<?> c) {
             super("C++ code has returned a temporary, immediately destructed object (was a " + c.getName()+")");
