a878f2a847fe2ac716b45540547ba638f602b249
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-07 20:54:11 +0000

    SchedulerLog: Log4j-Appender für scheduler.log verwirft Log-Zeilen, wenn Scheduler nicht mehr läuft.
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16169 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/log/SchedulerLog.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/log/SchedulerLog.java
@@ -2,17 +2,20 @@ package com.sos.scheduler.engine.kernel.log;
 
 import com.sos.scheduler.engine.kernel.cppproxy.SpoolerC;
 
-
 public class SchedulerLog {
     private final SpoolerC spoolerC;
-
 
     public SchedulerLog(SpoolerC spoolerC) {
         this.spoolerC = spoolerC;
     }
 
-    
     public final void write(LogCategory logCategory, String text) {
-        spoolerC.write_to_scheduler_log(logCategory.getString(), text);
+        try {
+            if (spoolerC.cppReferenceIsValid())
+                spoolerC.write_to_scheduler_log(logCategory.getString(), text);
+        }
+        catch (Throwable x) {
+            // Keine Log-Ausgabe, weil wir vielleicht schon von einem Logger aufgerufen worden sind.
+        }
     }
 }
