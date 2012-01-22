7b1628ee3aeaec25127820ff0bdd3b5ee5dbeb87
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-22 18:58:25 +0000

    FileBased..getUuid() liefert die UUID der Instanz
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16310 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/folder/FileBased.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/folder/FileBased.java
@@ -6,9 +6,18 @@ import com.sos.scheduler.engine.eventbus.EventSource;
 import com.sos.scheduler.engine.kernel.scheduler.AbstractHasPlatform;
 import com.sos.scheduler.engine.kernel.scheduler.Platform;
 
+import java.util.UUID;
+
 @ForCpp
 public abstract class FileBased extends AbstractHasPlatform implements Sister, EventSource {
+    private final UUID uuid = UUID.randomUUID();
+
     protected FileBased(Platform p) {
         super(p);
+    }
+
+    /** Jedes Exemplar hat seine eigene UUID. */
+    public final UUID getUuid() {
+        return uuid;
     }
 }
