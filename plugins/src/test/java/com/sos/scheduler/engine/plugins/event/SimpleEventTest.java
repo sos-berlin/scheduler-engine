80cde654fea6a3adf807c96e95ba7fcf062deeb1
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-01-25 09:32:17 +0000

    Überarbeitung der Tests für JMS & eventing
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16337 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- plugins/src/test/java/com/sos/scheduler/engine/plugins/event/SimpleEventTest.java
+++ plugins/src/test/java/com/sos/scheduler/engine/plugins/event/SimpleEventTest.java
@@ -4,23 +4,42 @@ import org.apache.log4j.Logger;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.sos.scheduler.engine.eventbus.Event;
+import com.sos.scheduler.engine.eventbus.HotEventHandler;
+import com.sos.scheduler.engine.kernel.folder.events.FileBasedActivatedEvent;
+import com.sos.scheduler.engine.kernel.job.Job;
+import com.sos.scheduler.engine.kernel.order.OrderFinishedEvent;
+import com.sos.scheduler.engine.kernel.order.UnmodifiableOrder;
+import com.sos.scheduler.engine.kernel.order.jobchain.JobChain;
 import com.sos.scheduler.engine.test.SchedulerTest;
-import com.sos.scheduler.engine.kernel.util.Time;
+import com.sos.scheduler.engine.test.util.JSCommandUtils;
 
 public class SimpleEventTest extends SchedulerTest {
 
-    private static final Time schedulerTimeout = Time.of(20);
-    private static Logger logger;
-    
-    @BeforeClass
-    public static void setUpBeforeClass () throws Exception {
-		logger = Logger.getLogger(Connector.class);
-		logger.info("Starting test of EventPlugin");
+	private final static Logger logger = Logger.getLogger(SimpleEventTest.class);
+	private final static JSCommandUtils utils = JSCommandUtils.getInstance();
+	private final static String jobChain = "EventPluginTest";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		logger.debug("Starting test of EventPlugin");
 	}
-    
-    @Test
-    public void test() throws Exception {
-        controller().runSchedulerAndTerminate(schedulerTimeout, "-e -log-level=warn");
+
+	@Test
+	public void test() throws Exception {
+		controller().activateScheduler();
+		controller().scheduler().executeXml(
+				utils.buildCommandAddOrder(jobChain,jobChain).addParam("myParam", "EventPluginTest").getCommand());
+		controller().waitForTermination(shortTimeout);
+	}
+	
+    @HotEventHandler
+    public void handleOrderEnd(OrderFinishedEvent e, UnmodifiableOrder o) throws Exception {
+    	logger.debug("ORDERFINISHED: " + o.getId().getString());
+    	if (o.getId().getString().equals(jobChain)) {
+    		Thread.sleep(3000);
+    		controller().scheduler().terminate();
+    	}
     }
-    
+
 }
