27875883f55dea6ea8f2ad495775fad004ebe454
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-02-11 13:49:17 +0000

    - OrderId aufgeräumt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16493 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/order/OrderId.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/order/OrderId.java
@@ -1,8 +1,9 @@
 package com.sos.scheduler.engine.kernel.order;
 
 import com.sos.scheduler.engine.kernel.util.StringValue;
-import com.sos.scheduler.engine.cplusplus.runtime.annotation.ForCpp;
 
 public class OrderId extends StringValue {
-    public OrderId(String x) { super(x); }
+    public OrderId(String x) {
+        super(x);
+    }
 }
