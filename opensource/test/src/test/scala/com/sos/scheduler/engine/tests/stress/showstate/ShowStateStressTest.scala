532a42cc814f6659060bf89ab5074a32180379d8
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-02-12 17:10:36 +0000

    ShowStateStressTest: @Ingore, weill Portnummer schon vergeben sein kann.
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16502 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/stress/showstate/ShowStateStressTest.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/stress/showstate/ShowStateStressTest.scala
@@ -2,72 +2,73 @@ package com.sos.scheduler.engine.tests.stress.showstate
 
 import java.io._
 import java.net._
-import java.nio.charset.Charset
-import org.junit._
+import com.google.common.base.Charsets
 import com.sos.scheduler.engine.cplusplus.scalautil.io.Util._
 import com.sos.scheduler.engine.test.SchedulerTest
+import org.junit._
 
 final class ShowStateStressTest extends SchedulerTest {
-    import ShowStateStressTest._
-    
-    @Test def test1() {
-        controller.startScheduler("-e", "-log-level=warn")
-        closingFinally(new Connection(new InetSocketAddress("localhost", scheduler.getTcpPort))) { connection =>
-            1 to 1000 foreach { i => connection.sendAndReceive(emptyCommand) }
-        }
-        controller.terminateScheduler()
+  import ShowStateStressTest._
+
+  @Ignore //TODO Test kann versagen, wenn Portnummer in scheduler.xml schon vergegeben ist.
+  @Test def test1() {
+    controller.startScheduler("-e", "-log-level=warn")
+    closingFinally(new Connection(new InetSocketAddress("localhost", scheduler.getTcpPort))) { connection =>
+        for (i <- 1 to 1000) connection.sendAndReceive(emptyCommand)
     }
+    controller.terminateScheduler()
+  }
 }
 
 object ShowStateStressTest {
-    private def emptyCommand = (  //<params/>.toString
-        <commands>
-            <subsystem.show what="statistics"/>
-            <show_state what="folders cluster remote_schedulers schedules" subsystems="lock schedule process_class folder"/>
-        </commands>
-    ).toString()
-    
-    def main(args: Array[String]) {
-        def inetSocketAddress(a: String) = {
-            val b = a split ':'
-            new InetSocketAddress(b(0), b(1).toInt)
-        }
+  private def emptyCommand = (//<params/>.toString
+      <commands>
+          <subsystem.show what="statistics"/>
+          <show_state what="folders cluster remote_schedulers schedules" subsystems="lock schedule process_class folder"/>
+      </commands>
+      ).toString()
 
-        val connection = new Connection(inetSocketAddress(args(0)))
-        1 to Int.MaxValue foreach { _ => connection.sendAndReceive(emptyCommand) }
+  def main(args: Array[String]) {
+    def inetSocketAddress(a: String) = {
+      val b = a split ':'
+      new InetSocketAddress(b(0), b(1).toInt)
     }
 
-    private class Connection(val address: InetSocketAddress) {
-        private def encoding = Charset.forName("UTF-8")     //TODO Statt XML.toString besser XML direkt nach OutputStream schreiben lassen
-        private val socket = new Socket
-        socket.connect(address)
-        private val writer = new OutputStreamWriter(socket.getOutputStream, encoding)
-        private val reader = new SplitReader(new InputStreamReader(socket.getInputStream), '\0')
+    val connection = new Connection(inetSocketAddress(args(0)))
+    for (i <- 1 to Int.MaxValue) connection.sendAndReceive(emptyCommand)
+  }
 
-        def close() {
-            socket.close()
-        }
+  private class Connection(val address: InetSocketAddress) {
+    private def encoding = Charsets.UTF_8
+    private val socket = new Socket
+    socket.connect(address)
+    private val writer = new OutputStreamWriter(socket.getOutputStream, encoding)
+    private val reader = new SplitReader(new InputStreamReader(socket.getInputStream), '\0')
 
-        def sendAndReceive(s: String): String = {
-            writer.write(s)
-            writer.flush()
-            reader.read()
-        }
+    def close() {
+      socket.close()
     }
 
-    private class SplitReader(reader: Reader, separator: Char) extends Closeable {
-        def close() {
-          reader.close()
-        }
-
-        def read(): String = {
-            val result = new StringBuilder
-            var c = reader.read()
-            while (c != -1  &&  c.toChar != separator) {
-                result.append(c.toChar)
-                c = reader.read()
-            }
-            result.toString()
-        }
+    def sendAndReceive(s: String): String = {
+      writer.write(s)
+      writer.flush()
+      reader.read()
     }
+  }
+
+  private class SplitReader(reader: Reader, separator: Char) extends Closeable {
+    def close() {
+      reader.close()
+    }
+
+    def read(): String = {
+      val result = new StringBuilder
+      var c = reader.read()
+      while (c != -1 && c.toChar != separator) {
+        result.append(c.toChar)
+        c = reader.read()
+      }
+      result.toString()
+    }
+  }
 }
