9530a778e5fc2ead3c40eab6fd855a398eda585d
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-02-17 09:03:43 +0000

    AbsolutePath.of(parent,sub)
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16536 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/folder/AbsolutePath.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/folder/AbsolutePath.java
@@ -1,6 +1,5 @@
 package com.sos.scheduler.engine.kernel.folder;
 
-
 public class AbsolutePath extends Path {
     public AbsolutePath(String p) {
         super(p);
@@ -11,8 +10,25 @@ public class AbsolutePath extends Path {
         return getString().substring(getString().lastIndexOf('/') + 1);
     }
 
+    public final String withTrailingSlash() {
+        String result = getString();
+        return result.endsWith("/")? result : result +"/";
+    }
+
     /** @param path ist absolut oder relativ zur Wurzel. */
     public static AbsolutePath of(String path) {
         return new AbsolutePath(path.startsWith("/")? path : "/" + path);
+    }
+
+    public static AbsolutePath of(AbsolutePath parentPath, String subpath) {
+        StringBuilder a = new StringBuilder();
+        a.append(stripTrailingSlash(parentPath.getString()));
+        a.append('/');
+        a.append(subpath);
+        return of(a.toString());
+    }
+
+    private static String stripTrailingSlash(String a) {
+        return a.endsWith("/")? a.substring(0, a.length() - 1) : a;
     }
 }
