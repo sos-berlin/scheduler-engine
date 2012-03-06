10ad2ce1acd71e4965165880b3b6ed64214fb3cf
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-03-06 07:31:53 +0000

    gpl.plugins heisst jetzt opensource.plugins
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16746 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/gpl.plugins/jetty/src/main/scala/com/sos/scheduler/engine/plugins/jetty/rest/marshal/Marshaller.scala
+++
@@ -1,9 +0,0 @@
-package com.sos.scheduler.engine.plugins.jetty.rest.marshal
-
-import org.codehaus.jackson.JsonGenerator
-
-trait Marshaller[A] {
-  val objectClass: Class[A]
-
-  def marshal(j: JsonGenerator, a: A)
-}
