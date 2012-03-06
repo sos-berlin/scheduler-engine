2aaaaaa970fa5359ceb876a4b995f882f8afa29b
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-03-06 03:54:09 +0000

    jetty plugin ausgelagert
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16734 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/gpl.plugins/jetty/src/main/java/com/sos/scheduler/engine/plugins/jms/stress/TaskInfo.java
+++
@@ -1,8 +0,0 @@
-package com.sos.scheduler.engine.plugins.jms.stress;
-
-public interface TaskInfo extends TaskInfoListener {
-	public int currentlyRunningTasks();
-	public int endedTasks();
-	public int highwaterTasks();
-	public int estimatedTasks();
-}
--- opensource/gpl.plugins/jetty/src/main/java/com/sos/scheduler/engine/plugins/jms/stress/TaskInfoListener.java
+++
@@ -1,5 +0,0 @@
-package com.sos.scheduler.engine.plugins.jms.stress;
-
-public interface TaskInfoListener {
-	public void onInterval(TaskInfo info);
-}
