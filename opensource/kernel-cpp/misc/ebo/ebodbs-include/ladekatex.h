e036f03a097a5129f85a01196111bc8cd051aeb9
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-03-15 05:41:51 +0000

    Hostware-Verzeichnis misc gelöscht
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16807 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel-cpp/misc/ebo/ebodbs-include/ladekatex.h
+++
@@ -1,8 +0,0 @@
-#ifndef LADEKATEX_INCL
-
-EXT DLL COUNT eroeffne_katalog(LONG *no_entrys, TEXT *katname);
-EXT DLL COUNT lade_katalog(TEXT *,LONG,LONG *);
-#define LADEKATEX_INCL
-
-#endif
-
--- opensource/kernel-cpp/misc/ebo/ebodbs-include/leatoolsex.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef LEATOOLSEX_INCL
-
-EXT DLL VOID print_record(COUNT, TEXT *);
-
-#define LEATOOLSEX_INCL
-
-#endif /* LEATOOLSEX_INCL */
-
