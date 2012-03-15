ae3c6eec3bcf00056c8e8feb737a7deb9f176965
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-03-15 05:37:10 +0000

    DatabaseQueryPluginTest überarbeitet
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16805 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- plugins/src/test/java/com/sos/scheduler/engine/plugins/databasequery/DatabaseQueryPluginTest.java
+++ plugins/src/test/java/com/sos/scheduler/engine/plugins/databasequery/DatabaseQueryPluginTest.java
@@ -1,22 +1,16 @@
 package com.sos.scheduler.engine.plugins.databasequery;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-
-import org.apache.log4j.Logger;
-import org.junit.Test;
-
 import com.sos.scheduler.engine.eventbus.EventHandler;
 import com.sos.scheduler.engine.kernel.job.events.TaskEndedEvent;
 import com.sos.scheduler.engine.kernel.util.Time;
-import com.sos.scheduler.engine.kernel.util.sync.Gate;
 import com.sos.scheduler.engine.test.SchedulerTest;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 public final class DatabaseQueryPluginTest extends SchedulerTest {
-    private static final Logger logger = Logger.getLogger(DatabaseQueryPluginTest.class);
-    
     private final Time timeout = Time.of(20);
-    private final Gate<Boolean> gate = new Gate<Boolean>();
 
     public DatabaseQueryPluginTest() throws Exception {
         controller().useDatabase();
@@ -24,32 +18,26 @@ public final class DatabaseQueryPluginTest extends SchedulerTest {
         controller().activateScheduler();
     }
 
-    @Test
-    public void testShowTaskHistory() throws Exception {
-        waitForTaskTermination();
-        String result = execute("<showTaskHistory/>");
-        assertThat(result, containsString("</myResult>"));
-        assertThat(result, containsString("<row "));
-        assertThat(result, containsString(" job="));
-        controller().terminateScheduler();
+    @Test public void testShowTaskHistory() throws Exception {
+        controller().waitForTermination(timeout);
     }
 
-    private void waitForTaskTermination() throws Exception {
-    	controller().waitUntilSchedulerIsActive();
-        gate.expect(true, timeout);
+    @EventHandler public void handleEvent(TaskEndedEvent e) {
+        try {
+            String result = execute("<showTaskHistory/>");
+            assertThat(result, containsString("</myResult>"));
+            assertThat(result, containsString("<row "));
+            assertThat(result, containsString(" job="));
+        } finally {
+            controller().terminateScheduler();
+        }
     }
 
-    @EventHandler public void handleEvent(TaskEndedEvent e) throws InterruptedException {
-        gate.put(true);
-    }
-
-    private String execute(String subcommandXml) throws Exception {
+    private String execute(String subcommandXml) {
         String commandXml =
                 "<plugin.command plugin_class='" + DatabaseQueryPlugin.class.getName() + "'>" +
                     subcommandXml +
                 "</plugin.command>";
-        String result = scheduler().executeXml(commandXml);
-        logger.debug(result);
-        return result;
+        return scheduler().executeXml(commandXml);
     }
 }
