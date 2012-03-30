a48c3f68c3547887b1ab844676c73a5ce163087c
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-03-30 16:35:52 +0000

    Event mit Jackson serialisierbar
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16898 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/eventbus/Event.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/eventbus/Event.java
@@ -1,7 +1,9 @@
 package com.sos.scheduler.engine.eventbus;
 
 import com.sos.scheduler.engine.cplusplus.runtime.annotation.ForCpp;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 
 // Abstrakte Klasse statt Interface, damit C++/Java-Generator Event als Oberklasse akzeptiert.
 @ForCpp
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "TYPE")
 public interface Event {}
