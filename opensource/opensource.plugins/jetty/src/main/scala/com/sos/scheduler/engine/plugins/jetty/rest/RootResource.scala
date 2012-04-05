5efc941c7b9de5c9717a7170124e48ac6b0558a5
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-04-05 16:51:06 +0000

    - RootResource aufgeräumt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@16955 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/opensource.plugins/jetty/src/main/scala/com/sos/scheduler/engine/plugins/jetty/rest/RootResource.scala
+++ opensource/opensource.plugins/jetty/src/main/scala/com/sos/scheduler/engine/plugins/jetty/rest/RootResource.scala
@@ -3,7 +3,6 @@ package com.sos.scheduler.engine.plugins.jetty.rest
 import javax.inject.Inject
 import javax.ws.rs._
 import com.sos.scheduler.engine.kernel.scheduler.SchedulerInstanceId
-import com.sos.scheduler.engine.plugins.jetty.rest.annotations.HtmlXsltResource
 import javax.ws.rs.core._
 
 @Path("")
@@ -12,7 +11,6 @@ class RootResource @Inject()(schedulerInstanceId: SchedulerInstanceId) {
 
   @GET
   @Produces(Array(MediaType.APPLICATION_JSON))
-  @HtmlXsltResource(path="com/sos/scheduler/engine/plugins/jetty/rest/RootResource.xsl")
   def get(@Context u: UriInfo) = {
     val result = RootView(u.getBaseUri)
     Response.ok(result).tag(tag).build()
