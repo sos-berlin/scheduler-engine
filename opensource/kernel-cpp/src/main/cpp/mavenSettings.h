2bd4b6a8a919e65cdb045af123fb15a00a56987e
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-04-26 15:31:50 +0000

    Formatierung des Zeitstempels für snapshot builds
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17021 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel-cpp/src/main/cpp/mavenSettings.h
+++ opensource/kernel-cpp/src/main/cpp/mavenSettings.h
@@ -8,11 +8,12 @@
 */
 
 // Maven-Plugin build-helper-maven-plugin wird benötigt für die Maven-Variablen parsedVersion.xxx, siehe pom.xml.
+// Die property "timestamp" ist das Ergebnis aus dem buildnumber-maven-plugin
 
 // Die numerische Dateiversion einer Windows-Datei (bei uns VER_PRODUCTVERSION) hat vier Zahlen
 
 #define VER_PRODUCTVERSION       ${parsedVersion.majorVersion},${parsedVersion.minorVersion},${parsedVersion.incrementalVersion}
-#define VER_PRODUCTVERSION_STR  "${project.version}-${maven.build.timestamp}" VER_PRODUCTVERSION_TAIL
+#define VER_PRODUCTVERSION_STR  "${project.version}-${timestamp}" VER_PRODUCTVERSION_TAIL
 
 #ifdef _DEBUG
 #   define VER_PRODUCTVERSION_DEBUG " (Debug)"
