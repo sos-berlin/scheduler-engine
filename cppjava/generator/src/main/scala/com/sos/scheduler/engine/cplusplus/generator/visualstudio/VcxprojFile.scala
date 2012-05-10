a4af7c49621407e46f8427c1497f3351d3c4a157
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-05-10 21:11:03 +0000

    C++/Java-Generator erzeugt besser mergebare .vcxproj
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17106 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/visualstudio/VcxprojFile.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/visualstudio/VcxprojFile.scala
@@ -13,107 +13,115 @@ import scala.collection.JavaConversions._
 
 
 class VcxprojFile(vcxprojFile: File, newIncludes: List[String]) {
-    import VcxprojFile._
+  import VcxprojFile._
 
-    private val saxBuilder = new SAXBuilder
-    private val xmlOutputter = new XMLOutputter
+  private val saxBuilder = new SAXBuilder
+  private val xmlOutputter = new XMLOutputter
 
-    val updatedDocs = {
-        val vcxprojFilterDoc = new VcxprojFilterDoc(new File(vcxprojFile.getPath + ".filters"))
-        val originalIncludes = vcxprojFilterDoc.originalIncludes
+  val updatedDocs = {
+    val vcxprojFilterDoc = new VcxprojFilterDoc(new File(vcxprojFile.getPath + ".filters"))
+    val originalIncludes = vcxprojFilterDoc.originalIncludes
 
-        if (newIncludes.toSet == originalIncludes.toSet)
-            List()
-        else
-            List(new VcxprojDoc(vcxprojFile), vcxprojFilterDoc) map { doc =>
-                val addedIncludes = newIncludes diff originalIncludes
-                doc.removeIncludes((originalIncludes diff newIncludes) ++ addedIncludes)
-                doc.addIncludes(addedIncludes)
-                doc
-            }
+    if (newIncludes.toSet == originalIncludes.toSet)
+      List()
+    else
+      List(new VcxprojDoc(vcxprojFile), vcxprojFilterDoc) map { doc =>
+        val addedIncludes = newIncludes diff originalIncludes
+        doc.removeIncludes((originalIncludes diff newIncludes) ++ addedIncludes)
+        doc.addIncludes(addedIncludes)
+        doc
+      }
+  }
+
+  def store() {
+    updatedDocs foreach { _.store() }
+  }
+
+  abstract class Doc(val file: File) {
+    val document = saxBuilder.build(file)
+    protected def projectElement = document.getRootElement
+    protected def namespace = projectElement.getNamespace
+    protected val clCompileElements = MyXPath("x:ItemGroup/x:ClCompile").selectElements(projectElement)
+
+    private val cppItemGroup = {    // Unter <Project> sind mehrere <ItemGroup>. Wir nehmen die erste, die eine .cxx-Datei enthält: <ItemGroup><ClCompile Include="...cxx"/>
+      val endings = cppFileExtensions map { "." + _ }
+      def hasCppEnding(s: String) = endings exists s.endsWith
+      def includeHasCppEnding(e: Element) = hasCppEnding(e.getAttributeValue("Include"))
+      val cppClCompile = clCompileElements find includeHasCppEnding getOrElse {
+        throw new RuntimeException("Visual Project file '" + file + "' contains no filename ending with " + endings.mkString(" "))
+      }
+      cppClCompile.getParentElement ensuring { _.getName == "ItemGroup" }
     }
+
+    def removeIncludes(includes: Seq[String]) {
+      def toBeRemoved(clCompile: Element) = includes contains includeAttributeString(clCompile)
+      clCompileElements filter toBeRemoved foreach { e => e.getParent.removeContent(e) }
+    }
+
+    def addIncludes(includes: Seq[String]) {
+      includes foreach { include =>
+        cppItemGroup.addContent(newClCompileElement(include))
+        cppItemGroup.addContent("\n    ")
+      }
+    }
+
+    protected def newClCompileElement(include: String) = new Element("ClCompile", namespace).setAttribute("Include", include)
+
+    def toXml = xmlOutputter.outputString(document)
 
     def store() {
-        updatedDocs foreach { _.store }
+      defaultPrinter.println(file)
+      closingFinally(new FileOutputStream(file)) { output => xmlOutputter.output(document, output) }
+    }
+  }
+
+  final class VcxprojDoc(file: File) extends Doc(file)
+
+  final class VcxprojFilterDoc(file: File) extends Doc(file) {
+    val filterFullName = {
+      MyXPath("x:ItemGroup/x:ClCompile/x:Filter")
+      .selectElements(projectElement) find { _.getValue endsWith visualStudioFilterName } match {
+        case Some(filter) => filter.getText     // Example: """Quelldateien\Generated C++/Java-Proxies"""
+        case None => visualStudioFilterName
+      }
     }
 
-    abstract class Doc(val file: File) {
-        val document = saxBuilder.build(file)
-        protected def projectElement = document.getRootElement
-        protected def namespace = projectElement.getNamespace
-        protected val clCompileElements = MyXPath("x:ItemGroup/x:ClCompile").selectElements(projectElement)
-
-        private val cppItemGroup = {    // Unter <Project> sind mehrere <ItemGroup>. Wir nehmen die erste, die eine .cxx-Datei enthält: <ItemGroup><ClCompile Include="...cxx"/>
-            val endings = cppFileExtensions map { "." + _ }
-            def hasCppEnding(s: String) = endings exists s.endsWith
-            def includeHasCppEnding(e: Element) = hasCppEnding(e.getAttributeValue("Include"))
-            val cppClCompile = clCompileElements find includeHasCppEnding getOrElse {
-                throw new RuntimeException("Visual Project file '" + file + "' contains no filename ending with " + endings.mkString(" "))
-            }
-            cppClCompile.getParentElement ensuring { _.getName == "ItemGroup" }
-        }
-
-        def removeIncludes(includes: Seq[String]) {
-            def toBeRemoved(clCompile: Element) = includes contains includeAttributeString(clCompile)
-            clCompileElements filter toBeRemoved foreach { e => e.getParent.removeContent(e) }
-        }
-
-        def addIncludes(includes: Seq[String]) {
-            includes foreach { include => cppItemGroup.addContent(newClCompileElement(include)) }
-        }
-
-        protected def newClCompileElement(include: String) = new Element("ClCompile", namespace).setAttribute("Include", include)
-
-        def toXml = xmlOutputter.outputString(document)
-
-        def store() {
-            defaultPrinter.println(file)
-            closingFinally(new FileOutputStream(file)) { output => xmlOutputter.output(document, output) }
-        }
+    val originalIncludes: Seq[String] = {
+      val filterElements = MyXPath("x:ItemGroup/x:ClCompile/x:Filter").selectElements(projectElement)
+      def isOurFilter(filter: Element) = filter.getValue endsWith visualStudioFilterName
+      (filterElements filter isOurFilter map { e => includeAttributeString(e.getParentElement) }).distinct
     }
 
-    class VcxprojDoc(file: File) extends Doc(file) 
-
-    class VcxprojFilterDoc(file: File) extends Doc(file) {
-        val filterFullName = {
-            MyXPath("x:ItemGroup/x:ClCompile/x:Filter")
-            .selectElements(projectElement) find { _.getValue() endsWith visualStudioFilterName } match {
-                case Some(filter) => filter.getText     // Example: """Quelldateien\Generated C++/Java-Proxies"""
-                case None => visualStudioFilterName
-            }
-        }
-
-        val originalIncludes: Seq[String] = {
-            val filterElements = MyXPath("x:ItemGroup/x:ClCompile/x:Filter").selectElements(projectElement)
-            def isOurFilter(filter: Element) = filter.getValue endsWith visualStudioFilterName
-            filterElements filter isOurFilter map { e => includeAttributeString(e.getParentElement) } distinct
-        }
-
-        override def newClCompileElement(include: String) = super.newClCompileElement(include)
-            .addContent(new Element("Filter", namespace).addContent(filterFullName))
+    override def newClCompileElement(include: String) = {
+      val filter = new Element("Filter", namespace).addContent(filterFullName)
+      super.newClCompileElement(include)
+          .addContent("\n      ")
+          .addContent(filter)
+          .addContent("\n    ")
     }
+  }
 }
 
 
 object VcxprojFile {
-    def update(dir: File, modules: Seq[CppModule]) {
-        val projectFilename = dir.getName + ".vcxproj"   // "scheduler/scheduler.vcxproj"
-        val projectFile = new File(dir, projectFilename)
-        if (projectFile.exists)
-            new VcxprojFile(projectFile, modules map { _.sourceCodeFile.path } toList).store()
-    }
+  def update(dir: File, modules: Seq[CppModule]) {
+    val projectFilename = dir.getName + ".vcxproj"   // "scheduler/scheduler.vcxproj"
+    val projectFile = new File(dir, projectFilename)
+    if (projectFile.exists)
+      new VcxprojFile(projectFile, modules map { _.sourceCodeFile.path } toList).store()
+  }
 
-    private def includeAttributeString(e: Element) = e.getAttributeValue("Include") match {
-        case o: String => o
-        case null => throw new RuntimeException("<ClCompile> without attribute Include=")
-    }
-    
-    private case class MyXPath(expr: String) {
-        val xPath = XPath.newInstance(expr)
-        xPath.addNamespace(Namespace.getNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003"))
+  private def includeAttributeString(e: Element) = e.getAttributeValue("Include") match {
+    case o: String => o
+    case null => throw new RuntimeException("<ClCompile> without attribute Include=")
+  }
 
-        def selectNodes(node: Content): Seq[Content] = xPath.selectNodes(node).asInstanceOf[java.util.List[Content]]
+  private case class MyXPath(expr: String) {
+    val xPath = XPath.newInstance(expr)
+    xPath.addNamespace(Namespace.getNamespace("x", "http://schemas.microsoft.com/developer/msbuild/2003"))
 
-        def selectElements(node: Content) = selectNodes(node) collect { case e: Element => e }
-    }
+    def selectNodes(node: Content): Seq[Content] = xPath.selectNodes(node).asInstanceOf[java.util.List[Content]]
+
+    def selectElements(node: Content) = selectNodes(node) collect { case e: Element => e }
+  }
 }
