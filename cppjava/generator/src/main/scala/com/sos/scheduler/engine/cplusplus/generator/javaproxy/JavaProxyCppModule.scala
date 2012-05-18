64bfac421081383286db9b6cdd5a41b22e743d3b
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-05-18 12:30:08 +0000

    C++/Java-Generator: #ifndef umfasst ganze generierte .h-Datei
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17183 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/javaproxy/JavaProxyCppModule.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/javaproxy/JavaProxyCppModule.scala
@@ -1,39 +1,35 @@
 package com.sos.scheduler.engine.cplusplus.generator.javaproxy
 
-import com.sos.scheduler.engine.cplusplus.generator.module.CppModule
 import com.sos.scheduler.engine.cplusplus.generator.cpp.Cpp._
 import com.sos.scheduler.engine.cplusplus.generator.javaproxy.clas._
+import com.sos.scheduler.engine.cplusplus.generator.module.CppModule
 import JavaProxyCppModule._
 
 /** Generator für C++-Code eines Java-Proxys, also der in C++ zu nutzenden Java-Klassen. */
 class JavaProxyCppModule(cppClass: CppClass, knownClasses: Set[Class[_]], includeHeader: String) extends CppModule {
-    val name = fileBasenameOfClass(cppClass.javaClass)
+  val name = fileBasenameOfClass(cppClass.javaClass)
 
-    lazy val headerCodeOption = Some(
-        includeHeader +
-        headerOnce(cppClass.headerPreprocessorMacro)(
-            knownClassHeaderIncludes(cppClass.javaSuperclasses) +
-            "\n" +
-            cppClass.neededForwardDeclarations +
-            "\n\n" +
-            cppClass.headerCode
-        )
-    )
+  lazy val headerCodeOption = Some(headerOnce(cppClass.headerPreprocessorMacro)(
+    includeHeader +
+      knownClassHeaderIncludes(cppClass.javaSuperclasses) +
+    "\n" +
+    cppClass.neededForwardDeclarations +
+    "\n\n" +
+    cppClass.headerCode))
 
-    lazy val sourceCodeOption = Some(
-        includeHeader +
-        includeQuoted(headerCodeFile.path) +
-        knownClassHeaderIncludes(cppClass.directlyUsedJavaClasses) +
-        "\n" +
-        cppClass.sourceCode
-    )
-    
-    private def knownClassHeaderIncludes(classes: Set[Class[_]]) = 
-        (classes intersect knownClasses map includeClassHeader).toSeq.sorted.mkString
+  lazy val sourceCodeOption = Some(
+    includeHeader +
+    includeQuoted(headerCodeFile.path) +
+    knownClassHeaderIncludes(cppClass.directlyUsedJavaClasses) +
+    "\n" +
+    cppClass.sourceCode)
+
+  private def knownClassHeaderIncludes(classes: Set[Class[_]]) =
+    (classes intersect knownClasses map includeClassHeader).toSeq.sorted.mkString
 }
 
 
 object JavaProxyCppModule {
-    private def fileBasenameOfClass(clas: Class[_]) = clas.getName.replace(".", "__")
-    private def includeClassHeader(clas: Class[_]) = includeQuoted(fileBasenameOfClass(clas) + ".h")
+  private def fileBasenameOfClass(clas: Class[_]) = clas.getName.replace(".", "__")
+  private def includeClassHeader(clas: Class[_]) = includeQuoted(fileBasenameOfClass(clas) + ".h")
 }
--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/javaproxy/PrecompiledHeaderModule.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/javaproxy/PrecompiledHeaderModule.scala
@@ -3,18 +3,11 @@ package com.sos.scheduler.engine.cplusplus.generator.javaproxy
 import com.sos.scheduler.engine.cplusplus.generator.Configuration._
 import com.sos.scheduler.engine.cplusplus.generator.cpp.Cpp._
 import com.sos.scheduler.engine.cplusplus.generator.module._
-import com.sos.scheduler.engine.cplusplus.generator.util.Util._
-import com.sos.scheduler.engine.cplusplus.scalautil.io.FileUtil._
 import java.io.File
-import scala.collection.mutable
-
 
 class PrecompiledHeaderModule(outputDirectory: File) extends CppModule {
-    val name = precompiledModuleName
-
-    lazy val headerCodeOption = Some((cppStandardIncludes map includeQuoted).mkString)
-
-    lazy val includeHeader = includeQuoted(headerCodeFile.path) + "\n"
-    
-    lazy val sourceCodeOption = Some(includeHeader)
+  val name = precompiledModuleName
+  lazy val headerCodeOption = Some((cppStandardIncludes map includeQuoted).mkString)
+  lazy val includeHeader = includeQuoted(headerCodeFile.path) + "\n"
+  lazy val sourceCodeOption = Some(includeHeader)
 }
