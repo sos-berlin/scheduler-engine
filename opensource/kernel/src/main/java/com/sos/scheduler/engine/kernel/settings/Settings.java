6c6f7290a7760e2af71d4d8614a5a29e178b757b
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-05-22 08:00:06 +0000

    - Settings aufgeräumt
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17222 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/settings/Settings.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/settings/Settings.java
@@ -1,24 +1,24 @@
 package com.sos.scheduler.engine.kernel.settings;
 
-import static com.google.common.collect.Maps.newHashMap;
+import com.sos.scheduler.engine.kernel.cppproxy.SettingsC;
 
 import java.util.Map;
 
-import com.sos.scheduler.engine.kernel.cppproxy.SettingsC;
+import static com.google.common.collect.Maps.newHashMap;
 
 public class Settings {
     private final Map<SettingName,String> values = newHashMap();
 
-    public void set(SettingName name, String value) {
+    public final void set(SettingName name, String value) {
         values.put(name, value);
     }
 
-    public void setSettingsInCpp(SettingsC cppProxy) {
+    public final void setSettingsInCpp(SettingsC cppProxy) {
         for (Map.Entry<SettingName,String> e: values.entrySet())
             cppProxy.set(e.getKey().getNumber(), e.getValue());
     }
 
-    public void setAll(Settings o) {
+    public final void setAll(Settings o) {
         values.putAll(o.values);
     }
 
