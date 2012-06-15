2de88dce15f472b11dbe23854972606d12a1db3c
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-06-15 14:16:56 +0000

    Test für reset einer Order mit <modify_order ... action="reset" />
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17358 c11b8382-0eeb-0310-9acf-d3b3cacd313b

---
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/order/reset/OrderResetTest.java
@@ -0,0 +1,48 @@
+/*
+ * <div class="sos_branding">
+ *   <p>(c) 2012 SOS GmbH - Berlin (<a style='color:silver' href='http://www.sos-berlin.com'>http://www.sos-berlin.com</a>)</p>
+ * </div>
+ */
+
+package com.sos.scheduler.engine.tests.order.reset;
+
+import com.sos.scheduler.engine.data.order.OrderResumedEvent;
+import com.sos.scheduler.engine.data.order.OrderSuspendedEvent;
+import com.sos.scheduler.engine.eventbus.EventHandler;
+import com.sos.scheduler.engine.test.SchedulerTest;
+import com.sos.scheduler.engine.test.util.CommandBuilder;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class OrderResetTest extends SchedulerTest {
+
+	private final String JOB_CHAIN = "chain";
+
+	@SuppressWarnings("unused")
+	private static Logger logger = Logger.getLogger(OrderResetTest.class);
+    
+    private int touchedCount = 0;
+
+	@Test
+	public void test() throws Exception {
+        controller().setTerminateOnError(false);
+		controller().activateScheduler();
+        CommandBuilder cmd = new CommandBuilder();
+        cmd.addOrder("chain","test");
+		scheduler().executeXml(cmd.getCommand());
+        controller().waitForTermination(shortTimeout);
+	}
+
+    @EventHandler
+    public void handleSuspend(OrderSuspendedEvent e) throws InterruptedException {
+        // reset the order via modify_order must trigger an OrderResumeEvent
+        String cmd = "<modify_order job_chain='/chain' order='test' action='reset'/>";
+        scheduler().executeXml(cmd);
+    }
+
+    @EventHandler
+    public void handleResume(OrderResumedEvent e) throws InterruptedException {
+        controller().terminateScheduler();
+    }
+
+}
