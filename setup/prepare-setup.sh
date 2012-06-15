3af9a16875118c480d8f186fb76b326d55e85486
ss <ss@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-06-15 07:36:11 +0000

    Versionsnummer entfällt als Parameter Der Aufruf bezieht sich immer auf die Version des Setup Artifaktes, d.h. für unterschiedliche release Versionen müssen die Setup Zweige dieser Versionen ausgeführt werden.
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17348 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- setup/prepare-setup.sh
+++ setup/prepare-setup.sh
@@ -3,16 +3,11 @@
 # /brief Prepare the resources of the scheduler kernel for setup
 # /details
 # This script is for downloading and preparing the necessary resources of the scheduler kernel from archiva.
+# The version conforms always to the given version of the setup Artifakt.
 #
-# in a windows environment you can use 'bash -c "./prepare-setup.sh <version>"' if cygwin is installed.
+# in a windows environment you can use 'bash -c "./prepare-setup.sh"' if cygwin is installed.
 # ---------------------------------------------------------------------------------------------------------------
-if [ $# != 1 ]
-then
-  echo "usage: prepare-setup.sh <version>"
-  exit 99
-fi
-
-version=$1
 mvn clean
-mvn install -Pprepare -Dengine.version=$version -Dengine.platform=windows-x86
-mvn install -Pprepare -Dengine.version=$version -Dengine.platform=linux-x86
+mvn install -U -Pprepare-platform -Dengine.platform=windows-x86
+mvn install -U -Pprepare-platform -Dengine.platform=linux-x86
+
