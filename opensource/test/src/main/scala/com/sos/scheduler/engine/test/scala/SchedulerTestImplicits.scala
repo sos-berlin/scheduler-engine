f014ec2d3c9b3583c9b2b8b4514aa58f8c56b2b3
jz <jz@c11b8382-0eeb-0310-9acf-d3b3cacd313b>
2012-07-06 13:27:54 +0000

    SchedulerTestImplicits.instance[] zum Zugriff auf Injektor
    git-svn-id: http://subversion.sos/svn/sos/scheduler/engine@17552 c11b8382-0eeb-0310-9acf-d3b3cacd313b

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/SchedulerTestImplicits.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/SchedulerTestImplicits.scala
@@ -4,7 +4,10 @@ import scala.xml.Elem
 import com.sos.scheduler.engine.kernel.Scheduler
 
 object SchedulerTestImplicits {
-    implicit def toRichScheduler(scheduler: Scheduler) = new {
-        def executeXml(e: Elem): Elem = xml.XML.loadString(scheduler.executeXml(e.toString()))
-    }
+  implicit def toRichScheduler(scheduler: Scheduler) = new ScaledScheduler(scheduler)
+
+  class ScaledScheduler(scheduler: Scheduler) {
+    def executeXml(e: Elem): Elem = xml.XML.loadString(scheduler.executeXml(e.toString()))
+    def instance[A](implicit m: Manifest[A]): A = scheduler.getInjector.getInstance(m.erasure).asInstanceOf[A]
+  }
 }
