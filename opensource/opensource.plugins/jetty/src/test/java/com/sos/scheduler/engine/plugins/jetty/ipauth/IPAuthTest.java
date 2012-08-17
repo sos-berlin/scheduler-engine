edaa31c484de91a52638b7bb5bc1972338c2b961
Stefan Schaedlich <stefan.schaedlich@sos-berlin.com>
2012-08-17 16:03:12 +0200

    Änderungen im Test

--- opensource/opensource.plugins/jetty/src/test/java/com/sos/scheduler/engine/plugins/jetty/ipauth/IPAuthTest.java
+++ opensource/opensource.plugins/jetty/src/test/java/com/sos/scheduler/engine/plugins/jetty/ipauth/IPAuthTest.java
@@ -1,15 +1,21 @@
 package com.sos.scheduler.engine.plugins.jetty.ipauth;
 
+import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
+import com.google.common.io.Files;
 import com.sos.scheduler.engine.plugins.jetty.Config;
 import com.sos.scheduler.engine.test.SchedulerTest;
+import com.sos.scheduler.engine.test.util.Sockets;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.URI;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -21,32 +27,53 @@ public class IPAuthTest extends SchedulerTest {
 	private static Logger logger = Logger.getLogger(IPAuthTest.class);
 
     private static final String xmlCommand = "<show_state />";
-    //TODO jetty portnummer ist fest eingestellt - mit Sockets.findAvailable port einen variablen port verwenden. Der gleiche Port
-    // muss in jetty.xml eingestellt sein,
-    private static final String tcpPort = "44441";
+    private static final int tcpPort = Sockets.findAvailablePort();
+
     private static final String ipAllowed = "127.0.0.1";
 
     //TODO die verwendete IpForbidden ist nicht allgemeingültig
-    private static final String ipForbidden = "192.11.0.79";
+    //private static final String ipForbidden = "192.11.0.79";
+    private static String ipForbidden = null;
+
 
 
     @Test
-    public void testAllowed() throws Exception {
+    public void test() throws Exception {
+        controller().prepare();
+        prepareEnvironment();
         controller().activateScheduler();
+        ipForbidden = InetAddress.getLocalHost().getHostAddress();
+        logger.debug("the external IP is " + ipForbidden);
         assertThat(doHttpRequest(ipAllowed).getClientResponseStatus(), equalTo(ClientResponse.Status.OK));
         assertThat(doHttpRequest(ipForbidden).getClientResponseStatus(), equalTo(ClientResponse.Status.FORBIDDEN));
         controller().terminateScheduler();
     }
 
-    private ClientResponse doHttpRequest(String server) throws Exception {
-        URI uri = new URI("http://" + server + ":"+ tcpPort + Config.contextPath() + Config.cppPrefixPath());
-        WebResource webResource = Client.create().resource(uri);
+    private ClientResponse doHttpRequest(String clientIp) throws Exception {
+        URI uri = new URI("http://localhost:"+ tcpPort + Config.contextPath() + Config.cppPrefixPath());
+        //TODO wie kann man dem Client sagen, mit welcher IP er sich am Server anmelden soll?
+        Client c = Client.create();
+        WebResource webResource = c.resource(uri);
         ClientResponse response = webResource.post(ClientResponse.class, xmlCommand);
         logger.info("response for " + uri.toASCIIString() + "/" + xmlCommand + ": " + response.getClientResponseStatus());
+        c.destroy();
         // logResult(response);
         return response;
     }
 
+    private void prepareEnvironment() throws Exception {
+        File tempDir = controller().environment().configDirectory();
+        prepareAndWriteJettyXml(tempDir);
+    }
+
+    private void prepareAndWriteJettyXml(File tempDir) throws IOException {
+        String sourceFilename = this.getClass().getResource("jetty.xml.template").toExternalForm().replace("file:/", "");
+        String targetFilename = tempDir.getAbsolutePath() + "/jetty.xml";
+        String content = Files.toString(new File(sourceFilename), Charsets.UTF_8);
+        String newContent = content.replace("${tcp.port}", String.valueOf(tcpPort));
+        Files.write(newContent, new File(targetFilename), Charsets.UTF_8);
+        logger.debug("file " + targetFilename + " created");
+    }
 
     private void logResult(ClientResponse r) throws Exception {
         String s = CharStreams.toString((new InputStreamReader(r.getEntityInputStream(), "UTF-8")));
