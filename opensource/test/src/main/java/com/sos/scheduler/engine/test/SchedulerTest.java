fff68259cc47b2ad36059aafb196576dccd14c5e
Stefan Schaedlich <stefan.schaedlich.git@sos-berlin.com>
2012-08-29 08:41:32 +0200

    Refaktorierung von test utilities

--- opensource/test/src/main/java/com/sos/scheduler/engine/test/SchedulerTest.java
+++ opensource/test/src/main/java/com/sos/scheduler/engine/test/SchedulerTest.java
@@ -1,12 +1,15 @@
 package com.sos.scheduler.engine.test;
 
+import com.google.common.io.Files;
 import com.sos.scheduler.engine.eventbus.EventHandlerAnnotated;
 import com.sos.scheduler.engine.kernel.Scheduler;
+import com.sos.scheduler.engine.kernel.scheduler.SchedulerException;
 import com.sos.scheduler.engine.kernel.util.Time;
 import org.junit.After;
 import org.junit.Before;
 
 import java.io.File;
+import java.io.IOException;
 
 public abstract class SchedulerTest implements EventHandlerAnnotated {
     public static final Time shortTimeout = TestSchedulerController.shortTimeout;
@@ -41,6 +44,12 @@ public abstract class SchedulerTest implements EventHandlerAnnotated {
 
     public final File getTempFile(Class<?> classInstance, String fileWithoutPath) {
         File resultDir = new File(controller.environment().directory().getAbsolutePath(),classInstance.getName());
+        File resultFile = new File(resultDir,fileWithoutPath);
+        try {
+            Files.createParentDirs(resultFile);
+        } catch (IOException e) {
+            throw new SchedulerException("error creating directory " + resultDir.getAbsolutePath());
+        }
         return new File(resultDir,fileWithoutPath);
     }
 
