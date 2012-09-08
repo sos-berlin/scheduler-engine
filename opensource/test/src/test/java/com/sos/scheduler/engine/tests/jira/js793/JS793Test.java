18c85c52e5245c0bab235a0b4ce08dbb2efe963d
Joacim Zschimmer <jogit@zschimmer.com>
2012-09-08 16:49:29 +0200

    - aufgeräumt

--- opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js793/JS793Test.java
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js793/JS793Test.java
@@ -3,16 +3,16 @@ package com.sos.scheduler.engine.tests.jira.js793;
 import com.google.common.io.Files;
 import com.sos.scheduler.engine.data.order.OrderFinishedEvent;
 import com.sos.scheduler.engine.eventbus.HotEventHandler;
-import com.sos.scheduler.engine.kernel.util.OperatingSystem;
 import com.sos.scheduler.engine.test.SchedulerTest;
-import org.apache.log4j.Logger;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
+import static com.sos.scheduler.engine.kernel.util.OperatingSystem.isWindows;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -29,81 +29,66 @@ import static org.junit.Assert.assertTrue;
  * 
  * This is feature is only available for shell jobs.
  */
-public class JS793Test extends SchedulerTest {
+public final class JS793Test extends SchedulerTest {
 
-	private static final Logger logger = Logger.getLogger(JS793Test.class);
+    private static final Logger logger = LoggerFactory.getLogger(JS793Test.class);
 
-	private File resultfile;
+    private File resultfile;
 
-	@BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        logger.debug("starting test for " + JS793Test.class.getName());
-	}
-	
-	@Test
-	public void myTest() throws InterruptedException, IOException {
-		controller().startScheduler();
-		if (OperatingSystem.isWindows) {
-			doJobchain("windows_node_parameter");
-		} else {
-			doJobchain("unix_node_parameter");
-		}
+    @Test
+    public void myTest() {
+        controller().startScheduler();
+        doJobchain(isWindows? "windows_node_parameter" :"unix_node_parameter");
         controller().tryWaitForTermination(shortTimeout);
-	}
-	
-	private void doJobchain(String jobchainName) throws IOException {
-		resultfile = prepareResultfile(jobchainName);
-		startOrder(jobchainName);
-	}
-	
-	private void startOrder(String jobchainName) throws IOException {
+    }
 
-		/*
-		 * hier sollte eine Order im code erzeugt werden, leider ist nicht klar, wie man die Order dazu bringt,
-		 * das sie gestartet wird.
-		JobChain jobchain = controller().scheduler().getOrderSubsystem().jobChain(new AbsolutePath("/node_parameter"));
-		Order o = jobchain.order( new OrderId("test_node_parameter") );
-		o.getParameters().put("", "");
-		 */
-		
-		
-		String command = 
-		"<add_order id='test_" + jobchainName + "' job_chain='" + jobchainName + "'>" +
-		"<params>" + 
-		"<param name='RESULT_FILE'    value='" + getResultfile(jobchainName).getAbsolutePath() + "' />" + 
-		"<param name='100/NODE_PARAM' value='param_state_100' />" + 
-		"<param name='200/NODE_PARAM' value='param_state_200' />" + 
-		"</params>" + 
-		"</add_order>";
-		controller().scheduler().executeXml(command);
-	}
-	
-	private final File prepareResultfile(String jobchainName) throws IOException {
-		File f = getResultfile(jobchainName);
-		// if (f.exists()) f.delete();
-		logger.debug("results of the jobs will be written in file " + f.getAbsolutePath());
-		return f;
-	}
-	
-	private File getResultfile(String jobchainName) throws IOException {
-		return getTempFile(JS793Test.class, "result_" + jobchainName + ".txt");
-	}
-		
-	@HotEventHandler
-	public void handleOrderEnd(OrderFinishedEvent e) throws IOException {
+    private void doJobchain(String jobchainName) {
+        resultfile = prepareResultfile(jobchainName);
+        startOrder(jobchainName);
+    }
 
-		String lines = Files.toString(resultfile, Charset.defaultCharset());
-		logger.debug("resultfile is " + resultfile.getName() + "\n"+ lines);
-		assertParameter(lines, "JOB_RESULT", "param_state_100" );
-		assertParameter(lines, "JOB_RESULT", "param_state_200" );
-		
-		controller().terminateScheduler();
-		if (resultfile.exists()) resultfile.delete();
-	}
-	
-	private void assertParameter(String content, String paramName, String expectedValue) {
-		assertTrue(paramName + "=" + expectedValue + " expected.",content.contains(paramName + "=" + expectedValue));
-	}
-	
+    private void startOrder(String jobchainName) {
+        /*
+         * hier sollte eine Order im code erzeugt werden, leider ist nicht klar, wie man die Order dazu bringt,
+         * das sie gestartet wird.
+        JobChain jobchain = controller().scheduler().getOrderSubsystem().jobChain(new AbsolutePath("/node_parameter"));
+        Order o = jobchain.order( new OrderId("test_node_parameter") );
+        o.getParameters().put("", "");
+         */
+        String command =
+            "<add_order id='test_" + jobchainName + "' job_chain='" + jobchainName + "'>" +
+            "<params>" +
+            "<param name='RESULT_FILE'    value='" + getResultfile(jobchainName).getAbsolutePath() + "' />" +
+            "<param name='100/NODE_PARAM' value='param_state_100' />" +
+            "<param name='200/NODE_PARAM' value='param_state_200' />" +
+            "</params>" +
+            "</add_order>";
+        controller().scheduler().executeXml(command);
+    }
 
+    private File prepareResultfile(String jobchainName) {
+        File f = getResultfile(jobchainName);
+        // if (f.exists()) f.delete();
+        logger.debug("Results of the jobs will be written in file " + f.getAbsolutePath());
+        return f;
+    }
+
+    private File getResultfile(String jobchainName) {
+        return getTempFile(JS793Test.class, "result_" + jobchainName + ".txt");
+    }
+
+    @HotEventHandler
+    public void handleOrderEnd(OrderFinishedEvent e) throws IOException {
+        String lines = Files.toString(resultfile, Charset.defaultCharset());
+        logger.debug("Resultfile is " + resultfile.getName() + "\n"+ lines);
+        assertParameter(lines, "JOB_RESULT", "param_state_100" );
+        assertParameter(lines, "JOB_RESULT", "param_state_200" );
+
+        controller().terminateScheduler();
+        resultfile.delete();
+    }
+
+    private void assertParameter(String content, String paramName, String expectedValue) {
+        assertTrue(paramName + "=" + expectedValue + " expected", content.contains(paramName + "=" + expectedValue));
+    }
 }
