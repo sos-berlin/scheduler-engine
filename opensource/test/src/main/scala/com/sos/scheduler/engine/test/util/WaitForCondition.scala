7741da6f7a2244b587a6e1f1a868cda07d8d1b0f
Joacim Zschimmer <j@zschimmer.com>
2012-09-08 21:24:24 +0200

    SpoolerProcessAfterTest: Logging eingebaut, um herauszufinden, warum auf manchen Maschinen job.state == stopping statt stopped.

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/util/WaitForCondition.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/util/WaitForCondition.scala
@@ -6,26 +6,40 @@ import org.joda.time.{ReadableInstant, Duration}
 
 object WaitForCondition {
 
-  /** Wartet längstens die Dauer d in Schritten von step, bis condition wahr wird.
-    * condition wird bei d > 0 wenigsten zweimal aufgerufen: am Anfang und am Ende.
+  /** Wartet längstens t.timeout in Schritten von t.step, bis condition wahr wird.
+    * condition wird bei t.timeout > 0 wenigsten zweimal aufgerufen: am Anfang und am Ende.
     * @return letztes Ergebnis von condition */
   def waitForCondition(t: TimeoutWithSteps)(condition: => Boolean) =
     waitAtInstantsFor(t.toMillisInstantIterator(now()))(condition)
 
-  /** Wartet bis zu den Zeitpunkten, bis condition wahr wird.
-    * condition wird am Anfang und am Ende aufgerufen.
-    * @return letztes Ergebnis von condition */
-  def waitAtInstantsFor(instances: TraversableOnce[Long])(condition: => Boolean) =
-    condition || (millisFromNowUntilInstanceIterator(instances) exists { w => sleep(w); condition })
-
-  def millisFromNowUntilInstanceIterator(instances: TraversableOnce[Long]) =
-    instances.toIterator map { _ - now().getMillis } filter { _ > 0 }     // toIterator führt dazu, das now() lazy erst bei next() oder hasNext aufgerufen wird.
-
+  /** Eine in Schritte unterteilte Frist.
+    */
   final case class TimeoutWithSteps(timeout: Duration, step: Duration) {
+    /** Liefert einen Iterator mit den Zeitpunkten startInstant, startInstant + step, ..., startInstant + timeout.
+      */
     def toMillisInstantIterator(startInstant: ReadableInstant) =
       millisInstantIterator(startInstant.getMillis, timeout.getMillis, step.getMillis)
   }
 
+  /** Liefert einen Iterator mit den Zeitpunkten startInstant, startInstant + step, ..., startInstant + timeout.
+    */
   private[util] def millisInstantIterator(startInstant: Long, timeout: Long, step: Long): Iterator[Long] =
-    ((step to timeout - 1 by step).toIterator map { o => startInstant + o }) ++ Iterator(startInstant + timeout)
+    ((0L to timeout - 1 by step).toIterator map { startInstant + _ }) ++ Iterator(startInstant + timeout)
+
+  /** Wartet bis zu den Zeitpunkten, bis condition wahr wird.
+    * condition wird am Anfang und am Ende geprüft.
+    * @return letztes Ergebnis von condition */
+  def waitAtInstantsFor(instants: TraversableOnce[Long])(condition: => Boolean) =
+    realTimeIterator(instants) exists { _ => condition }
+
+  /** Ein Iterator, der bei next() (oder hasNext) auf den nächsten Zeitpunkt wartet.
+    * Wenn aufeinanderfolgende Zeitpunkte schon erreicht sind, kehrt der Iterator trotzdem jedesmal zurück.
+    * Dass kann zu überflüssigen Aktionen des Aufrufers führen (aufeinanderfolgende Prüfung einer aufwändigen Bedingung). */
+  def realTimeIterator(instants: TraversableOnce[Long]): Iterator[Unit] =
+    instants.toIterator map sleepUntil    // toIterator führt dazu, das now() erst bei next() oder hasNext lazy aufgerufen wird.
+
+  private def sleepUntil(until: Long) {
+    val w = until - now().getMillis
+    if (w > 0) sleep(w)
+  }
 }
--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/order/monitor/spoolerprocessafter/SpoolerProcessAfterTest.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/order/monitor/spoolerprocessafter/SpoolerProcessAfterTest.scala
@@ -17,6 +17,7 @@ import org.joda.time.Duration.millis
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import scala.collection.mutable
+import org.slf4j.LoggerFactory
 
 @RunWith(classOf[JUnitRunner])
 final class SpoolerProcessAfterTest extends ScalaSchedulerTest {
@@ -58,15 +59,20 @@ final class SpoolerProcessAfterTest extends ScalaSchedulerTest {
       job.endTasks()   // Task kann schon beendet und Job schon gestoppt sein.
       eventPipe.next[TaskClosedEvent] match {
         case e => assert(e.getId === new TaskId(index), "TaskClosedEvent not for expected task - probably a previous test failed") }
-      waitForCondition(TimeoutWithSteps(millis(3000), millis(10))) { job.state == expected.jobState }   // Der Job-Zustand wird asynchron geändert (stopping -> stopped, running -> pending). Wir warten kurz darauf.
+      waitForCondition(TimeoutWithSteps(millis(3000), millis(10))) {
+        val jobState = job.state
+        val result = jobState == expected.jobState   // Der Job-Zustand wird asynchron geändert (stopping -> stopped, running -> pending). Wir warten kurz darauf.
+        logger.info("*) job.state="+ jobState +" expected.jobState="+ expected.jobState +" => "+ result)  // waitForCondition() scheint auf manchen Maschinen nicht zu greifen?
+        result
+      }
     }
 
     def checkAssertions(event: MyFinishedEvent) {
       assert(event.orderKey === setting.orderKey)
       assert(expected.orderStateExpectation matches event.state, "Expected OrderState="+expected.orderStateExpectation+", but was "+event.state)
       assert(event.spoolerProcessAfterParameterOption === expected.spoolerProcessAfterParameterOption, "Parameter for spooler_process_after(): ")
-      assert(job.state === expected.jobState, "Job.state is not as expected: ")
       assert(messageCodes.toMap === expected.messageCodes.toMap)
+      assert(job.state === expected.jobState, "Job.state is not as expected: ")
     }
 
     private def cleanUpAfterTest() {
@@ -101,7 +107,7 @@ final class SpoolerProcessAfterTest extends ScalaSchedulerTest {
 }
 
 private object SpoolerProcessAfterTest {
-  //private val logger = LoggerFactory.getLogger(classOf[SpoolerProcessAfterTest])
+  val logger = LoggerFactory.getLogger(classOf[SpoolerProcessAfterTest])
 
   class MyMutableMultiMap[A,B] extends mutable.HashMap[A, mutable.Set[B]] with mutable.MultiMap[A, B]
 
