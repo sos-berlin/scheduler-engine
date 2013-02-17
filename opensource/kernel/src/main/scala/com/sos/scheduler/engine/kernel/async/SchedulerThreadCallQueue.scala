9d0e28f610dd53e471a52e44b8f989c3a1d02dc8
Joacim Zschimmer <jogit@zschimmer.com>
2013-02-17 21:47:35 +0100

    JS-947: SchedulerThreadCallQueue: Race condition entschärft

--- opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/async/SchedulerThreadCallQueue.scala
+++ opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/async/SchedulerThreadCallQueue.scala
@@ -4,21 +4,20 @@ import com.sos.scheduler.engine.common.async.{PoppableCallQueue, CallQueue, Time
 import com.sos.scheduler.engine.kernel.cppproxy.SpoolerC
 import org.slf4j.LoggerFactory
 import SchedulerThreadCallQueue._
+import com.sos.scheduler.engine.cplusplus.runtime.CppProxyInvalidatedException
 
 class SchedulerThreadCallQueue(val delegate: PoppableCallQueue, cppProxy: SpoolerC, val thread: Thread) extends CallQueue {
   def add(o: TimedCall[_]) = {
     if (logger.isDebugEnabled) logger.debug(s"Enqueue $o")
     delegate.add(o)
-    wakeCppScheduler()
+    try cppProxy.signal("TimedCall")
+    catch { case e: CppProxyInvalidatedException => }   // Das passiert, wenn der TimedCall den Scheduler beendet und er beim signal() schon beendet ist.
   }
 
   def tryRemove(o: TimedCall[_]) = delegate.tryRemove(o)
 
   def nextTime = delegate.nextTime
 
-  private def wakeCppScheduler() {
-    cppProxy.signal("TimedCall")
-  }
 }
 
 object SchedulerThreadCallQueue {
