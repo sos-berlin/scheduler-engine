e281e88b03629e02f0356b0b6448e4906f3ba78d
Joacim Zschimmer <jogit@zschimmer.com>
2013-03-14 11:35:35 +0100

    - JS-946: SchedulerThreadCallQueue protokolliert mehr

--- opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/async/SchedulerThreadCallQueue.scala
+++ opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/async/SchedulerThreadCallQueue.scala
@@ -2,13 +2,13 @@ package com.sos.scheduler.engine.kernel.async
 
 import com.sos.scheduler.engine.common.async.{PoppableCallQueue, CallQueue, TimedCall}
 import com.sos.scheduler.engine.kernel.cppproxy.SpoolerC
-import org.slf4j.LoggerFactory
 import SchedulerThreadCallQueue._
 import com.sos.scheduler.engine.cplusplus.runtime.CppProxyInvalidatedException
+import com.sos.scheduler.engine.common.scalautil.Logger
 
 class SchedulerThreadCallQueue(val delegate: PoppableCallQueue, cppProxy: SpoolerC, val thread: Thread) extends CallQueue {
   def add(o: TimedCall[_]) = {
-    if (logger.isDebugEnabled) logger.debug(s"Enqueue $o")
+    logger debug s"Enqueue $o"
     delegate.add(o)
     try cppProxy.signal("TimedCall")
     catch { case e: CppProxyInvalidatedException => }   // Das passiert, wenn der TimedCall den Scheduler beendet und er beim signal() schon beendet ist.
@@ -17,9 +17,8 @@ class SchedulerThreadCallQueue(val delegate: PoppableCallQueue, cppProxy: Spoole
   def tryRemove(o: TimedCall[_]) = delegate.tryRemove(o)
 
   def nextTime = delegate.nextTime
-
 }
 
 object SchedulerThreadCallQueue {
-  private val logger = LoggerFactory.getLogger(classOf[SchedulerThreadCallQueue])
+  private val logger = Logger(classOf[SchedulerThreadCallQueue])
 }
