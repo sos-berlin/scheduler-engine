84b1892a5a8903a9e91a6ad62a1a70abaaad6573
Joacim Zschimmer <jogit@zschimmer.com>
2013-03-17 12:23:30 +0100

    JS-946: ManyJobsIT

--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/scheduler/job/manyjobs/ManyJobsIT.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/scheduler/job/manyjobs/ManyJobsIT.scala
@@ -5,43 +5,53 @@ import com.sos.scheduler.engine.common.time.ScalaJoda.{DurationRichInt, sleep}
 import com.sos.scheduler.engine.data.folder.{FileBasedActivatedEvent, JobPath}
 import com.sos.scheduler.engine.data.job.TaskStartedEvent
 import com.sos.scheduler.engine.eventbus.EventHandler
+import com.sos.scheduler.engine.test.binary.CppBinariesDebugMode
 import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
 import com.sos.scheduler.engine.test.scala.SchedulerTestImplicits._
 import com.sos.scheduler.engine.test.util.time.WaitForCondition.waitForCondition
-import org.joda.time.Duration.standardSeconds
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.ShouldMatchers._
 import scala.collection.mutable
+import scala.math._
+import scala.sys.error
+import scala.util.Try
 
 @RunWith(classOf[JUnitRunner])
 class ManyJobsIT extends ScalaSchedulerTest {
 
-  //override val binariesDebugMode = CppBinariesDebugMode.release
+  override val binariesDebugMode = if (n > 0) Some(CppBinariesDebugMode.release) else None
   private var activatedJobCount = 0
   private var taskCount = 0
   private lazy val jobs = 1 to n map JobDefinition
   private lazy val jobStatistics = mutable.HashMap[JobPath, JobStatistics]() ++ (jobs map { j => j.path -> JobStatistics() })
 
-  ignore(s"Adding $n jobs") {
+  override def checkedBeforeAll() {
+    controller.useDatabase()    // Damit die History-Dateien nicht die File-handles aufbrauchen.
+    controller.setLogCategories("java.stackTrace-")
+  }
+
+  test(s"Adding $n jobs") {
     for (j <- jobs) scheduler executeXml j.xmlElem
     waitForCondition(timeout=n*100.ms, step=100.ms) { activatedJobCount == n }
   }
 
-  val duration = 4 * interval
-  val expectedTaskCount = (duration.getStandardSeconds * tasksPerSecond).toInt
+  val duration = 30.s //millis(min(30000, (1000 * n / tasksPerSecond).toInt)) //4 * interval
+  val expectedTaskCount = (duration.getMillis * tasksPerSecond / 1000).toInt
 
-  ignore(s"$expectedTaskCount tasks should run in ${duration.getStandardSeconds}s ($tasksPerSecond tasks/s)") {
-    scheduler executeXml <commands>{
-      jobs map { o =>
-          <modify_job job={o.path.string} cmd="enable"/>
-          <modify_job job={o.path.string} cmd="unstop"/>
-      }
-    }</commands>
-    sleep(duration)
-    controller.getEventBus.dispatchEvents()
-    taskCount should be (expectedTaskCount plusOrMinus (0.2 * expectedTaskCount).toInt)
-    //(for (j <- jobStatistics) j.taskCount should equal (tasksPer)
+  test(s"$expectedTaskCount tasks should run in ${duration.getMillis}ms ($tasksPerSecond tasks/s)") {
+    if (n == 0) pending
+    else {
+      scheduler executeXmls (
+            jobs map { o =>
+              <modify_job job={o.path.string} cmd="enable"/>
+              <modify_job job={o.path.string} cmd="unstop"/>
+            })
+      sleep(duration)
+      controller.getEventBus.dispatchEvents()
+      System.err.println(s"$taskCount in ${duration.getStandardSeconds}s, ${1000 * taskCount.toDouble / duration.getMillis} tasks/s")
+      //taskCount should be (expectedTaskCount plusOrMinus (0.2 * expectedTaskCount).toInt)
+      //(for (j <- jobStatistics) j.taskCount should equal (tasksPer)
+    }
   }
 
   @EventHandler def handle(e: FileBasedActivatedEvent) {
@@ -55,20 +65,30 @@ class ManyJobsIT extends ScalaSchedulerTest {
 }
 
 private object ManyJobsIT {
-  private val n = 50
-  private val tasksPerSecond = 2.0
-  private val interval = standardSeconds((n / tasksPerSecond).toInt)
+  private val propertyName = "ManyJobsIT"
+  private val minimumTasks = 10
+
+  private lazy val n = {
+    val v = System.getProperty(propertyName, "0")
+    Try(v.toInt) getOrElse error(s"System property $propertyName=$v not a number") match {
+      case 0 => 0
+      case o => max(o, minimumTasks)
+    }
+  }
+  private val tasksPerSecond = 40.0
+  //private lazy val interval = standardSeconds((n / tasksPerSecond).toInt)
 
   case class JobDefinition(name: Int) {
     def path = JobPath.of(s"/a-$name")
 
     def xmlElem =
       <job name={path.getName} enabled="false">
-        <script java_class="com.sos.scheduler.engine.test.jobs.SingleStepJob"/>
-        <run_time>
-          <period absolute_repeat={interval.getStandardSeconds.toString}/>
-        </run_time>
+        <script language="shell">exit 0</script>
+        <run_time repeat="1"/>
       </job>
+//          <run_time>
+//            <period absolute_repeat={interval.getStandardSeconds.toString}/>
+//          </run_time>
   }
 
   case class JobStatistics(var taskCount: Int = 0)
