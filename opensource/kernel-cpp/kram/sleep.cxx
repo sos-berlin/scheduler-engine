fe6c2f8cc21ad8136e76306615dc11f9aa2d0d3e
Joacim Zschimmer <jogit@zschimmer.com>
2013-03-21 23:37:48 +0100

    Neue Log-Kategorie "sleep"

--- opensource/kernel-cpp/kram/sleep.cxx
+++ opensource/kernel-cpp/kram/sleep.cxx
@@ -3,46 +3,34 @@
 //#define AUTHOR      "Joacim Zschimmer"
 
 #include "precomp.h"
-
 #include <math.h>
-
 #include "../kram/sysdep.h"
 
 #if defined SYSTEM_UNIX
 #   include <unistd.h>
 #endif
+#if defined SYSTEM_WIN32
+#   include <windows.h>
+#endif
 
 #include "../kram/sos.h"
 #include "../kram/sleep.h"
 
-
-#if defined SYSTEM_WIN32
-
-#include <windows.h>
-
 using namespace std;
 namespace sos {
 
 void sos_sleep( double seconds )
 {
-    Sleep( (uint4)ceil( seconds * 1000 ) );
+    Z_LOG2("sleep", seconds << "s\n");
+
+    #if defined SYSTEM_WIN32
+        Sleep( (uint4)ceil( seconds * 1000 ) );
+    #else
+        struct timespec t;
+        t.tv_sec = (time_t)floor( seconds );
+        t.tv_nsec = (time_t)max( 0.0, ( seconds - floor( seconds ) ) * 1e9 );
+        nanosleep( &t, NULL );
+    #endif
 }
 
 } //namespace sos
-
-#else
-
-using namespace std;
-namespace sos {
-
-void sos_sleep( double seconds )
-{
-    struct timespec t;
-    t.tv_sec = (time_t)floor( seconds );
-    t.tv_nsec = (time_t)max( 0.0, ( seconds - floor( seconds ) ) * 1e9 );
-    nanosleep( &t, NULL );
-}
-
-} //namespace sos
-
-#endif
