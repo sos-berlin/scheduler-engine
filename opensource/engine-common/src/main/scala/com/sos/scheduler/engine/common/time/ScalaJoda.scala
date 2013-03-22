6a985554ac2732e22da2622350c2629583fcb360
Joacim Zschimmer <jogit@zschimmer.com>
2013-03-22 16:49:48 +0100

    ScalaJoda korrigiert

--- opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/time/ScalaJoda.scala
+++ opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/time/ScalaJoda.scala
@@ -1,14 +1,15 @@
 package com.sos.scheduler.engine.common.time
 
-import org.joda.time.{Instant, Duration}
+import org.joda.time.Duration.{millis, standardSeconds, standardDays}
+import org.joda.time.{Duration, Instant}
 
 object ScalaJoda {
   implicit class DurationRichInt(val int: Int) extends AnyVal {
-    final def ms = Duration.millis(int)
-    final def s = Duration.standardSeconds(int)
-    final def hours = Duration.standardSeconds(int)
-    final def days = Duration.standardDays(int)
-    final def *(o: Duration) = Duration.millis(4 * o.getMillis)
+    final def ms = millis(int)
+    final def s = standardSeconds(int)
+    final def hours = standardSeconds(int)
+    final def days = standardDays(int)
+    final def *(o: Duration) = millis(int * o.getMillis)
   }
 
   implicit class RichInstant(val instant: Instant) extends AnyVal {
