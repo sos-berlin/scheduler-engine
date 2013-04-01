06f4055980b53cb7499e92a7862e3291060b8437
Joacim Zschimmer <jogit@zschimmer.com>
2013-04-02 00:53:22 +0200

    SchedulerEventBus extends Runnable

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/eventbus/ColdEventBus.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/eventbus/ColdEventBus.java
@@ -1,18 +1,20 @@
 package com.sos.scheduler.engine.eventbus;
 
-import com.sos.scheduler.engine.eventbus.annotated.ColdMethodEventSubscriptionFactory;
 import com.sos.scheduler.engine.data.event.Event;
+import com.sos.scheduler.engine.eventbus.annotated.ColdMethodEventSubscriptionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-public class ColdEventBus extends AbstractEventBus {
+import static com.google.common.base.Throwables.propagate;
+
+public class ColdEventBus extends AbstractEventBus implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(ColdEventBus.class);
 
-    private final Queue<Call> callQueue = new ConcurrentLinkedQueue<Call>();
+    private final LinkedBlockingQueue<Call> callQueue = new LinkedBlockingQueue<Call>();
 
     public ColdEventBus() {
         super(ColdMethodEventSubscriptionFactory.singleton);
@@ -26,9 +28,21 @@ public class ColdEventBus extends AbstractEventBus {
         callQueue.addAll(c);
     }
 
+    public final void run() {
+        try {
+            dispatchEvents(true);
+        } catch (InterruptedException ignored) {}
+    }
+
     public final void dispatchEvents() {
+        try {
+            dispatchEvents(false);
+        } catch (InterruptedException e) { throw propagate(e); }    // Kommt nicht vor
+    }
+
+    private void dispatchEvents(boolean wait) throws InterruptedException {
         while (true) {
-            Call call = callQueue.poll();
+            Call call = wait? callQueue.poll(Long.MAX_VALUE, TimeUnit.DAYS) : callQueue.poll();
             if (call == null) break;
             logger.trace("dispatch "+call);
             dispatchCall(call);
