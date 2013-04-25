0112f53cb351c54bfae1a47a8ba38eadc65a5555
Joacim Zschimmer <jogit@zschimmer.com>
2013-04-25 13:31:11 +0200

    JS-957: TestSchedulerController.newJdbcConnection()

--- opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/util/Hostware.java
+++ opensource/kernel/src/main/java/com/sos/scheduler/engine/kernel/util/Hostware.java
@@ -1,18 +1,11 @@
 package com.sos.scheduler.engine.kernel.util;
 
-import java.io.File;
-
 public final class Hostware {
     private Hostware() {}
 
-    public static String h2DatabasePath(File file) {
-        return h2DatabasePath(file.toString());
-    }
-
-    public static String h2DatabasePath(String name) {
-        String classOption = " -class=org.h2.Driver";
-        String jdbcUrl = quoted("jdbc:h2:"+name);
-        return "jdbc" + classOption + " " + jdbcUrl;
+    public static String databasePath(String className, String jdbcUrl) {
+        String classOption = " -class="+className;
+        return "jdbc" + classOption + " " + quoted(jdbcUrl);
     }
 
     public static String quoted(String s) {
