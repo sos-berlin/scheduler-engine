2f814269898925229a8c102c7f69aaa9f4907e46
Joacim Zschimmer <jogit@zschimmer.com>
2013-05-05 09:02:34 +0200

    - überarbeitet

--- opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/time/ScalaJoda.scala
+++ opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/time/ScalaJoda.scala
@@ -1,7 +1,7 @@
 package com.sos.scheduler.engine.common.time
 
 import org.joda.time.Duration.{millis, standardSeconds, standardDays}
-import org.joda.time.{LocalTime, Duration, Instant}
+import org.joda.time.{DateTime, LocalTime, Duration, Instant}
 
 object ScalaJoda {
   implicit class DurationRichInt(val int: Int) extends AnyVal {
@@ -12,9 +12,22 @@ object ScalaJoda {
     final def *(o: Duration) = millis(int * o.getMillis)
   }
 
+  implicit class DurationRichLong(val long: Long) extends AnyVal {
+    final def ms = millis(long)
+    final def s = standardSeconds(long)
+    final def hours = standardSeconds(long)
+    final def days = standardDays(long)
+    final def *(o: Duration) = millis(long * o.getMillis)
+  }
+
   implicit class RichInstant(val instant: Instant) extends AnyVal {
     def +(o: Duration) = instant plus o
     def -(o: Duration) = instant minus o
+  }
+
+  implicit class RichDateTime(val dateTime: DateTime) extends AnyVal {
+    def +(o: Duration) = dateTime plus o
+    def -(o: Duration) = dateTime minus o
   }
 
   implicit class RichLocalTime(val delegate: LocalTime) extends AnyVal {
@@ -22,6 +35,11 @@ object ScalaJoda {
     def <=(o: LocalTime) = !(delegate isAfter o)
     def >(o: LocalTime) = delegate isAfter o
     def >=(o: LocalTime) = !(delegate isBefore o)
+  }
+
+  implicit object DurationOrdering extends Ordering[Duration]{
+    def compare(x: Duration, y: Duration) =
+      x.getMillis compare y.getMillis
   }
 
   def sleep(d: Duration) =
--- opensource/test/src/test/scala/com/sos/scheduler/engine/test/util/time/WaitForConditionTest.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/test/util/time/WaitForConditionTest.scala
@@ -1,52 +1,51 @@
-package com.sos.scheduler.engine
-package test.util.time
+package com.sos.scheduler.engine.test.util.time
 
+import WaitForConditionTest._
+import com.sos.scheduler.engine.common.time.ScalaJoda._
 import com.sos.scheduler.engine.test.util.time.WaitForCondition._
 import java.lang.System.currentTimeMillis
 import org.joda.time.DateTime.now
-import org.joda.time.Duration.millis
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers._
+import org.joda.time.Duration
 
 @RunWith(classOf[JUnitRunner])
 final class WaitForConditionTest extends FunSuite {
 
-  import WaitForConditionTest._
-
-  test("realTimeIterator (time critical test)") {
+  test("realTimeIterator (time-critical test)") {
     realTimeIterator(Seq(now().getMillis)) // Aufruf zum Warmwerden. Laden der Klasse kann eine Weile dauern
-    meterElapsedTime { realTimeIterator(Seq(now().getMillis + 10000)) } should be < (50L) // Bereitstellung soll nicht warten
+    meterElapsedTime { realTimeIterator(Seq((now() + 10.s).getMillis)) } should be < (50.ms) // Bereitstellung soll nicht warten
     val t0 = now().getMillis
     val (t1, t2, t3) = (t0 + 100, t0 + 300, t0 + 400)
     val i = realTimeIterator(Seq(t1, t2, t3))
-    meterElapsedTime { i.next() } should be (t1 - t0 plusOrMinus 90)
-    meterElapsedTime { i.next() } should be (t2 - t1 plusOrMinus 90)
-    meterElapsedTime { i.next() } should be (t3 - t2 plusOrMinus 90)
+    meterElapsedTime { i.next() } .getMillis should be (t1 - t0 plusOrMinus 90)
+    meterElapsedTime { i.next() } .getMillis should be (t2 - t1 plusOrMinus 90)
+    meterElapsedTime { i.next() } .getMillis should be (t3 - t2 plusOrMinus 90)
   }
 
   test("waitForCondition(TimeoutWithSteps) 0 steps (time-critical test)") {
-    val elapsed = meterElapsedTime { waitForCondition(TimeoutWithSteps(millis(2000), millis(1000)))(true) }
-    elapsed.toInt should be < (80)
+    val elapsed = meterElapsedTime { waitForCondition(TimeoutWithSteps(2.s, 1.s)) { true } }
+    elapsed should be < (80.ms)
   }
 
   test("waitForCondition(TimeoutWithSteps) all steps (time-critical test)") {
-    val elapsed = meterElapsedTime { waitForCondition(TimeoutWithSteps(millis(100), millis(1)))(false) }
-    elapsed.toInt should (be >= 100 and be <= 200)
+    val elapsed = meterElapsedTime { waitForCondition(TimeoutWithSteps(100.ms, 1.ms)) { false } }
+    elapsed should (be >= 100.ms and be <= 200.ms)
   }
 
   test("waitForCondition(TimeoutWithSteps) some steps (time-critical test)") {
     var cnt = 0
-    val elapsed = meterElapsedTime { waitForCondition(TimeoutWithSteps(millis(1000), millis(10))) { cnt += 1; cnt > 10 } }  // 100ms
-    elapsed.toInt should (be >= 100 and be < 200)
+    val elapsed = meterElapsedTime { waitForCondition(TimeoutWithSteps(1.s, 10.ms)) { cnt += 1; cnt > 10 } }  // 100ms
+    elapsed should (be >= 100.ms and be < 200.ms)
   }
 }
 
 private object WaitForConditionTest {
-  def meterElapsedTime(f: => Unit) = {
+  def meterElapsedTime(f: => Unit): Duration = {
     val start = currentTimeMillis()
     f
-    currentTimeMillis() - start
+    (currentTimeMillis() - start).ms
   }
 }
