e7ceb5ce665bd3c05fc0a83e543de677be33b39c
Joacim Zschimmer <jogit@zschimmer.com>
2013-05-10 16:42:08 +0200

    JobChainPath.orderKey()

--- opensource/data/src/main/java/com/sos/scheduler/engine/data/folder/JobChainPath.java
+++ opensource/data/src/main/java/com/sos/scheduler/engine/data/folder/JobChainPath.java
@@ -2,10 +2,16 @@ package com.sos.scheduler.engine.data.folder;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sos.scheduler.engine.data.order.OrderId;
+import com.sos.scheduler.engine.data.order.OrderKey;
 
-public class JobChainPath extends TypedPath {
+public final class JobChainPath extends TypedPath {
     public JobChainPath(@JsonProperty("absolutePath") AbsolutePath path) {
         super(FileBasedType.jobChain, path);
+    }
+
+    public OrderKey orderKey(OrderId o) {
+        return new OrderKey(this, o);
     }
 
     @JsonCreator
