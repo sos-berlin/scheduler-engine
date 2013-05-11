22f5996147bd0b0b83b3ad9873007972d0d7c25d
Joacim Zschimmer <jogit@zschimmer.com>
2013-05-11 09:55:06 +0200

    UnmodifiableOrder.scala

---
+++ opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/order/UnmodifiableOrder.scala
@@ -0,0 +1,30 @@
+package com.sos.scheduler.engine.kernel.order
+
+import com.sos.scheduler.engine.cplusplus.runtime.annotation.ForCpp
+import com.sos.scheduler.engine.data.order.{OrderState, OrderId, OrderKey}
+import com.sos.scheduler.engine.eventbus.EventSource
+import com.sos.scheduler.engine.kernel.log.PrefixLog
+import com.sos.scheduler.engine.kernel.order.jobchain.UnmodifiableJobchain
+import com.sos.scheduler.engine.kernel.variable.UnmodifiableVariableSet
+import java.lang.String
+import javax.annotation.Nullable
+
+@ForCpp trait UnmodifiableOrder extends EventSource {
+  def getKey: OrderKey
+
+  def getId: OrderId
+
+  def getState: OrderState
+
+  def getEndState: OrderState
+
+  def getTitle: String
+
+  def getJobChain: UnmodifiableJobchain
+
+  @Nullable def getJobChainOrNull: UnmodifiableJobchain
+
+  def getParameters: UnmodifiableVariableSet
+
+  def getLog: PrefixLog
+}
