3a03bcf8305441883cb211add47617b27430efd0
Joacim Zschimmer <jogit@zschimmer.com>
2013-05-14 12:42:13 +0200

    SchedulerThreadFutures mit Timeout (provisorisch)

--- opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/async/SchedulerThreadFutures.scala
+++ opensource/kernel/src/main/scala/com/sos/scheduler/engine/kernel/async/SchedulerThreadFutures.scala
@@ -1,16 +1,24 @@
- package com.sos.scheduler.engine.kernel.async
+package com.sos.scheduler.engine.kernel.async
 
- import com.sos.scheduler.engine.common.async.FutureCompletion.{callFuture, timedCallFuture}
- import java.lang.Thread.currentThread
- import org.joda.time.Instant
- import scala.concurrent.duration.Duration
- import scala.concurrent.{Await, Future}
+import com.sos.scheduler.engine.common.async.FutureCompletion.{callFuture, timedCallFuture}
+import com.sos.scheduler.engine.common.time.ScalaJoda._
+import java.lang.Thread.currentThread
+import org.joda.time.Instant
+import scala.concurrent.{Await, Future, TimeoutException}
 
- object SchedulerThreadFutures {
+object SchedulerThreadFutures {
+
+  private val timeout = 30.s
 
   def inSchedulerThread[A](f: => A)(implicit q: SchedulerThreadCallQueue): A =
     if (currentThread == q.thread) f
-    else Await.result(schedulerThreadFuture(f), Duration.Inf)
+    else {
+      try Await.result(schedulerThreadFuture(f), timeout.toScalaDuration)
+      catch {
+        case e: TimeoutException =>
+          throw new AssertionError(s"SchedulerThreadFutures.inSchedulerThread does not return within ${timeout.getMillis}ms: $e", e)
+      }
+    }
 
   def schedulerThreadFuture[A](f: => A)(implicit callQueue: SchedulerThreadCallQueue): Future[A] =
     callFuture(f)
