1d71c4de7f303818948fd2008dd4bb84a64ef0ce
Joacim Zschimmer <jogit@zschimmer.com>
2013-08-18 09:57:22 +0200

    - aufgeräumt

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/SchedulerTestImplicits.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/scala/SchedulerTestImplicits.scala
@@ -1,32 +1,28 @@
 package com.sos.scheduler.engine.test.scala
 
-import scala.xml.{NodeBuffer, Node, NodeSeq, Elem}
 import com.sos.scheduler.engine.kernel.Scheduler
 import scala.reflect.ClassTag
 import scala.xml
-import scala.xml
-import com.sos.scheduler.engine.common.Lazy
 
 object SchedulerTestImplicits {
   implicit class ScaledScheduler(val scheduler: Scheduler) extends AnyVal {
-    def executeXmls(e: Iterable[NodeBuffer]): Result =
+    def executeXmls(e: Iterable[xml.NodeBuffer]): Result =
       executeXmlString(<commands>{e}</commands>.toString())
 
-    def executeXmls(e: NodeSeq): Result =
+    def executeXmls(e: xml.NodeSeq): Result =
       executeXmlString(<commands>{e}</commands>.toString())
 
-    def executeXml(e: Elem): Result =
+    def executeXml(e: xml.Elem): Result =
       executeXmlString(e.toString())
 
-    private def executeXmlString(o: String): Result = {
-      val result = scheduler.executeXml(o)
-      Result(result)
-    }
+    private def executeXmlString(o: String) =
+      Result(scheduler.executeXml(o))
 
-    def instance[A](implicit c: ClassTag[A]): A = scheduler.injector.getInstance(c.runtimeClass).asInstanceOf[A]
+    def instance[A](implicit c: ClassTag[A]): A =
+      scheduler.injector.getInstance(c.runtimeClass).asInstanceOf[A]
   }
 
-  case class Result(string: String) {
+  final case class Result(string: String) {
     lazy val elem = xml.XML.loadString(string)
   }
 }
