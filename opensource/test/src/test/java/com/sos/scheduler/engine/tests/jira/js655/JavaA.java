c640ab598e324ebacfea25780ce74212b5720a1b
Stefan Schaedlich <stefan.schaedlich.git@sos-berlin.com>
2013-08-29 15:06:40 +0200

    Korrekturen an bean Schicht für rhino

--- opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js655/JavaA.java
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js655/JavaA.java
@@ -10,8 +10,6 @@ public class JavaA extends Job_impl {
         Web_service_operation op = spooler_task.order().web_service_operation();
         //String request_string = new String( request.binary_content(), request.charset_name() );
         Web_service_response response = op.response();
-        response.set_content_type("text/plain");
-        response.set_charset_name("ISO-8859-1");
         response.set_string_content("Bye!");
         response.send();
         return true;
