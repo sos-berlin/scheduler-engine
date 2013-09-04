7358ec8b7f2ed5f7c5ffc0febd40cf75f1d396ee
Joacim Zschimmer <jogit@zschimmer.com>
2013-09-04 09:24:32 +0200

    Weitere Klassen für XML-Kommandos

--- opensource/data/src/main/scala/com/sos/scheduler/engine/data/xmlcommands/OrderCommand.scala
+++ opensource/data/src/main/scala/com/sos/scheduler/engine/data/xmlcommands/OrderCommand.scala
@@ -1,7 +1,10 @@
 package com.sos.scheduler.engine.data.xmlcommands
 
-import com.sos.scheduler.engine.data.order.OrderKey
+import com.sos.scheduler.engine.data.order.{OrderState, OrderKey}
 
-final case class OrderCommand(orderKey: OrderKey) extends XmlCommand {
-  def xmlElem = <order job_chain={orderKey.jobChainPathString} id={orderKey.idString}/>
+final case class OrderCommand(orderKey: OrderKey, state: Option[OrderState] = None) extends XmlCommand {
+  def xmlElem = <order
+    job_chain={orderKey.jobChainPathString}
+    id={orderKey.idString}
+    state={(state map { _.string }).orNull}/>
 }
