00bcfc97733b65430f0affe6dffcb1b3bb9c291e
fs <florian.schreiber.git@sos-berlin.com>
2013-10-18 18:37:36 +0200

    JS-1042: API method spooler.supervisor_client() returns supervisor_client or null (fixed)

--- opensource/kernel-cpp/scheduler/spooler.cxx
+++ opensource/kernel-cpp/scheduler/spooler.cxx
@@ -1197,8 +1197,6 @@ Task_subsystem* Spooler::task_subsystem() const
 
 supervisor::Supervisor_client_interface* Spooler::supervisor_client()
 { 
-    if( !_supervisor_client )  z::throw_xc( "SCHEDULER-423" );
-
     return _supervisor_client;
 }
 
