c387d5a2114c93ff939d4df8e400fc2f6c58003b
Joacim Zschimmer <jogit@zschimmer.com>
2013-12-04 10:49:01 +0100

    JS-1079: _connection_manager später initialisiert, wenn settings() bereitsteht

--- opensource/kernel-cpp/scheduler/database.cxx
+++ opensource/kernel-cpp/scheduler/database.cxx
@@ -1384,7 +1384,8 @@ void Database::try_reopen_after_error( const exception& callers_exception, const
                     }
 
                     _spooler->log()->warn( message_string( "SCHEDULER-958", seconds_before_reopen ) );   // "Eine Minute warten bevor Datenbank erneut geöffnet wird ..."
-                    _spooler->_connection_manager->async_continue_selected( is_allowed_operation_while_waiting, seconds_before_reopen );
+                    if (_spooler->_connection_manager)
+                        _spooler->_connection_manager->async_continue_selected( is_allowed_operation_while_waiting, seconds_before_reopen );
                 }
             }
         }
