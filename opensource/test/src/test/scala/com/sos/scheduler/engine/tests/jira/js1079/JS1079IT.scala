97fd804150fb267fd77a71e8a1da3fc30480742d
Joacim Zschimmer <jogit@zschimmer.com>
2013-12-04 22:45:04 +0100

    JS-1079: Test für com_remote.cxx hinzugefügt

--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/jira/js1079/JS1079IT.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/jira/js1079/JS1079IT.scala
@@ -4,9 +4,12 @@ import JS1079IT._
 import com.google.common.base.Charsets.UTF_8
 import com.google.common.io.Files
 import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder.findRandomFreePort
+import com.sos.scheduler.engine.data.folder.JobPath
+import com.sos.scheduler.engine.data.job.TaskEndedEvent
 import com.sos.scheduler.engine.test.binary.CppBinariesDebugMode
 import com.sos.scheduler.engine.test.configuration.TestConfiguration
 import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
+import com.sos.scheduler.engine.test.scala.SchedulerTestImplicits._
 import java.io.File
 import java.net._
 import org.junit.runner.RunWith
@@ -24,7 +27,6 @@ import org.scalatest.junit.JUnitRunner
   */
 @RunWith(classOf[JUnitRunner])
 final class JS1079IT extends ScalaSchedulerTest {
-
   override protected lazy val testConfiguration = TestConfiguration(
     binariesDebugMode = Some(CppBinariesDebugMode.release))  // debug ist zu langsam
   private val nextUdpSocket = new UdpSocketGenerator
@@ -45,16 +47,28 @@ final class JS1079IT extends ScalaSchedulerTest {
       c.connect()
       c.registerMe()   // Gleich nach connect(), damit C++-Code nicht in "Nichts getan"-Schlaf fällt.
     }
+    checkTasks()
     clients.head.fetchUpdatedFiles()   // Einmal, damit Supervisor.activate() aufgerufen wird
-    Files.write("<job><script>exit 0</script></job>", new File(allDirectory, "test.job.xml"), UTF_8)
+    Files.write("<job><script>exit 0</script></job>", new File(allDirectory, "test.job.xml"), UTF_8)   // Datei im Konfigurationsserver erzeugen, die ausgeteilt werden soll
     for (c <- clients) c expectUdp "<check_folders/>"
     for (c <- clients) c.close()
+  }
+
+  /** Auch bei den vielen Verbindungen soll com_remote.cxx noch funktionieren. */
+  private def checkTasks() {
+    val eventPipe = controller.newEventPipe()
+    for (i <- 1 to 2) {
+      scheduler executeXml <start_job job={testJobPath.string}/>
+      eventPipe.nextWithCondition[TaskEndedEvent] { _.jobPath == testJobPath }
+    }
+    eventPipe.close()
   }
 }
 
 
 private object JS1079IT {
   private val connectionsMax = 5000   // Bei 16359 (Windows 8.1, 12GB): java.net.SocketException: No buffer space available (maximum connections reached?): connect; Bei 8000 (Unbuntu 12.04, 8GB): ERRNO-24 Too many open files
+  private val testJobPath = JobPath.of("/test")
 
   private def schedulerXml(tcpPort: Int) =
     <spooler>
