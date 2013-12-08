699d659e1b0941c50918595b17824b08eb1fd735
Joacim Zschimmer <jogit@zschimmer.com>
2013-12-08 16:00:14 +0100

    JS-995 (not fixed): Test vorbereitet - A job referencing a name <schedule> does not initialize (SCHEDULER-161) when read from disk before the schedule

---
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/jira/js995/JS995IT.scala
@@ -0,0 +1,23 @@
+package com.sos.scheduler.engine.tests.jira.js995
+
+import com.sos.scheduler.engine.data.folder.JobPath
+import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
+import org.junit.Ignore
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/** JS-995 */
+@Ignore("Nicht gelöst")
+@RunWith(classOf[JUnitRunner])
+final class JS995IT extends ScalaSchedulerTest {
+
+  test("Schedule wirkt, ob Job vor oder nach Schedule gelesen wird") {
+    // Prüfen, ob Jobs trotz SCHEDULER-161 die richtige Startzeit aus dem <schedule> haben.
+    //instance[JobSubsystem].job(aJobPath).nextInstantOption should equal
+  }
+}
+
+private object JS995IT {
+  private val aJobPath = JobPath.of("/a")
+  private val bJobPath = JobPath.of("/b")
+}
