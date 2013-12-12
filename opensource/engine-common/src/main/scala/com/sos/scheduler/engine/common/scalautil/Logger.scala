c6fc7324dabf7ac7594f881161163ce82443e8c8
Joacim Zschimmer <jogit@zschimmer.com>
2013-12-12 11:56:18 +0100

    - Logger strenger gefasst

--- opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/Logger.scala
+++ opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/Logger.scala
@@ -1,63 +1,77 @@
 package com.sos.scheduler.engine.common.scalautil
 
 import org.slf4j.{Logger => Slf4jLogger, Marker, LoggerFactory}
-import scala.reflect.ClassTag
 
 final class Logger(val delegate: Slf4jLogger) extends AnyVal {
 
-  @inline def error(f: => Any) {
-    if (delegate.isErrorEnabled) delegate.warn(f.toString)
+  @inline def error(line: => String) {
+    if (delegate.isErrorEnabled)
+      delegate.warn(line)
   }
 
-  @inline def error(f: => Any, t: Throwable) {
-    if (delegate.isErrorEnabled) delegate.warn(f.toString, t)
+  @inline def error(line: => String, t: Throwable) {
+    if (delegate.isErrorEnabled)
+      delegate.warn(line, t)
   }
 
-  @inline def warn(f: => Any) {
-    if (delegate.isWarnEnabled) delegate.warn(f.toString)
+  @inline def warn(line: => String) {
+    if (delegate.isWarnEnabled)
+      delegate.warn(line)
   }
 
-  @inline def warn(f: => Any, t: Throwable) {
-    if (delegate.isWarnEnabled) delegate.warn(f.toString, t)
+  @inline def warn(line: => String, t: Throwable) {
+    if (delegate.isWarnEnabled)
+      delegate.warn(line, t)
   }
 
-  @inline def debug(f: => Any) {
-    if (delegate.isDebugEnabled) delegate.debug(f.toString)
+  @inline def debug(line: => String) {
+    if (delegate.isDebugEnabled)
+      delegate.debug(line)
   }
 
-  @inline def debug(f: => Any, t: Throwable) {
-    if (delegate.isDebugEnabled) delegate.debug(f.toString, t)
+  @inline def debug(line: => String, t: Throwable) {
+    if (delegate.isDebugEnabled)
+      delegate.debug(line, t)
   }
 
-  @inline def info(f: => Any) {
-    if (delegate.isInfoEnabled) delegate.info(f.toString)
+  @inline def info(line: => String) {
+    if (delegate.isInfoEnabled)
+      delegate.info(line)
   }
 
-  @inline def info(f: => Any, t: Throwable) {
-    if (delegate.isInfoEnabled) delegate.info(f.toString, t)
+  @inline def info(line: => String, t: Throwable) {
+    if (delegate.isInfoEnabled)
+      delegate.info(line, t)
   }
 
-  @inline def info(m: Marker, f: => Any) {
-    if (delegate.isInfoEnabled(m)) delegate.info(m, f.toString)
+  @inline def info(m: Marker, line: => String) {
+    if (delegate.isInfoEnabled(m))
+      delegate.info(m, line)
   }
 
-  @inline def info(m: Marker, f: => Any, t: Throwable) {
-    if (delegate.isInfoEnabled(m)) delegate.info(m, f.toString, t)
+  @inline def info(m: Marker, line: => String, t: Throwable) {
+    if (delegate.isInfoEnabled(m))
+      delegate.info(m, line, t)
   }
 
-  @inline def trace(f: => Any) {
-    if (delegate.isTraceEnabled) delegate.trace(f.toString)
+  @inline def trace(line: => String) {
+    if (delegate.isTraceEnabled)
+      delegate.trace(line)
   }
 
-  @inline def trace(f: => Any, t: Throwable) {
-    if (delegate.isTraceEnabled) delegate.trace(f.toString, t)
+  @inline def trace(line: => String, t: Throwable) {
+    if (delegate.isTraceEnabled)
+      delegate.trace(line, t)
   }
 }
 
-object Logger {
-  def apply[A <: AnyRef](implicit c: ClassTag[A]): Logger = apply(c.runtimeClass)
 
-  def apply(c: Class[_]) = new Logger(LoggerFactory.getLogger(normalizeClassName(c)))
+object Logger {
+//  def apply[A <: AnyRef](implicit c: ClassTag[A]): Logger =
+//    apply(c.runtimeClass)
+
+  def apply(c: Class[_]) =
+    new Logger(LoggerFactory.getLogger(normalizeClassName(c)))
 
   /** Entfernt das '$' der object-Klasse. */
   private def normalizeClassName(c: Class[_]) =
