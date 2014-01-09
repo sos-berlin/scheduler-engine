dc724702d0fd7f81f486094ff484f3a9164dc732
Joacim Zschimmer <jogit@zschimmer.com>
2014-01-09 14:15:02 +0100

    JS-1090 - aufgeräumt

--- opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js1090/JS1090IT.java
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js1090/JS1090IT.java
@@ -1,48 +1,46 @@
 package com.sos.scheduler.engine.tests.jira.js1090;
 
 import com.google.common.io.Files;
+import com.sos.scheduler.engine.data.folder.JobChainPath;
+import com.sos.scheduler.engine.data.folder.JobPath;
 import com.sos.scheduler.engine.data.order.OrderFinishedEvent;
 import com.sos.scheduler.engine.eventbus.EventHandler;
 import com.sos.scheduler.engine.test.SchedulerTest;
 import com.sos.scheduler.engine.test.util.CommandBuilder;
-import org.junit.Ignore;
+import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 
-import static junit.framework.Assert.assertTrue;
+import static com.sos.scheduler.engine.kernel.scheduler.SchedulerConstants.schedulerEncoding;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public final class JS1090IT extends SchedulerTest {
 
-    private static final String jobChain = "myJobChain";
-    private static final String job = "a";
-    private static final String includeContent = "echo *** hello world ***";
+    private static final JobChainPath jobChainPath = new JobChainPath("/myJobChain");
+    private static final JobPath jobPath = new JobPath("/a");
+    private static final String echoText = "+++ hello world +++";
+    private static final String includeContent = "echo " + echoText;
 
-    private CommandBuilder cmd = new CommandBuilder();
+    private final CommandBuilder cmd = new CommandBuilder();
     private String logFileContent = null;
 
-    // läuft derzeit auf Fehler, da JS-1090 noch nicht korrigiert
-    @Ignore
+    @Test
     public void test() throws Exception {
         controller().prepare();
-        File include = new File(controller().environment().configDirectory(),"include.txt");
-        Files.append(includeContent, include, Charset.forName("UTF-8"));
-        controller().activateScheduler(Arrays.asList("-e") );
-        String command = cmd.addOrder(jobChain,"myId").getCommand();
+        File includeFile = new File(controller().environment().liveDirectory(), "include.txt");
+        Files.write(includeContent, includeFile, schedulerEncoding);
+        controller().activateScheduler();
+        String command = cmd.addOrder(jobChainPath.string(), "myId").getCommand();
         scheduler().executeXml(command);
         controller().waitForTermination(shortTimeout);
         assertNotNull(logFileContent);
-        assertTrue(logFileContent.contains(includeContent));
+        assertTrue(logFileContent.contains(echoText));
     }
 
     @EventHandler
-    public void handleEvent(OrderFinishedEvent e) throws IOException {
+    public void handleEvent(OrderFinishedEvent e) {
         scheduler().terminate();
-        File logFile = new File(controller().environment().logDirectory(),"task." + job + ".log");
-        logFileContent = Files.toString(logFile,Charset.forName("UTF-8"));
+        logFileContent = controller().environment().taskLogFileString(jobPath);
     }
-
 }
