ad0f53eaf567c1814c403956843b4472b180c6a5
Joacim Zschimmer <jogit@zschimmer.com>
2014-01-22 17:52:47 +0100

    JS-1090: Test ist wirksam

--- opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js1090/JS1090IT.java
+++ opensource/test/src/test/java/com/sos/scheduler/engine/tests/jira/js1090/JS1090IT.java
@@ -5,7 +5,7 @@ import com.sos.scheduler.engine.data.order.OrderFinishedEvent;
 import com.sos.scheduler.engine.eventbus.EventHandler;
 import com.sos.scheduler.engine.test.SchedulerTest;
 import com.sos.scheduler.engine.test.util.CommandBuilder;
-import org.junit.Ignore;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,17 +21,15 @@ public final class JS1090IT extends SchedulerTest {
     private static final String job = "a";
     private static final String includeContent = "echo *** hello world ***";
 
-    private CommandBuilder cmd = new CommandBuilder();
     private String logFileContent = null;
 
-    // läuft derzeit auf Fehler, da JS-1090 noch nicht korrigiert
-    @Ignore
+    @Test
     public void test() throws Exception {
         controller().prepare();
         File include = new File(controller().environment().configDirectory(),"include.txt");
         Files.append(includeContent, include, Charset.forName("UTF-8"));
         controller().activateScheduler(Arrays.asList("-e") );
-        String command = cmd.addOrder(jobChain,"myId").getCommand();
+        String command = new CommandBuilder().addOrder(jobChain, "myId").getCommand();
         scheduler().executeXml(command);
         controller().waitForTermination(shortTimeout);
         assertNotNull(logFileContent);
@@ -44,5 +42,4 @@ public final class JS1090IT extends SchedulerTest {
         File logFile = new File(controller().environment().logDirectory(),"task." + job + ".log");
         logFileContent = Files.toString(logFile,Charset.forName("UTF-8"));
     }
-
 }
