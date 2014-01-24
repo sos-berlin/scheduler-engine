fe5bf824faffbc3018472326c1f05305fd4aa502
Joacim Zschimmer <jogit@zschimmer.com>
2014-01-24 17:13:19 +0100

    - SchedulerTestUtils benutzt

--- opensource/test/src/test/scala/com/sos/scheduler/engine/tests/jira/js1048/JS1048IT.scala
+++ opensource/test/src/test/scala/com/sos/scheduler/engine/tests/jira/js1048/JS1048IT.scala
@@ -3,13 +3,12 @@ package com.sos.scheduler.engine.tests.jira.js1048
 import JS1048IT._
 import com.sos.scheduler.engine.common.scalautil.ScalaXmls.implicits._
 import com.sos.scheduler.engine.data.folder.JobChainPath
-import com.sos.scheduler.engine.data.order.OrderKey
 import com.sos.scheduler.engine.data.xmlcommands.ModifyOrderCommand
-import com.sos.scheduler.engine.kernel.order.{Order, OrderSubsystem}
+import com.sos.scheduler.engine.test.ProvidesTestEnvironment
+import com.sos.scheduler.engine.test.SchedulerTestUtils.order
 import com.sos.scheduler.engine.test.configuration.{DefaultDatabaseConfiguration, TestConfiguration}
 import com.sos.scheduler.engine.test.scala.SchedulerTestImplicits._
 import com.sos.scheduler.engine.test.scalatest.HasCloserBeforeAndAfterAll
-import com.sos.scheduler.engine.test.{ProvidesTestEnvironment, TestSchedulerController}
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
@@ -26,20 +25,17 @@ final class JS1048IT
   override lazy val testConfiguration =
     TestConfiguration(database = Some(DefaultDatabaseConfiguration()))
 
-  private def order(key: OrderKey)(implicit controller: TestSchedulerController): Order =
-    controller.instance[OrderSubsystem].order(key)
-
   "After JobScheduler restart with an unchanged .order.xml, a previous modification should remain" in {
     runScheduler { implicit controller =>
       controller.scheduler executeXml ModifyOrderCommand(suspendOrderKey, suspended = Some(true))
-      order(suspendOrderKey).isSuspended shouldBe true
+      order(suspendOrderKey) shouldBe 'suspended
       order(titleOrderKey).title shouldEqual originalTitle
       controller.scheduler executeXml ModifyOrderCommand(titleOrderKey, title = Some(commandModifiedTitle))
       order(titleOrderKey).title shouldEqual commandModifiedTitle
     }
 
     runScheduler { implicit controller =>
-      order(suspendOrderKey).isSuspended shouldBe true
+      order(suspendOrderKey) shouldBe 'suspended
       order(titleOrderKey).title shouldEqual commandModifiedTitle
     }
   }
@@ -56,7 +52,7 @@ final class JS1048IT
     titleOrderKey.file(testEnvironment.liveDirectory).xml = <order title={fileChangedTitle}><run_time/></order>
 
     runScheduler { implicit controller =>
-      order(suspendOrderKey).isSuspended shouldBe false
+      order(suspendOrderKey) should not be 'suspended
       order(titleOrderKey).title shouldEqual fileChangedTitle
     }
   }
