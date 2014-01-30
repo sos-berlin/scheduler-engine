0f25afebdae8ea53a7035b5870d537d0128582a0
Joacim Zschimmer <jogit@zschimmer.com>
2014-01-30 18:33:46 +0100

    JS-1048: OrderCommand mit title

--- opensource/data/src/main/scala/com/sos/scheduler/engine/data/xmlcommands/OrderCommand.scala
+++ opensource/data/src/main/scala/com/sos/scheduler/engine/data/xmlcommands/OrderCommand.scala
@@ -2,9 +2,16 @@ package com.sos.scheduler.engine.data.xmlcommands
 
 import com.sos.scheduler.engine.data.order.{OrderState, OrderKey}
 
-final case class OrderCommand(orderKey: OrderKey, state: Option[OrderState] = None) extends XmlCommand {
+final case class OrderCommand(
+    orderKey: OrderKey,
+    state: Option[OrderState] = None,
+    title: Option[String] = None,
+    xmlChildren: xml.NodeSeq = Nil)
+extends XmlCommand {
+
   def xmlElem = <order
     job_chain={orderKey.jobChainPath.string}
     id={orderKey.id.string}
-    state={(state map { _.string }).orNull}/>
+    state={(state map { _.string }).orNull}
+    title={title.orNull}>{xmlChildren}</order>
 }
