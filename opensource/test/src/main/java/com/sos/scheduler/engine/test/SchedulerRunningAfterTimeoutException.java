850a7f6eeac71fc84edc0c919107c620b0b60597
Joacim Zschimmer <jogit@zschimmer.com>
2014-02-02 11:13:52 +0100

    Klasse Time durch Joda-Duration ersetzt - Nachtrag

--- opensource/test/src/main/java/com/sos/scheduler/engine/test/SchedulerRunningAfterTimeoutException.java
+++
@@ -1,9 +0,0 @@
-package com.sos.scheduler.engine.test;
-
-import org.joda.time.Duration;
-
-class SchedulerRunningAfterTimeoutException extends RuntimeException {
-    SchedulerRunningAfterTimeoutException(Duration timeout) {
-        super("Scheduler has not been terminated within "+timeout);
-    }
-}
---
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/SchedulerRunningAfterTimeoutException.scala
@@ -0,0 +1,6 @@
+package com.sos.scheduler.engine.test
+
+import org.joda.time.Duration
+
+class SchedulerRunningAfterTimeoutException(timeout: Duration)
+    extends RuntimeException(s"Scheduler has not been terminated within ${timeout.getStandardSeconds}")
