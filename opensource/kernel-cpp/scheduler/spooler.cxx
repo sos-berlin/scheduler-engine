4b7392d4f8003bef14f1cf316d1ec6f6ffcb19aa
Stefan Schaedlich <stefan.schaedlich.git@sos-berlin.com>
2014-02-04 14:48:26 +0100

    JS-1098: fixed

--- opensource/kernel-cpp/scheduler/spooler.cxx
+++ opensource/kernel-cpp/scheduler/spooler.cxx
@@ -803,26 +803,21 @@ string Spooler::name() const
     return result;
 }
 
-//--------------------------------------------------------------------------Spooler::trucate_head
+//-------------------------------------------------------------------------Spooler::truncate_head
 string Spooler::truncate_head(const string& str)
 {
-	string result = str;
-
 	int max_length = _spooler->settings()->_max_length_of_blob_entry;
-	if (str.length() > max_length) {
-		string msg = zschimmer::message_string("SCHEDULER-722", max_length);
-		Z_LOG2("scheduler", msg);
-		if (max_length <= msg.length())	{
-			result = msg;
-		}
-		else {
-			size_t start = str.length() - max_length + msg.length() - 1;
-			size_t x = str.substr(start).find_first_of("\n");
-			if (x == string::npos) x = 0;
-			result = msg + str.substr(start + x);
-		}
+	assert(max_length>=0);
+
+	if (str.length() <= max_length)
+		return str;
+	else {
+		string msg = message_string("SCHEDULER-722", max_length) + "\n";
+		_spooler->log()->debug(msg);
+		msg += "...";
+		size_t tail_start = str.length() - max_length + msg.length();
+		return msg + str.substr( min(tail_start,str.length()) );
 	}
-	return result;
 }
 
 
