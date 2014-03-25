57e34eaae36e4c97fc4df707a8f60014156302cd
Joacim Zschimmer <jogit@zschimmer.com>
2014-03-25 08:49:59 +0100

    C++/Java converter revised

--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/cppproxy/CppClassConfiguration.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/cppproxy/CppClassConfiguration.scala
@@ -5,17 +5,18 @@ import com.sos.scheduler.engine.cplusplus.runtime.annotation
 
 /** Parameter für einen C++-Proxy, angegeben als Annotation @CppClass zum Java-Interface, das den Proxy beschreibt. */
 final case class CppClassConfiguration(
-    interface: Class[_],
-    className: String,
-    directory: String = "",
-    includeOption: Option[String] = None)
+  interface: Class[_],
+  className: String,
+  directory: String = "",
+  includeOption: Option[String] = None)
 
 
 object CppClassConfiguration {
-  def apply(interface: Class[_]) = interface.getAnnotation(classOf[annotation.CppClass]) match {
-    case null =>
-      new CppClassConfiguration(interface, className = interface.getSimpleName)
-    case a =>
-      new CppClassConfiguration(interface, className = a.clas, directory = a.directory, includeOption = noneIfEmpty(a.include))
-  }
+  def apply(interface: Class[_]) =
+    interface.getAnnotation(classOf[annotation.CppClass]) match {
+      case null =>
+        new CppClassConfiguration(interface, className = interface.getSimpleName)
+      case a =>
+        new CppClassConfiguration(interface, className = a.clas, directory = a.directory, includeOption = noneIfEmpty(a.include))
+    }
 }
--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/main/Main.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/main/Main.scala
@@ -1,50 +1,49 @@
 package com.sos.scheduler.engine.cplusplus.generator.main
 
+import Main._
 import com.sos.scheduler.engine.cplusplus.generator._
 import com.sos.scheduler.engine.cplusplus.runtime.CppProxy
-import Main._
 
 /** Generiert Java- und C++-Proxys.
- * Für ein C++-Proxy in Java wird ein Java-Interface erwartet, das die C++-Klasse beschreibt.
- * Als Programmparameter können Klassen oder Java-Pakete angegeben werden.
- * Aus den Java-Paketen wählt der Generator die Interfaces aus, die das Interface CppProxy implementieren
- * (aber nicht mit @JavaOnlyInterface annotiert sind)
- * oder die Klassen oder Interfaces, die mit @ForCpp annotiert sind.
- */
-class Main(args: Array[String]) {
-    private val parameters = Parameters.ofCommandLine(args)
+  * Für ein C++-Proxy in Java wird ein Java-Interface erwartet, das die C++-Klasse beschreibt.
+  * Als Programmparameter können Klassen oder Java-Pakete angegeben werden.
+  * Aus den Java-Paketen wählt der Generator die Interfaces aus, die das Interface CppProxy implementieren
+  * (aber nicht mit @JavaOnlyInterface annotiert sind)
+  * oder die Klassen oder Interfaces, die mit @ForCpp annotiert sind.
+  */
+final class Main(args: Array[String]) {
+  private val parameters = Parameters.ofCommandLine(args)
 
-    private val (cppProxyInterfaces, javaProxies) = {
-        val (classNames, packageNames) = parameters.classOrPackageNames partition isClassName
-        val packageClasses = packageNames flatMap { Package(_).relevantClasses }
-        val classes = (classNames map Class.forName) ++ packageClasses
-        classes partition { classOf[CppProxy] isAssignableFrom _ }
-    }
-    
-    def apply() {
-        generateCppProxies()
-        generateJavaProxies()
-    }
+  private val (cppProxyInterfaces, javaProxies) = {
+    val (classNames, packageNames) = parameters.classOrPackageNames partition isClassName
+    val packageClasses = packageNames flatMap { Package(_).relevantClasses }
+    val classes = (classNames map Class.forName) ++ packageClasses
+    classes partition classOf[CppProxy].isAssignableFrom
+  }
 
-    private def generateCppProxies() {
-        cppproxy.Generator.generate(parameters.cppOutputDirectory, parameters.javaOutputDirectory, cppProxyInterfaces)
-    }
+  def apply() {
+    generateCppProxies()
+    generateJavaProxies()
+  }
 
-    private def generateJavaProxies() {
-        parameters.cppOutputDirectory foreach { dir =>
-            javaproxy.Generator.generate(dir, javaProxies ++ cppProxyInterfaces, parameters.deep)
-        }
-    }
+  private def generateCppProxies() {
+    cppproxy.Generator.generate(parameters.cppOutputDirectory, parameters.javaOutputDirectory, cppProxyInterfaces)
+  }
+
+  private def generateJavaProxies() {
+    for (dir <- parameters.cppOutputDirectory)
+      javaproxy.Generator.generate(dir, javaProxies ++ cppProxyInterfaces, parameters.deep)
+  }
 }
 
 /** Exception nicht nach stderr aus und gibt keinen Exit code zurück (s. MainWithExitCode). */
 object Main {
-    def main(args: Array[String]) {
-         new Main(args).apply()
-    }
+  def main(args: Array[String]) {
+    new Main(args).apply()
+  }
 
-    def isClassName(x: String) = {
-        val simpleName = ("" :: x.split('.').toList).last
-        simpleName.nonEmpty && simpleName.head.isUpper
-    }
+  def isClassName(x: String) = {
+    val simpleName = ("" :: x.split('.').toList).last
+    simpleName.nonEmpty && simpleName.head.isUpper
+  }
 }
