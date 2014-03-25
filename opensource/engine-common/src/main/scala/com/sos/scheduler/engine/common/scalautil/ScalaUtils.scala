d359f102e61124ff1288bc402c97029fe6d26148
Joacim Zschimmer <jogit@zschimmer.com>
2014-03-25 13:53:04 +0100

    toImmutableSeq for java.lang.Iterable

--- opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/ScalaUtils.scala
+++ opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/scalautil/ScalaUtils.scala
@@ -2,6 +2,7 @@ package com.sos.scheduler.engine.common.scalautil
 
 import scala.collection.immutable
 import scala.reflect.ClassTag
+import scala.collection.JavaConversions._
 
 object ScalaUtils {
   // Warum ist das nicht in Scala enthalten?
@@ -23,6 +24,16 @@ object ScalaUtils {
   }
 
   implicit class RichArray[A](val delegate: Array[A]) extends AnyVal {
+    def toImmutableSeq: immutable.Seq[A] =
+      Vector() ++ delegate
+  }
+
+  implicit class RichJavaIterable[A](val delegate: java.lang.Iterable[A]) extends AnyVal {
+    def toImmutableSeq: immutable.Seq[A] =
+      Vector() ++ delegate
+  }
+
+  implicit class RichJavaIterator[A](val delegate: java.util.Iterator[A]) extends AnyVal {
     def toImmutableSeq: immutable.Seq[A] =
       Vector() ++ delegate
   }
