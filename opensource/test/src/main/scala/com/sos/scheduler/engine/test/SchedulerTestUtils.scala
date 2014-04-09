281abfbb9bcfb973e111a18a07dc81334886d94c
Joacim Zschimmer <jogit@zschimmer.com>
2014-04-09 16:20:09 +0200

    runJobAndWaitForEnd() über neues runJobFuture() implementiert

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/SchedulerTestUtils.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/SchedulerTestUtils.scala
@@ -1,8 +1,13 @@
 package com.sos.scheduler.engine.test
 
+import _root_.scala.concurrent.{Await, Future}
+import com.sos.scheduler.engine.common.inject.GuiceImplicits._
+import com.sos.scheduler.engine.common.time.ScalaJoda._
 import com.sos.scheduler.engine.data.folder.{JobChainPath, JobPath}
 import com.sos.scheduler.engine.data.job.{TaskId, TaskClosedEvent}
 import com.sos.scheduler.engine.data.order.OrderKey
+import com.sos.scheduler.engine.kernel.async.SchedulerThreadCallQueue
+import com.sos.scheduler.engine.kernel.async.SchedulerThreadFutures._
 import com.sos.scheduler.engine.kernel.job.{Job, JobSubsystem}
 import com.sos.scheduler.engine.kernel.order.jobchain.JobChain
 import com.sos.scheduler.engine.kernel.order.{OrderSubsystem, Order}
@@ -25,9 +30,23 @@ object SchedulerTestUtils {
     hasInjector.injector.getInstance(classOf[OrderSubsystem]).orderOption(key)
 
   def runJobAndWaitForEnd(jobPath: JobPath)(implicit controller: TestSchedulerController, timeout: TestTimeout): TaskId = {
-    val event = controller.getEventBus.awaitingEvent[TaskClosedEvent](predicate = _.jobPath == jobPath) {
-      controller.scheduler executeXml <start_job job={jobPath.string}/>
+    val (taskId, future) = runJobFuture(jobPath)
+    Await.result(future, timeout.duration)
+    taskId
     }
-    event.taskId
+
+  def runJobFuture(jobPath: JobPath)(implicit controller: TestSchedulerController): (TaskId, Future[TaskClosedEvent]) = {
+    implicit val callQueue = controller.injector.apply[SchedulerThreadCallQueue]
+    inSchedulerThread {
+      val taskId = startJob(jobPath)
+      // Im selben Thread, damit wir sicher das Event abonnieren, bevor es eintrifft. Sonst ginge es verloren.
+      val future = controller.getEventBus.keyedEventFuture[TaskClosedEvent](taskId)
+      Tuple2(taskId, future)
+  }
+  }
+
+  def startJob(jobPath: JobPath)(implicit controller: TestSchedulerController): TaskId = {
+    val response = controller.scheduler executeXml <start_job job={jobPath.string}/>
+    TaskId((response.elem \ "answer" \ "ok" \ "task" \ "@id").toString().toInt)
   }
 }
