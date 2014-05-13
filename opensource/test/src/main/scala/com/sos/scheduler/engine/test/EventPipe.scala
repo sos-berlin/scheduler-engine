bb2769e59e546c66be39c637c601e5de54387245
Joacim Zschimmer <jogit@zschimmer.com>
2014-05-13 17:51:24 +0200

    - Scala-Warnungen umgangen

--- opensource/test/src/main/scala/com/sos/scheduler/engine/test/EventPipe.scala
+++ opensource/test/src/main/scala/com/sos/scheduler/engine/test/EventPipe.scala
@@ -3,7 +3,7 @@ package com.sos.scheduler.engine.test
 import EventPipe._
 import _root_.scala.annotation.tailrec
 import _root_.scala.reflect.ClassTag
-import _root_.scala.sys.error
+import com.sos.scheduler.engine.common.scalautil.ScalaUtils.implicitClass
 import com.sos.scheduler.engine.common.time.ScalaJoda._
 import com.sos.scheduler.engine.data.event.{KeyedEvent, Event}
 import com.sos.scheduler.engine.eventbus._
@@ -32,45 +32,49 @@ extends EventHandlerAnnotated with AutoCloseable {
   def nextKeyed[E <: KeyedEvent : ClassTag](key: E#Key, timeout: Duration = defaultTimeout): E =
     nextEvent[E](timeout, { _.key == key })
 
-  def nextWithCondition[E <: Event : ClassTag](condition: E => Boolean): E =
+  def nextWithCondition[E <: Event : ClassTag](condition: E ⇒ Boolean): E =
     nextEvent[E](defaultTimeout, condition)
 
-  def nextWithTimeoutAndCondition[E <: Event : ClassTag](timeout: Duration)(condition: E => Boolean): E =
+  def nextWithTimeoutAndCondition[E <: Event : ClassTag](timeout: Duration)(condition: E ⇒ Boolean): E =
     nextEvent[E](timeout, condition)
 
-  private def nextEvent[E <: Event](timeout: Duration, predicate: E => Boolean)(implicit classTag: ClassTag[E]): E =
-    nextEvent2(timeout, predicate, classTag.runtimeClass.asInstanceOf[Class[E]])
+  private def nextEvent[E <: Event : ClassTag](timeout: Duration, predicate: E ⇒ Boolean): E =
+    nextEvent2(timeout, predicate, implicitClass[E])
 
-  private def nextEvent2[E <: Event](timeout: Duration, predicate: E => Boolean, expectedEventClass: Class[E]): E = {
+  private def nextEvent2[E <: Event](timeout: Duration, predicate: E ⇒ Boolean, expectedEventClass: Class[E]): E = {
     val until = now() + timeout
+    def expectedName = expectedEventClass.getSimpleName
 
-    @tailrec def waitForEvent(): E = {
-      def expectedName = expectedEventClass.getSimpleName
+    @tailrec def waitForEvent(): E =
       tryPoll(until - now()) match {
-        case None => throw new TimeoutException(s"Expected Event '$expectedName' has not arrived with ${timeout.pretty}")
-        case Some(e: TerminatedEvent) => error(s"Expected event '$expectedName' has not arrived before ${classOf[TerminatedEvent].getName} has arrived")
-        case Some(e: E) if (expectedEventClass isAssignableFrom e.getClass) && evalPredicateIfDefined(predicate, e) => e
-        case _ => waitForEvent()
+        case None ⇒
+          throw new TimeoutException(s"Expected Event '$expectedName' has not arrived with ${timeout.pretty}")
+        case Some(e: TerminatedEvent) ⇒
+          sys.error(s"Expected event '$expectedName' has not arrived before ${classOf[TerminatedEvent].getName} has arrived")
+        case Some(e: Event) if (expectedEventClass isAssignableFrom e.getClass) && evalPredicateIfDefined(predicate, e.asInstanceOf[E]) ⇒
+          e.asInstanceOf[E]
+        case _ ⇒
+          waitForEvent()
       }
-    }
 
     waitForEvent()
   }
 
   def poll(t: ReadableDuration): Event =
-    tryPoll(t) getOrElse error(s"Event has not arrived within ${t.pretty}")
+    tryPoll(t) getOrElse sys.error(s"Event has not arrived within ${t.pretty}")
 
   def tryPoll(t: ReadableDuration): Option[Event] =
     Option(queue.poll(t.getMillis, TimeUnit.MILLISECONDS))
 }
 
 object EventPipe {
-  private val everyEvent = (e: Event) => true
+  private val everyEvent = (e: Event) ⇒ true
 
   class TimeoutException(override val getMessage: String) extends RuntimeException
 
-  private def evalPredicateIfDefined[E <: Event](o: E => Boolean, e: E) = o match {
-    case o: PartialFunction[Event, Boolean] => (o isDefinedAt e) && o(e)
-    case _ => o(e)
-  }
+  private def evalPredicateIfDefined[E <: Event](predicate: E ⇒ Boolean, event: E): Boolean =
+    predicate match {
+      case pf: PartialFunction[E, Boolean] ⇒ PartialFunction.cond(event)(pf)
+      case _ ⇒ predicate(event)
+    }
 }
