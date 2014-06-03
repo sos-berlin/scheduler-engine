82f585733fa7725a1c493165edb3c58070fd7674
Joacim Zschimmer <jogit@zschimmer.com>
2014-06-03 08:35:33 +0200

    JS-1155: UnsafeWebServicePlugin for not recommended command execution via HTTP GET

---
+++ opensource/opensource.plugins/jetty/src/main/scala/com/sos/scheduler/engine/plugins/unsafewebservice/UnsafeWebServicePlugin.scala
@@ -0,0 +1,23 @@
+package com.sos.scheduler.engine.plugins.unsafewebservice
+
+import UnsafeHttpGetCommandPlugin._
+import com.google.inject.AbstractModule
+import com.sos.scheduler.engine.kernel.plugin.{Plugin, UseGuiceModule}
+import com.sos.scheduler.engine.common.scalautil.Logger
+
+/**
+ * @author Joacim Zschimmer
+ */
+@UseGuiceModule(classOf[WebServicesModule])
+final class UnsafeHttpGetCommandPlugin extends Plugin
+
+object UnsafeHttpGetCommandPlugin {
+  private val logger = Logger(getClass)
+
+  final class WebServicesModule extends AbstractModule {
+    def configure() {
+      logger.warn("Use of UnsafeHttpGetCommandPlugin IS UNSAFE")
+      bind(classOf[UnsafeCommandService])
+    }
+  }
+}
