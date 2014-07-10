e8909a1d239da83b005f3af2344c92d22cadacd8
Joacim Zschimmer <jogit@zschimmer.com>
2014-07-10 18:33:21 +0200

    mavenSettings.h 1.8-SNAPSHOT

--- opensource/kernel-cpp/generated/mavenSettings.h
+++ opensource/kernel-cpp/generated/mavenSettings.h
@@ -12,8 +12,8 @@
 
 // Die numerische Dateiversion einer Windows-Datei (bei uns VER_PRODUCTVERSION) hat vier Zahlen
 
-#define VER_PRODUCTVERSION       1,7,2
-#define VER_PRODUCTVERSION_STR  "1.7.2-SNAPSHOT" VER_PRODUCTVERSION_TAIL
+#define VER_PRODUCTVERSION       1,8,0
+#define VER_PRODUCTVERSION_STR  "1.8-SNAPSHOT" VER_PRODUCTVERSION_TAIL
 
 #ifdef _DEBUG
 #   define VER_PRODUCTVERSION_DEBUG " (Debug)"
