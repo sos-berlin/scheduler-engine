1a9ebfc31da0c22a0e50d23a00bb53588b295077
Joacim Zschimmer <jogit@zschimmer.com>
2014-08-08 08:19:34 +0200

    - Test an langsamen Rechner angepasst

--- opensource/engine-common/src/test/scala/com/sos/scheduler/engine/common/async/FutureCompletionTest.scala
+++ opensource/engine-common/src/test/scala/com/sos/scheduler/engine/common/async/FutureCompletionTest.scala
@@ -6,16 +6,16 @@ import org.joda.time.Instant.now
 import org.junit.runner.RunWith
 import org.scalatest.Matchers._
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.{FreeSpec, OneInstancePerTest, FunSuite}
 import scala.util.Success
 
 @RunWith(classOf[JUnitRunner])
-final class FutureCompletionTest extends FunSuite with OneInstancePerTest {
+final class FutureCompletionTest extends FreeSpec with OneInstancePerTest {
 
   private lazy val queue: PoppableCallQueue = new StandardCallQueue
   private lazy val dispatcher = new CallRunner(queue)
 
-  test("warm-up") {
+  "warm-up" in {
     val call = futureTimedCall(now()) {}
     queue.add(call)
     val future = call.future
@@ -23,32 +23,32 @@ final class FutureCompletionTest extends FunSuite with OneInstancePerTest {
     future.isCompleted
   }
 
-  test("Success") {
-    val call = futureTimedCall(now() + 200.ms) { "Hej!" }
+  "Success" in {
+    val call = futureTimedCall(now() + 500.ms) { "Hej!" }
     queue.add(call)
     val future = call.future
     (future.isCompleted, future.value) shouldBe (false, None)
     dispatcher.executeMatureCalls()
     (future.isCompleted, future.value) shouldBe (false, None)
-    sleep(220.ms)
+    sleep(520.ms)
     dispatcher.executeMatureCalls()
     (future.isCompleted, future.value) shouldBe (true, Some(Success("Hej!")))
   }
 
-  test("Failure") {
-    val call = futureTimedCall(now() + 100.ms) { throw new TestException }
+  "Failure" in {
+    val call = futureTimedCall(now() + 500.ms) { throw new TestException }
     queue.add(call)
     val future = call.future
     (future.isCompleted, future.value) shouldBe (false, None)
     dispatcher.executeMatureCalls()
     (future.isCompleted, future.value) shouldBe (false, None)
-    sleep(101.ms)
+    sleep(501.ms)
     dispatcher.executeMatureCalls()
     future.isCompleted shouldBe true
     intercept[TestException] { future.value.get.get }
   }
 
-  test("ShortTermCall Success") {
+  "ShortTermCall Success" in {
     val call = futureCall { "Hej!" }
     queue.add(call)
     val future = call.future
@@ -57,7 +57,7 @@ final class FutureCompletionTest extends FunSuite with OneInstancePerTest {
     (future.isCompleted, future.value) shouldBe (true, Some(Success("Hej!")))
   }
 
-  test("ShortTermCall Failure") {
+  "ShortTermCall Failure" in {
     val call = futureCall { throw new TestException }
     queue.add(call)
     val future = call.future
@@ -67,7 +67,7 @@ final class FutureCompletionTest extends FunSuite with OneInstancePerTest {
     intercept[TestException] { future.value.get.get }
   }
 
-  test("callFuture") {
+  "callFuture" in {
     implicit val implicitQueue = queue
     val future = callFuture { throw new TestException }
     (future.isCompleted, future.value) shouldBe (false, None)
