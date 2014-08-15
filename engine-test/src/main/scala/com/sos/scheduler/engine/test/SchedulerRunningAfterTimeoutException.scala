72fd3c76aa1403ebb9abe02af17794c3ef475933
Joacim Zschimmer <jogit@zschimmer.com>
2014-08-15 15:41:34 +0200

    - log message corrected

--- engine-test/src/main/scala/com/sos/scheduler/engine/test/SchedulerRunningAfterTimeoutException.scala
+++ engine-test/src/main/scala/com/sos/scheduler/engine/test/SchedulerRunningAfterTimeoutException.scala
@@ -1,6 +1,7 @@
 package com.sos.scheduler.engine.test
 
 import org.joda.time.Duration
+import com.sos.scheduler.engine.common.time.ScalaJoda._
 
 class SchedulerRunningAfterTimeoutException(timeout: Duration)
-    extends RuntimeException(s"Scheduler has not been terminated within ${timeout.getStandardSeconds}")
+extends RuntimeException(s"Scheduler has not been terminated within ${timeout.pretty}")
