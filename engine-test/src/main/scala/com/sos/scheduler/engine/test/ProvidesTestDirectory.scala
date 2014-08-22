ac9df13199504d682f131d899ef02e221818a8f8
Joacim Zschimmer <jogit@zschimmer.com>
2014-08-22 16:05:45 +0200

    ProvidesTestDirectory.testDirectory is a canonical file path

--- engine-test/src/main/scala/com/sos/scheduler/engine/test/ProvidesTestDirectory.scala
+++ engine-test/src/main/scala/com/sos/scheduler/engine/test/ProvidesTestDirectory.scala
@@ -1,30 +1,30 @@
 package com.sos.scheduler.engine.test
 
-import ProvidesTestDirectory._
 import com.sos.scheduler.engine.common.scalautil.HasCloser
 import com.sos.scheduler.engine.common.system.Files._
-import com.sos.scheduler.engine.test.util.TestUtils.currentTestClass
+import com.sos.scheduler.engine.test.ProvidesTestDirectory._
 import java.io.File
 
 trait ProvidesTestDirectory extends HasCloser {
 
-  protected lazy val testDirectory: File =
-    System.getProperty(workDirectoryPropertyName) match {
-      case null =>
+  protected lazy val testDirectory: File = {
+    val file = System.getProperty(WorkDirectoryPropertyName) match {
+      case null ⇒
         val result = new File(System.getProperty("java.io.tmpdir"), s"test-$testName")
         onClose { tryRemoveDirectoryRecursivly(testDirectory) }
         result
-      case workDir =>
+      case workDir ⇒
         new File(workDir).mkdir
         new File(workDir, testName)
     }
+    file.getCanonicalFile
+  }
 
-  final def testName =
-    testClass.getName
+  final def testName = testClass.getName
 
   protected def testClass: Class[_]
 }
 
 private object ProvidesTestDirectory {
-  private val workDirectoryPropertyName = "com.sos.scheduler.engine.test.directory"
+  private val WorkDirectoryPropertyName = "com.sos.scheduler.engine.test.directory"
 }
