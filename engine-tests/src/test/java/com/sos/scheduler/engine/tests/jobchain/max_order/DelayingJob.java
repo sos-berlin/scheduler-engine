f780f060f0fdee8777a13e0d14a0b69813e239d6
Joacim Zschimmer <jogit@zschimmer.com>
2014-09-01 09:59:26 +0200

    - MaxOrderIT: spooler_task.delay_spooler_process does not work with order processing

--- engine-tests/src/test/java/com/sos/scheduler/engine/tests/jobchain/max_order/DelayingJob.java
+++ engine-tests/src/test/java/com/sos/scheduler/engine/tests/jobchain/max_order/DelayingJob.java
@@ -2,10 +2,10 @@ package com.sos.scheduler.engine.tests.jobchain.max_order;
 
 import sos.spooler.Job_impl;
 
-public class DelayingJob extends Job_impl {
+public final class DelayingJob extends Job_impl {
     @Override
-    public boolean spooler_process() {
-        spooler_task.set_delay_spooler_process(1);
+    public boolean spooler_process() throws InterruptedException {
+        Thread.sleep(1000);
         return true;
     }
 }
