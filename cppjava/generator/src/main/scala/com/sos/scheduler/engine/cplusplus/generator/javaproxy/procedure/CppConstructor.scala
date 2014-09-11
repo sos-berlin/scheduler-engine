0c37a2ad617976132206e1d813c6201bb28ddd40
Joacim Zschimmer <jogit@zschimmer.com>
2014-09-11 10:14:13 +0200

    - Warning removed

--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/javaproxy/procedure/CppConstructor.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/javaproxy/procedure/CppConstructor.scala
@@ -1,34 +1,33 @@
 package com.sos.scheduler.engine.cplusplus.generator.javaproxy.procedure
 
 import com.sos.scheduler.engine.cplusplus.generator.cpp._
-import com.sos.scheduler.engine.cplusplus.generator.javaproxy._
 import com.sos.scheduler.engine.cplusplus.generator.javaproxy.clas.CppClass
 import com.sos.scheduler.engine.cplusplus.generator.util.ProcedureSignature
 import java.lang.reflect.Constructor
+import scala.language.reflectiveCalls
 
+final class CppConstructor(cppClass: CppClass, s: ProcedureSignature) extends CppProcedure(cppClass, s) {
 
-class CppConstructor(cppClass: CppClass, s: ProcedureSignature) extends CppProcedure(cppClass, s)
-{
-    object objectInstantiator extends CppCode {
-        val myName = cppClass.name.simpleName
+  object objectInstantiator extends CppCode {
+    val myName = cppClass.name.simpleName
 
-        def headerCode = "static " + myName + " new_instance" + parameterListDeclaration + ";"
+    def headerCode = s"static $myName new_instance$parameterListDeclaration;"
 
-        def sourceCode = {
-            val declaration = myName + " " + myName + "::new_instance" + parameterListDeclaration
-            val body =
-                "{\n" +
-                    "    " + myName + " result;\n" +
-                    "    " + "result.java_object_allocate_();\n" +
-                             objectClassCode.call("result._class.get()", "result.get_jobject()") +
-                    "    " + "return result;\n" +
-                 "}\n"
-            declaration + " " + body
-        }
+    def sourceCode = {
+      val declaration = s"$myName $myName::new_instance$parameterListDeclaration"
+      val body =
+        s"""{
+           |    $myName result;
+           |    result.java_object_allocate_();
+           |${ objectClassCode.call("result._class.get()", "result.get_jobject()") }    return result;
+           |}
+           |""".stripMargin
+      s"$declaration $body"
     }
+  }
+
 }
 
-
 object CppConstructor {
-    def apply(cppClass: CppClass, c: Constructor[_]) = new CppConstructor(cppClass, ProcedureSignature(c))
+  def apply(cppClass: CppClass, c: Constructor[_]) = new CppConstructor(cppClass, ProcedureSignature(c))
 }
