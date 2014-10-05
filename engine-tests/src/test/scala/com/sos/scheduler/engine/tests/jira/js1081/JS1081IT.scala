212bcfb04666b5a34923d7211bd7975ae62cf53a
Joacim Zschimmer <jogit@zschimmer.com>
2014-10-05 08:45:45 +0200

    JS1081IT: Timeout prolonged

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1081/JS1081IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1081/JS1081IT.scala
@@ -1,45 +1,44 @@
 package com.sos.scheduler.engine.tests.jira.js1081
 
-import JS1081IT._
 import com.sos.scheduler.engine.common.scalautil.AutoClosing.autoClosing
 import com.sos.scheduler.engine.common.time.ScalaJoda._
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
 import com.sos.scheduler.engine.data.order.{OrderFinishedEvent, OrderKey}
-import com.sos.scheduler.engine.kernel.order.OrderSubsystem
+import com.sos.scheduler.engine.test.SchedulerTestUtils.order
 import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
 import com.sos.scheduler.engine.test.scala.SchedulerTestImplicits._
+import com.sos.scheduler.engine.tests.jira.js1081.JS1081IT._
 import org.joda.time.Instant.now
 import org.joda.time.format.DateTimeFormat
-import org.joda.time.{Instant, LocalTime, Duration}
+import org.joda.time.{Duration, Instant, LocalTime}
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
 import org.scalatest.junit.JUnitRunner
 
 /** JS-1081 */
 @RunWith(classOf[JUnitRunner])
-final class JS1081IT extends FunSuite with ScalaSchedulerTest {
+final class JS1081IT extends FreeSpec with ScalaSchedulerTest {
 
-  test("period with begin and absolute_repeat=1") {
+  s"period with begin and absolute_repeat=$AbsoluteRepeat" in {
     autoClosing(controller.newEventPipe()) { eventPipe =>
-      val absoluteRepeat = 2.s
-      val instants = absoluteRepeatedInstants(now() + 1.s, absoluteRepeat)
-      val myOrderElem = orderElem(aOrderKey, begin = instants.head.toDateTime.toLocalTime, absoluteRepeat = absoluteRepeat)
+      val instants = absoluteRepeatedInstants(now() + AbsoluteRepeat, AbsoluteRepeat)
+      val myOrderElem = orderElem(AOrderKey, begin = instants.head.toDateTime.toLocalTime, absoluteRepeat = AbsoluteRepeat)
       scheduler executeXml myOrderElem
       for (i <- 0 to 2) {
         withClue(s"Instant $i, $myOrderElem") {
-          scheduler.instance[OrderSubsystem].order(aOrderKey).nextInstantOption should equal (Some(instants(i)))
-          eventPipe.nextKeyed[OrderFinishedEvent](aOrderKey)
+          order(AOrderKey).nextInstantOption shouldEqual Some(instants(i))
+          eventPipe.nextKeyed[OrderFinishedEvent](AOrderKey)
         }
       }
     }
   }
 }
 
-
 private object JS1081IT {
-  private val testJobChainPath = JobChainPath("/test")
-  private val aOrderKey = testJobChainPath orderKey "A"
+  private val AbsoluteRepeat = 2.s
+  private val TestJobChainPath = JobChainPath("/test")
+  private val AOrderKey = TestJobChainPath orderKey "A"
   private val hhmmssFormatter = DateTimeFormat forPattern "HH:mm:ss"
 
   private def absoluteRepeatedInstants(start: Instant, absoluteRepeat: Duration): Stream[Instant] =
