dd83dd12d5313314bd5765ead359eda671052cac
Joacim Zschimmer <jogit@zschimmer.com>
2014-10-12 12:17:01 +0200

    JS-1188: CallQueue.executionContext

--- opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/async/CallQueue.scala
+++ opensource/engine-common/src/main/scala/com/sos/scheduler/engine/common/async/CallQueue.scala
@@ -1,9 +1,27 @@
 package com.sos.scheduler.engine.common.async
 
+import com.sos.scheduler.engine.common.async.CallQueue._
+import com.sos.scheduler.engine.common.scalautil.Logger
 import java.util.NoSuchElementException
 import org.joda.time.Instant
+import scala.concurrent.ExecutionContext
 
 trait CallQueue extends AutoCloseable {
+
+  object executionContext extends ExecutionContext {
+    def execute(o: Runnable): Unit = {
+      add(o)
+    }
+
+    def reportFailure(t: Throwable): Unit = {
+      // Should never be called, because NonFatal exceptions are already handled by class Call.
+      logger.error(t.toString, t)
+    }
+  }
+
+  object implicits {
+    implicit val executionContext: ExecutionContext = CallQueue.this.executionContext
+  }
 
   def at[A](t: Instant)(f: ⇒ A): TimedCall[A] = {
     val call = TimedCall(t)(f)
@@ -29,4 +47,8 @@ trait CallQueue extends AutoCloseable {
   }
 
   def nextTime: Long
+}
+
+object CallQueue {
+  private val logger = Logger(getClass)
 }
