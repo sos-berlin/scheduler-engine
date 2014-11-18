4724fa74c40e51888e424011f47e48171c6291fd
Joacim Zschimmer <jogit@zschimmer.com>
2014-11-18 12:36:37 +0100

    JS-1052 Test revised

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1052/JS1052IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1052/JS1052IT.scala
@@ -1,47 +1,52 @@
 package com.sos.scheduler.engine.tests.jira.js1052
 
-import JS1052IT._
+import com.sos.scheduler.engine.client.command.RemoteSchedulers
 import com.sos.scheduler.engine.common.system.OperatingSystem.isWindows
-import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder.{findRandomFreeTcpPort, alternateTcpPortRange}
+import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder.findRandomFreeTcpPort
 import com.sos.scheduler.engine.test.configuration.TestConfiguration
 import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
-import java.io.{OutputStreamWriter, InputStreamReader}
+import com.sos.scheduler.engine.tests.jira.js1052.JS1052IT._
+import java.io.{ByteArrayInputStream, OutputStreamWriter}
 import java.net.Socket
+import javax.xml.transform.stream.StreamSource
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
-import org.scalatest.Matchers._
+import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
 import scala.collection.mutable
 
-/** JS-1052
- * @author Florian
+/** JS-1052 Many TCP supervisor clients
+ * @author Joacim Zschimmer
  */
 @RunWith(classOf[JUnitRunner])
-final class JS1052IT extends FunSuite with ScalaSchedulerTest {
-  private lazy val supervisorPortNumber = findRandomFreeTcpPort(alternateTcpPortRange)
+final class JS1052IT extends FreeSpec with ScalaSchedulerTest {
+  private lazy val supervisorTcpPort = findRandomFreeTcpPort()
   override lazy val testConfiguration = TestConfiguration(
     testClass = getClass,
-    mainArguments = List(s"-tcp-port=$supervisorPortNumber"))
+    mainArguments = List(s"-tcp-port=$supervisorTcpPort"))
 
-  test("JS-1052") {
-    val sockets = mutable.Buffer[Socket]()
-
-    for (i <- 1 to maxConnections) {   // Unter dem Maximum 1024 von Unix
-      val socket = new Socket("127.0.0.1", supervisorPortNumber)
-      sockets += socket
-      register(socket)
-    }
-
+  s"Simulate $MaxConnections supervisor clients" in {
     def register(socket: Socket): Unit = {
       val writer = new OutputStreamWriter(socket.getOutputStream)
-      val reader = new InputStreamReader(socket.getInputStream)
-      writer.write("<register_remote_scheduler scheduler_id='TEST-SCHEDULER-ID' tcp_port='4444' version='1.5.3251'/>")
+      writer.write(<register_remote_scheduler scheduler_id='TEST-SCHEDULER-ID' tcp_port='4444' version='1.5.3251'/>.toString())
       writer.flush()
-      reader.read() should equal ('<'.toInt)
+      val b = new Array[Byte](1000)
+      val length = socket.getInputStream.read(b)
+      assert(length > 0)
+      val len = if (b(length - 1) == '\0') length -1 else length
+      RemoteSchedulers.checkResponseForError(new StreamSource(new ByteArrayInputStream(b, 0, len)))
     }
+
+    val sockets = mutable.Buffer[Socket]()
+    try
+      for (i <- 1 to MaxConnections) {
+        val socket = new Socket("127.0.0.1", supervisorTcpPort)
+        sockets += socket
+        register(socket)
+      }
+    finally for (o ← sockets) o.close()
   }
 }
 
 private object JS1052IT {
-  private val maxConnections = if (isWindows) 2000 else 950/2   // Unter Unix unterm Maximum von 1024, aufgeteilt zwischen Test und Scheduler (im selben Prozess)
+  private val MaxConnections = if (isWindows) 2000 else 950/2   // Unter Unix unterm Maximum von 1024, aufgeteilt zwischen Test und Scheduler (im selben Prozess)
 }
