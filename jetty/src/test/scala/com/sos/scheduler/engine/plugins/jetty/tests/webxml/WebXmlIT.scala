ad9585517d965bf2d3c5044b064751461e2ac9e3
Joacim Zschimmer <jogit@zschimmer.com>
2015-01-16 13:40:49 +0100

    Test: JettyPlugin allows access to symlinked files, when allowed in web.xml
    Old Jetty 8.1.5 wrongly allowed symlinks (and other aliases). For Jetty 8.1.16, this has to be explicitly allowed.

--- jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/tests/webxml/WebXmlIT.scala
+++ jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/tests/webxml/WebXmlIT.scala
@@ -1,11 +1,14 @@
 package com.sos.scheduler.engine.plugins.jetty.tests.webxml
 
 import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
+import com.sos.scheduler.engine.common.system.OperatingSystem.isWindows
 import com.sos.scheduler.engine.kernel.plugin.PluginSubsystem
 import com.sos.scheduler.engine.plugins.jetty.JettyPlugin
 import com.sos.scheduler.engine.plugins.jetty.test.{DirectoryListingTests, JettyPluginJerseyTester}
 import com.sos.scheduler.engine.test.TestEnvironment
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
+import java.nio.file.Files
+import java.nio.file.Files.{createSymbolicLink, createTempFile}
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
@@ -38,5 +41,18 @@ final class WebXmlIT extends FreeSpec with ScalaSchedulerTest with JettyPluginJe
 
   "Directory access" - {
     addDirectoryListingTests(webDirectoryName)
+  }
+
+  if (!isWindows) {  // Windows mklink needs the special policy right "Local Policies\User Rights Assignment\Create symbolic links"
+    "Symlink is accessible" in {
+      val symlinkedFile = createTempFile("JettyPlugin-", ".tmp")
+      onClose { Files.delete(symlinkedFile) }
+      symlinkedFile.toFile.write("TEST")
+      val name = "symlink.html"
+      val symlinkFile = (configDir / name).toPath
+      createSymbolicLink(symlinkFile, symlinkedFile)
+      onClose { Files.delete(symlinkFile) }
+      get[String](s"/jobscheduler/${configDir.getName}/$name") shouldEqual "TEST"
+    }
   }
 }
