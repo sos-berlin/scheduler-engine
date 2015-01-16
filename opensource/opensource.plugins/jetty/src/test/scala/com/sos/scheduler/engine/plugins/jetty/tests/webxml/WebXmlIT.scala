b68b883fa1452f9cf29a03253e14a1bae0238f05
Joacim Zschimmer <jogit@zschimmer.com>
2015-01-16 13:05:48 +0100

    Test: JettyPlugin allows access to symlinked files

--- opensource/opensource.plugins/jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/tests/webxml/WebXmlIT.scala
+++ opensource/opensource.plugins/jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/tests/webxml/WebXmlIT.scala
@@ -1,12 +1,15 @@
 package com.sos.scheduler.engine.plugins.jetty.tests.webxml
 
 import com.google.common.base.Charsets.UTF_8
-import com.google.common.io.{Resources, Files}
+import com.google.common.io.{Files, Resources}
+import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
+import com.sos.scheduler.engine.common.system.OperatingSystem._
 import com.sos.scheduler.engine.kernel.plugin.PluginSubsystem
 import com.sos.scheduler.engine.plugins.jetty.JettyPlugin
 import com.sos.scheduler.engine.plugins.jetty.test.JettyPluginJerseyTester
 import com.sos.scheduler.engine.test.scala.ScalaSchedulerTest
 import java.io.File
+import java.nio.file.Files._
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
@@ -41,6 +44,19 @@ final class WebXmlIT extends FreeSpec with ScalaSchedulerTest with JettyPluginJe
 
   "Web server should deliver external files described in web.xml" in {
     get[String](s"/jobscheduler/${configDir.getName}/scheduler.xml") shouldEqual readFile(new File(configDir, "scheduler.xml"))
+  }
+
+  if (!isWindows) {  // Windows mklink needs the special policy right "Local Policies\User Rights Assignment\Create symbolic links"
+    "Symlink is accessible" in {
+      val symlinkedFile = createTempFile("JettyPlugin-", ".tmp")
+      onClose { symlinkedFile.toFile.delete() }
+      symlinkedFile.toFile.write("TEST")
+      val name = "symlink.html"
+      val symlinkFile = (configDir / name).toPath
+      createSymbolicLink(symlinkFile, symlinkedFile)
+      onClose { symlinkFile.toFile.delete() }
+      get[String](s"/jobscheduler/${configDir.getName}/$name") shouldEqual "TEST"
+    }
   }
 
   private def readFile(f: File) = Files.toString(f, UTF_8)
