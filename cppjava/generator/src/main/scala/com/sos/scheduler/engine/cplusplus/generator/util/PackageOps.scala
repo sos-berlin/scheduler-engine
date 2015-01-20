23d896445ab39b2f48b65bb32c6bf1d8abe74df8
Joacim Zschimmer <jogit@zschimmer.com>
2015-01-20 10:34:21 +0100

    C++/Java generator reads a packages class names more carefully to avoid FileNotFoundException in "mvn deploy"
    Since deployment of JobScheduler v1.8-RC1, "mvn deploy" fails with
    java.io.FileNotFoundException: JAR entry com/sos/scheduler/engine not found in /home/jenkins/.m2/repository/com/sos/scheduler/engine/engine-common/1.8.0/engine-common-1.8.0.jar
    So we open the jar URL with root entry "/", and then the needless JarURLConnection.entryName is null.

--- cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/util/PackageOps.scala
+++ cppjava/generator/src/main/scala/com/sos/scheduler/engine/cplusplus/generator/util/PackageOps.scala
@@ -18,10 +18,17 @@ object PackageOps {
     entryNames flatMap classNameOfEntryName filter { _.startsWith(packageName + ".")}
   }
 
-  def entryNamesOfUrl(url: URL) = url.openConnection() match {
-    case a: JarURLConnection => (a.getJarFile.entries map { _.getName }).toList
+  private def entryNamesOfUrl(url: URL) = url.openConnection() match {
+    case o: JarURLConnection ⇒ entryNamesOfJar(o)
   //case a: FileURLConnection.. => new File(a.xx).listNames ... rekursiv durch die Unterverzeichnisse
     case _ => Nil
+  }
+
+  private def entryNamesOfJar(jarURLConnection: JarURLConnection) = {
+    // Since deployment of JobScheduler v1.8-RC1, "mvn deploy" fails with java.io.FileNotFoundException: JAR entry com/sos/scheduler/engine not found in /home/jenkins/.m2/repository/com/sos/scheduler/engine/engine-common/1.8.0/engine-common-1.8.0.jar
+    // So we open the jar URL with root entry "/", and then the needless JarURLConnection.entryName is null.
+    val rootEntryJarUrl = new URL(s"jar:${jarURLConnection.getJarFileURL}!/")
+    (rootEntryJarUrl.openConnection().asInstanceOf[JarURLConnection].getJarFile.entries map { _.getName }).toList
   }
 
   def classNameOfEntryName(entryName: String) = entryName match {
