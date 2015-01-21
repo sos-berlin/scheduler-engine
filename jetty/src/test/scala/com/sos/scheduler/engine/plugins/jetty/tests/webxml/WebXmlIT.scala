e0c3f24d4f0da5aab9e0b7089c6beae43c39c48c
Joacim Zschimmer <jogit@zschimmer.com>
2015-01-21 19:48:49 +0100

    WebXmlIT: Test with web.xml and URL /jobscheduler/operations_gui

--- jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/tests/webxml/WebXmlIT.scala
+++ jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/tests/webxml/WebXmlIT.scala
@@ -1,14 +1,14 @@
 package com.sos.scheduler.engine.plugins.jetty.tests.webxml
 
+import com.sos.scheduler.engine.common.guice.GuiceImplicits._
 import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
 import com.sos.scheduler.engine.common.system.OperatingSystem.isWindows
 import com.sos.scheduler.engine.kernel.plugin.PluginSubsystem
 import com.sos.scheduler.engine.plugins.jetty.JettyPlugin
 import com.sos.scheduler.engine.plugins.jetty.test.{DirectoryListingTests, JettyPluginJerseyTester}
-import com.sos.scheduler.engine.test.TestEnvironment
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import java.nio.file.Files
-import java.nio.file.Files.{createSymbolicLink, createTempFile}
+import java.nio.file.Files.{createDirectory, createSymbolicLink, createTempFile}
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
@@ -19,24 +19,26 @@ final class WebXmlIT extends FreeSpec with ScalaSchedulerTest with JettyPluginJe
 
   private lazy val baseDir = controller.environment.directory.getCanonicalFile
   private lazy val configDir = controller.environment.configDirectory
-  private val webDirectoryName = TestEnvironment.ConfigSubdirectoryName
+  private val webDirectoryName = "operations_gui"
+  private lazy val webDirectory = baseDir / webDirectoryName
 
   override protected def checkedBeforeAll(): Unit = {
     controller.activateScheduler()
     prepareWebXml()
-    val pluginSubsystem = injector.getInstance(classOf[PluginSubsystem])
-    pluginSubsystem.activatePlugin(classOf[JettyPlugin])
+    createDirectory(webDirectory)
+    injector.apply[PluginSubsystem].activatePlugin(classOf[JettyPlugin])
     super.checkedBeforeAll()
   }
 
   private def prepareWebXml(): Unit = {
-    assert(webDirectoryName == configDir.getName)
     val webXmlFile = configDir / "web.xml"
     webXmlFile.contentString = webXmlFile.contentString.replace("{{SCHEDULER_WORK_URL}}", baseDir.toURI.toURL.toString)
   }
 
   "Web server delivers external files described in web.xml" in {
-    get[String](s"/jobscheduler/$webDirectoryName/scheduler.xml") shouldEqual (configDir / "scheduler.xml").contentString
+    val file = webDirectory / "test.html"
+    file.contentString = "TEST"
+    get[String](s"/jobscheduler/$webDirectoryName/test.html") shouldEqual file.contentString
   }
 
   "Directory access" - {
@@ -45,14 +47,14 @@ final class WebXmlIT extends FreeSpec with ScalaSchedulerTest with JettyPluginJe
 
   if (!isWindows) {  // Windows mklink needs the special policy right "Local Policies\User Rights Assignment\Create symbolic links"
     "Symlink is accessible" in {
-      val symlinkedFile = createTempFile("JettyPlugin-", ".tmp")
+      val symlinkedFile = createTempFile("JettyPlugin-", ".tmp").toFile
       onClose { Files.delete(symlinkedFile) }
-      symlinkedFile.toFile.write("TEST")
+      symlinkedFile.contentString = "TEST"
       val name = "symlink.html"
-      val symlinkFile = (configDir / name).toPath
+      val symlinkFile = webDirectory / name
       createSymbolicLink(symlinkFile, symlinkedFile)
       onClose { Files.delete(symlinkFile) }
-      get[String](s"/jobscheduler/${configDir.getName}/$name") shouldEqual "TEST"
+      get[String](s"/jobscheduler/$webDirectoryName/$name") shouldEqual symlinkFile.contentString
     }
   }
 }
