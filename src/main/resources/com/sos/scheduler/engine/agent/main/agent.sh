9ae604b37f14933b9757cdee6e9554805a5056f1
Joacim Zschimmer <jogit@zschimmer.com>
2015-02-02 14:15:06 +0100

    JS-1291 agent.sh respects JAVA_HOME

--- src/main/resources/com/sos/scheduler/engine/agent/main/agent.sh
+++ src/main/resources/com/sos/scheduler/engine/agent/main/agent.sh
@@ -5,8 +5,14 @@ set -e
 
 if [ "$OSTYPE" = "cygwin" ]; then
     jarDir=$(cygpath -w "$SCHEDULER_AGENT_HOME/jar")
+    javaHome=""
+    [ -n "$JAVA_HOME" ] && javaHome=$(cygpath "$JAVA_HOME")
 else
     jarDir="$SCHEDULER_AGENT_HOME/jar"
+    javaHome="$JAVA_HOME"
 fi
 
-java -classpath "$jarDir/*" com.sos.scheduler.engine.agent.main.Main "$@"
+java=java
+[ -n "$javaHome" ] && java="$javaHome/bin/java"
+
+"$java" -classpath "$jarDir/*" com.sos.scheduler.engine.agent.main.Main "$@"
