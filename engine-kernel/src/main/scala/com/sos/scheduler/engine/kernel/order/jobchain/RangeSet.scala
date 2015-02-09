ea4a167c3ef3b6843edda815190ceee74bd9d986
Joacim Zschimmer <jogit@zschimmer.com>
2015-02-09 14:04:46 +0100

    JS-1190 RangeSet describes multiple ranges

---
+++ engine-kernel/src/main/scala/com/sos/scheduler/engine/kernel/order/jobchain/RangeSet.scala
@@ -0,0 +1,37 @@
+package com.sos.scheduler.engine.kernel.order.jobchain
+
+import com.sos.scheduler.engine.common.scalautil.DuplicateKeyException
+import com.sos.scheduler.engine.kernel.order.jobchain.RangeSet._
+
+/**
+ * @author Joacim Zschimmer
+ */
+private[jobchain] final case class RangeSet private(inclusiveRanges: Seq[InclusiveRange]) {
+  for (Seq(a, b) ← inclusiveRanges sortBy { _.from } sliding 2) if (a.to >= b.from) throw new DuplicateKeyException(s"Overlapping ranges: $a $b")
+
+  def contains(i: Int) = inclusiveRanges exists { _ contains i }
+
+  override def toString = inclusiveRanges mkString " "
+}
+
+private[jobchain] object RangeSet {
+  def apply(string: String): RangeSet = new RangeSet(string.trim split " +" map InclusiveRange.apply)
+
+  private[jobchain] case class InclusiveRange(from: Int, to: Int) {
+    require(from <= to, s"Reverse range is invalid: $this")
+    def contains(i: Int) = from <= i && i <= to
+    override def toString = if (from == to) s"$from" else s"$from..$to"
+  }
+
+  private[jobchain] object InclusiveRange {
+    private val Separator = ".."
+    def apply(o: String): InclusiveRange = {
+      o indexOf Separator match {
+        case -1 ⇒ apply(o.toInt)
+        case i ⇒ apply(o.substring(0, i).toInt, o.substring(i + Separator.size).toInt)
+      }
+    }
+
+    def apply(i: Int): InclusiveRange = new InclusiveRange(i, i)
+  }
+}
---
+++ engine-kernel/src/test/scala/com/sos/scheduler/engine/kernel/order/jobchain/RangeSetTest.scala
@@ -0,0 +1,48 @@
+package com.sos.scheduler.engine.kernel.order.jobchain
+
+import com.sos.scheduler.engine.common.scalautil.DuplicateKeyException
+import com.sos.scheduler.engine.kernel.order.jobchain.RangeSet.InclusiveRange
+import org.scalatest.FreeSpec
+import org.scalatest.Matchers._
+
+/**
+ * @author Joacim Zschimmer
+ */
+final class RangeSetTest extends FreeSpec {
+
+  "Parser" - {
+    List[(String, List[InclusiveRange])](
+      "1" → List(InclusiveRange(1)),
+      "1..3" → List(InclusiveRange(1, 3)),
+      "1..3 5" → List(InclusiveRange(1, 3), InclusiveRange(5)),
+      "1..3 5 -3..0 -9..-7" → List(InclusiveRange(1, 3), InclusiveRange(5), InclusiveRange(-3, 0), InclusiveRange(-9, -7)),
+      "1  2" → List(InclusiveRange(1), InclusiveRange(2)),
+      "  1   2  " → List(InclusiveRange(1), InclusiveRange(2)))
+    .foreach { case (string, ranges) ⇒
+        string in {
+          RangeSet(string).inclusiveRanges shouldEqual ranges
+        }
+     }
+  }
+
+  "Parser rejections" in {
+    intercept[IllegalArgumentException] { RangeSet("2..1").inclusiveRanges }
+    intercept[DuplicateKeyException] { RangeSet("1 1").inclusiveRanges }
+    intercept[DuplicateKeyException] { RangeSet("1..2 3 2").inclusiveRanges }
+    intercept[NumberFormatException] { RangeSet("").inclusiveRanges }
+    intercept[NumberFormatException] { RangeSet("x").inclusiveRanges }
+  }
+
+  "contains" in {
+    val r = RangeSet("3 9..10 -7..1 ")
+    val set = Set(3, 9, 10) ++ (-7 to 1)
+    for (i ← set) assert(r contains i)
+    for (i ← -100 to +100 filterNot set) assert(!(r contains i))
+  }
+
+  "Int.MaxValue and Int.MinValue" in {
+    for (i ← List(Int.MaxValue, Int.MinValue)) assert(!(RangeSet("1") contains i))
+    RangeSet(Int.MaxValue.toString) contains Int.MaxValue
+    RangeSet(Int.MinValue.toString) contains Int.MinValue
+  }
+}
