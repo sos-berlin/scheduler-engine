a5ac21a181c06aebfb4522417fb743232253dc9d
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-11 09:15:12 +0100

    - clean-up

--- src/main/scala/com/sos/scheduler/engine/minicom/idispatch/Invocable.scala
+++ src/main/scala/com/sos/scheduler/engine/minicom/idispatch/Invocable.scala
@@ -7,7 +7,9 @@ import com.sos.scheduler.engine.minicom.types.IUnknown
 import java.lang.reflect.Method
 
 /**
- * Public methods annotated with @[[invocable]] are callable via [[InvocableIDispatch]]
+ * Public methods annotated with @[[invocable]] are callable via [[InvocableIDispatch]].
+ * Nearly a [[IDispatch]], but without need to implement the `IDispatch`s methods.
+ *
  * @author Joacim Zschimmer
  */
 trait Invocable extends IUnknown {
