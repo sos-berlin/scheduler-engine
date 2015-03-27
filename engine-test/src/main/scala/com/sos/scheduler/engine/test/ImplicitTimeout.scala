210fc7f4352d3cd7176506b85bf42d99b966d313
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-27 09:43:33 +0100

    Test framework: ImplicitTimeout with implict conversion

--- engine-test/src/main/scala/com/sos/scheduler/engine/test/ImplicitTimeout.scala
+++ engine-test/src/main/scala/com/sos/scheduler/engine/test/ImplicitTimeout.scala
@@ -2,8 +2,13 @@ package com.sos.scheduler.engine.test
 
 import java.util.concurrent.TimeUnit
 import org.joda.time.Duration
+import scala.language.implicitConversions
 
 final case class ImplicitTimeout(duration: Duration) {
 
   def concurrentDuration = concurrent.duration.Duration(duration.getMillis, TimeUnit.MILLISECONDS)
+}
+
+object ImplicitTimeout {
+  implicit def durationToImplicitDuration(duration: Duration): ImplicitTimeout = new ImplicitTimeout(duration)
 }
