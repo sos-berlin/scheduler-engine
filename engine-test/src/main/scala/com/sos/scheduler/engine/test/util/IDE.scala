c42135e76d9d69bfef47c74bda618e19665f5db4
Joacim Zschimmer <jogit@zschimmer.com>
2015-03-31 11:16:08 +0200

    Possible implementation for isRunningUnderDebugger

--- engine-test/src/main/scala/com/sos/scheduler/engine/test/util/IDE.scala
+++ engine-test/src/main/scala/com/sos/scheduler/engine/test/util/IDE.scala
@@ -7,4 +7,9 @@ object IDE {
       mainClassName.startsWith("com.intellij.") || mainClassName.startsWith("org.jetbrains.")
     }
   }
+
+//  Method according to http://stackoverflow.com/questions/8417524
+//  import java.lang.management.ManagementFactory
+//  import scala.collection.JavaConversions._
+//  val isRunningUnderDebugger: Boolean = ManagementFactory.getRuntimeMXBean.getInputArguments exists { _ startsWith "-agentlib" }
 }
