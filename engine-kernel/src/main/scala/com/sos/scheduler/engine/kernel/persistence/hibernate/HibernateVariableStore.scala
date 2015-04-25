03cc47c736aa298de570e58b27b9351fe4c4dda0
Joacim Zschimmer <jogit@zschimmer.com>
2015-04-25 21:58:23 +0200

    JS-1357 Access to database table scheduler_variables via Hibernate

---
+++ engine-kernel/src/main/scala/com/sos/scheduler/engine/kernel/persistence/hibernate/HibernateVariableStore.scala
@@ -0,0 +1,18 @@
+package com.sos.scheduler.engine.kernel.persistence.hibernate
+
+import com.sos.scheduler.engine.data.job.TaskId
+import com.sos.scheduler.engine.data.scheduler.VariablePersistentState
+import com.sos.scheduler.engine.persistence.entities.{VariableEntity, VariableEntityConverter}
+import javax.inject.{Inject, Singleton}
+import javax.persistence.{EntityManager, EntityManagerFactory}
+
+/**
+ * @author Joacim Zschimmer
+ */
+@Singleton
+final class HibernateVariableStore @Inject private(protected val entityManagerFactory: EntityManagerFactory)
+extends AbstractHibernateStore[VariablePersistentState, String, VariableEntity]
+with VariableEntityConverter {
+
+  def nextTaskId(implicit em: EntityManager) = TaskId(fetch(VariableEntity.JobIdName).int)
+}
