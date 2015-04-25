9a66c1841de15f39b6325b9c533913acc3b73cde
Joacim Zschimmer <jogit@zschimmer.com>
2015-04-25 21:59:30 +0200

    JS-1357 Job path requesting the next ID is logged in scheduler.log

--- engine-kernelcpp/scheduler/database.cxx
+++ engine-kernelcpp/scheduler/database.cxx
@@ -1382,10 +1382,10 @@ Database_lock_syntax Database::lock_syntax()
 
 //---------------------------------------------------------------------------------Database::get_id
 
-int Database::get_id( const string& variable_name, Transaction* outer_transaction )
+int Database::get_id(const string& variable_name, Transaction* outer_transaction, const string& debug)
 {
     try {
-        return get_id_( variable_name, outer_transaction );
+        return get_id_(variable_name, outer_transaction, debug);
     }
     catch (exception& x) { 
         _spooler->log()->error( message_string( "SCHEDULER-304", x, variable_name ) );   // "FEHLER BEIM LESEN DER NÄCHSTEN ID: "
@@ -1395,39 +1395,40 @@ int Database::get_id( const string& variable_name, Transaction* outer_transactio
 
 //--------------------------------------------------------------------------------Database::get_id_
 
-int Database::get_id_( const string& variable_name, Transaction* outer_transaction )
+int Database::get_id_(const string& variable_name, Transaction* outer_transaction, const string& debug)
 {
+    string my_debug = Z_FUNCTION + " " + debug;
     int id = -1;
     for (Retry_nested_transaction ta (db(), outer_transaction); ta.enter_loop(); ta++) 
     try {
         if( dbms_kind() == dbms_access ||
             dbms_kind() == dbms_sybase)
         {
-            ta.execute( S() << "UPDATE " << _variables_tablename << "  set `wert`=`wert`+1  where `name`=" << sql::quoted( variable_name ), Z_FUNCTION );
+            ta.execute( S() << "UPDATE " << _variables_tablename << "  set `wert`=`wert`+1  where `name`=" << sql::quoted( variable_name ), my_debug);
             Any_file sel = ta.open_result_set( S() << "select `wert`  from " << _variables_tablename << "  where `name`=" << sql::quoted( variable_name ),
-                                               Z_FUNCTION );
+                                               my_debug);
             if (sel.eof()) {
                 id = 1;
                 ta.execute( S() << "INSERT into " << _variables_tablename << " (`name`,`wert`) " 
                             "values (" << sql::quoted( variable_name ) << "," << id << ")",
-                            Z_FUNCTION);
+                            my_debug);
             } else {
                 id = sel.get_record().as_int(0);
             }
         } else {
             for (int tries = 2; tries > 0; tries--) {
                 Any_file sel = ta.open_commitable_result_set(S() << "select `wert`  from " << _variables_tablename << " %update_lock  where `name`=" << sql::quoted(variable_name), 
-                                                              Z_FUNCTION );
+                                                             my_debug);
                 if (!sel.eof()) {
                     id = sel.get_record().as_int(0) + 1;
-                    ta.execute(S() << "UPDATE " << _variables_tablename << "  set `wert`=`wert`+1  where `name`=" << sql::quoted( variable_name ), Z_FUNCTION);
+                    ta.execute(S() << "UPDATE " << _variables_tablename << "  set `wert`=`wert`+1  where `name`=" << sql::quoted( variable_name ), my_debug);
                     tries = 0;
                 } else {
                     try {
                         id = 1;
                         ta.execute( S() << "INSERT into " << _variables_tablename << " (`name`,`wert`) " 
                                     "values (" << sql::quoted(variable_name) << "," << id << ")",
-                                    Z_FUNCTION);
+                                    my_debug);
                     }
                     catch (exception& x) {
                         if (tries <= 1) throw;
@@ -1436,11 +1437,11 @@ int Database::get_id_( const string& variable_name, Transaction* outer_transacti
                 }
             }
         }
-        Z_LOG2( "scheduler", "Database::get_id(\"" + variable_name + "\") = " << id << '\n' );
-        ta.commit( Z_FUNCTION );
+        Z_LOG2( "scheduler", "Database::get_id(\"" + variable_name + "\") = " << id << " " << debug << '\n' );
+        ta.commit(my_debug);
     }
-    catch (exception& x) { ta.reopen_database_after_error(z::Xc("SCHEDULER-306", _variables_tablename, x  ), Z_FUNCTION); }
-    if (id == -1)  z::throw_xc(Z_FUNCTION);
+    catch (exception& x) { ta.reopen_database_after_error(z::Xc("SCHEDULER-306", _variables_tablename, x  ), my_debug); }
+    if (id == -1)  z::throw_xc(Z_FUNCTION, debug);
     return id;
 }
 
@@ -1603,7 +1604,7 @@ void Database::spooler_start()
 {
     if( _db.opened() )
     {
-        _id = get_task_id();     // Der Spooler-Satz hat auch eine Id
+        _id = get_task_id("Scheduler");     // Der Spooler-Satz hat auch eine Id
 
         Transaction ta ( this );
         {
