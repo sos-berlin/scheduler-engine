ab5cfa672904fd40d7c8c317171ed6b5c3b02dc0
Joacim Zschimmer <jogit@zschimmer.com>
2015-04-30 20:37:02 +0200

    JS-1352 Agent: Java job with monitor. Test SpoolerProcessAfterIT covers Agent

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/order/monitor/spoolerprocessafter/TestJob.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/order/monitor/spoolerprocessafter/TestJob.scala
@@ -4,11 +4,11 @@ import sos.spooler.Job_impl
 
 class TestJob extends Job_impl {
   override def spooler_process() = {
-    import setting.SpoolerProcessNames.{returns, throwException, logError}
+    import setting.SpoolerProcessNames.{logError, returns, throwException}
 
     val params = spooler_task.order.params
     if (!params.value(logError).isEmpty)  spooler_log.error(params.value(logError) +" ...")
-    if (!params.value(throwException).isEmpty)  throw new RuntimeException("EXCEPTION IN SPOOLER_PROCESS_AFTER")
+    if (!params.value(throwException).isEmpty)  throw new RuntimeException("EXCEPTION IN SPOOLER_PROCESS")
     params.value(returns).toBoolean
   }
 }
