0b544bf84aacd6d04862a4936add470b247ede11
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-11 14:20:13 +0200

    JS-1291 AgentTest.agentUri

--- engine-agent-test/src/main/scala/com/sos/scheduler/engine/agent/test/AgentTest.scala
+++ engine-agent-test/src/main/scala/com/sos/scheduler/engine/agent/test/AgentTest.scala
@@ -1,12 +1,10 @@
 package com.sos.scheduler.engine.agent.test
 
 import com.sos.scheduler.engine.agent.Agent
-import com.sos.scheduler.engine.agent.configuration.AgentConfiguration
 import com.sos.scheduler.engine.agent.test.AgentTest._
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits.RichClosersAutoCloseable
 import com.sos.scheduler.engine.common.scalautil.Futures._
 import com.sos.scheduler.engine.common.scalautil.HasCloser
-import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder._
 import com.sos.scheduler.engine.data.processclass.ProcessClassPath
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import scala.concurrent.duration._
@@ -17,12 +15,12 @@ import scala.concurrent.duration._
 trait AgentTest extends HasCloser {
   this: ScalaSchedulerTest ⇒
 
-  private lazy val agentTcpPort = findRandomFreeTcpPort()
-  private lazy val agent = new Agent(AgentConfiguration(httpPort = agentTcpPort, httpInterfaceRestriction = Some("127.0.0.1"))).closeWithCloser
+  protected lazy val agent = Agent.forTest().closeWithCloser
+  protected lazy val agentUri = agent.localUri
 
   protected override def onSchedulerActivated() = {
     val started = agent.start()
-    scheduler executeXml <process_class name={AgentProcessClassPath.withoutStartingSlash} remote_scheduler={s"http://127.0.0.1:$agentTcpPort"}/>
+    scheduler executeXml <process_class name={AgentProcessClassPath.withoutStartingSlash} remote_scheduler={agentUri}/>
     awaitResult(started, 10.seconds)
   }
 }
