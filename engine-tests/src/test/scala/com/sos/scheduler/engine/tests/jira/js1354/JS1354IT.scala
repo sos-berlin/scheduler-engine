bec2485e1e2d4d6de0506d6facde85320f638722
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-21 17:28:19 +0200

    JS-1391 Failing test with job_chain max_orders=1

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1354/JS1354IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1354/JS1354IT.scala
@@ -11,15 +11,17 @@ import com.sos.scheduler.engine.kernel.folder.FolderSubsystem
 import com.sos.scheduler.engine.kernel.persistence.hibernate.HibernateVariableStore
 import com.sos.scheduler.engine.kernel.persistence.hibernate.ScalaHibernate.transaction
 import com.sos.scheduler.engine.test.EventBusTestFutures.implicits.RichEventBus
+import com.sos.scheduler.engine.test.SchedulerTestUtils._
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import com.sos.scheduler.engine.tests.jira.js1354.JS1354IT._
 import javax.persistence.EntityManagerFactory
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
+import scala.concurrent.Future
 
 /**
- * JS-1354, JS-1390 Avoid unneccessary creation of new ids for table scheduler_history.
+ * JS-1354, JS-1390, JS-1391 file_order_source should not lead to creation of needless task IDs
  *
  * @author Joacim Zschimmer
  */
@@ -29,33 +31,56 @@ final class JS1354IT extends FreeSpec with ScalaSchedulerTest {
   private lazy val fileOrderDir = testEnvironment.newFileOrderSourceDirectory()
   private implicit lazy val entityManagerFactory: EntityManagerFactory = instance[EntityManagerFactory]
 
-  "Database variable for next task ID is not incremented while JobScheduler waits for a file order" in {
-    testEnvironment.fileFromPath(TestJobChainPath).xml =
-      <job_chain>
-        <file_order_source directory={fileOrderDir.getPath} repeat="1"/>
-        <job_chain_node state="100" job="/test-100"/>
-        <job_chain_node state="200" job="/test-200"/>
-      </job_chain>
-    instance[FolderSubsystem].updateFolders()
-    runFile("TESTFILE-1")
-  }
-
-  "Second file" in {
-    runFile("TESTFILE-2")
-  }
-
-  "Third file" in {
-    runFile("TESTFILE-3")
-  }
-
-  private def runFile(name: String): Unit = {
-    val preId = nextTaskId
-    val file = fileOrderDir / name
-    eventBus.awaitingKeyedEvent[OrderFinishedEvent](TestJobChainPath orderKey file.getPath) {
-      touch(file)
+  "JS-1391 No needless task ID creation while the limit of a job chain with file_order_source is reached" - {
+    "job_chain max_order=1" in {
+      runFiles("TESTFILE-1a", "TESTFILE-1b") {
+        testEnvironment.fileFromPath(TestJobChainPath).xml =
+          <job_chain max_orders="1">
+            <file_order_source directory={fileOrderDir.getPath} repeat="1"/>
+            <job_chain_node state="100" job="/test-100"/>
+            <job_chain_node state="200" job="/test-200"/>
+          </job_chain>
+        instance[FolderSubsystem].updateFolders()
+      }
     }
+
+    "Second files" in {
+      runFiles("TESTFILE-2a", "TESTFILE-2b") {}
+    }
+  }
+
+  "JS-1354 No needless task ID creation while waiting for a new file order" - {
+    "job_chain max_order=unlimited" in {
+      testEnvironment.fileFromPath(TestJobChainPath).xml =
+        <job_chain>
+          <file_order_source directory={fileOrderDir.getPath} repeat="1"/>
+          <job_chain_node state="100" job="/test-100"/>
+          <job_chain_node state="200" job="/test-200"/>
+        </job_chain>
+      instance[FolderSubsystem].updateFolders()
+      runFiles("TESTFILE-1") {}
+    }
+  }
+
+  "JS-1390 Don't fall asleep after first file order" - {
+    "Second files" in {
+      runFiles("TESTFILE-2a", "TESTFILE-2b") {}
+    }
+
+    "Third file" in {
+      runFiles("TESTFILE-3") {}
+    }
+  }
+
+  private def runFiles(names: String*)(body: ⇒ Unit): Unit = {
+    val preId = nextTaskId
+    val files = names map { o ⇒ fileOrderDir / o }
+    val ordersFinished = Future.sequence(files map { f ⇒ eventBus.keyedEventFuture[OrderFinishedEvent](TestJobChainPath orderKey f.getPath) })
+    files foreach touch
+    body
+    awaitSuccess(ordersFinished)
     val postId = nextTaskId
-    assert(postId == TaskId(preId.value + JobChainTaskCount))
+    assert(postId == TaskId(preId.value + files.size * JobChainTaskCount))
     sleep(1.s)
     assert(nextTaskId == postId)
   }
