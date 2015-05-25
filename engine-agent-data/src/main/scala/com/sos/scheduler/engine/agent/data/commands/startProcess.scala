6f998c8c1cd4db70d5880d0bda86fe432790a0bf
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-25 10:31:00 +0200

    - clean-up

--- engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/commands/startProcess.scala
+++ engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/commands/startProcess.scala
@@ -1,17 +1,11 @@
 package com.sos.scheduler.engine.agent.data.commands
 
-import com.sos.scheduler.engine.agent.data.AgentProcessId
-
 /**
  * @author Joacim Zschimmer
  */
-sealed trait StartProcess extends ProcessCommand {
+trait StartProcess extends ProcessCommand {
   type Response = StartProcessResponse
   val controllerAddress: String
-}
-
-object StartProcess {
-  val XmlElementName = "remote_scheduler.start_remote_task"
 }
 
 final case class StartThread(controllerAddress: String)
@@ -19,5 +13,3 @@ extends StartProcess
 
 final case class StartSeparateProcess(controllerAddress: String, javaOptions: String, javaClasspath: String)
 extends StartProcess
-
-
