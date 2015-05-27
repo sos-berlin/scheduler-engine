a5013748b6700ef10cb4450e185a3d1b98a52024
Joacim Zschimmer <jogit@zschimmer.com>
2015-05-27 19:11:09 +0200

    JS-1300 Test for distributed job chain, too

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1300/JS1300IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1300/JS1300IT.scala
@@ -8,14 +8,18 @@ import com.sos.scheduler.engine.data.job.JobPath
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
 import com.sos.scheduler.engine.data.log.InfoLogEvent
 import com.sos.scheduler.engine.data.message.MessageCode
-import com.sos.scheduler.engine.data.order.{OrderFinishedEvent, OrderTouchedEvent}
+import com.sos.scheduler.engine.data.order.{OrderFinishedEvent, OrderKey, OrderTouchedEvent}
+import com.sos.scheduler.engine.kernel.persistence.hibernate.HibernateOrderStore
+import com.sos.scheduler.engine.kernel.persistence.hibernate.ScalaHibernate._
 import com.sos.scheduler.engine.test.EventBusTestFutures.implicits.RichEventBus
 import com.sos.scheduler.engine.test.SchedulerTestUtils._
+import com.sos.scheduler.engine.test.configuration.TestConfiguration
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import com.sos.scheduler.engine.tests.jira.js1300.JS1300IT._
 import java.nio.file.Files.delete
 import java.nio.file.Path
 import java.time.Duration
+import javax.persistence.EntityManagerFactory
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
@@ -28,56 +32,74 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 final class JS1300IT extends FreeSpec with ScalaSchedulerTest with AgentTest {
 
+  override protected lazy val testConfiguration = TestConfiguration(getClass, mainArguments = List("-distributed-orders"))
+  private implicit lazy val entityManagerFactory = instance[EntityManagerFactory]
   private val directory = testEnvironment.newFileOrderSourceDirectory()
 
-  "Some files, one after the other" in {
-    val repeat = 3600.s
-    scheduler executeXml newJobChainElem(directory, agentUri = agentUri, JobPath("/test-delete"), repeat = repeat)
-    for (_ ← 1 to 3) {
-      val file = directory / "TEST-DELETE"
-      val orderKey = TestJobChainPath orderKey file.toString
-      eventBus.awaitingEvent[InfoLogEvent](_.codeOption contains MessageCode("SCHEDULER-981")) {
-        eventBus.awaitingKeyedEvent[OrderFinishedEvent](orderKey) {
-          touch(file)
+  for (isDistributed ← List(false, true)) {
+    (if (isDistributed) "Distributed" else "Not distributed") - {
+      "Some files, one after the other" in {
+        val repeat = if (isDistributed) 1.s else 3600.s  // Short period when distributed, because JobScheduler cannot immediately check file existence via Agent (because the order vanishes)
+        scheduler executeXml newJobChainElem(directory, agentUri = agentUri, JobPath("/test-delete"), repeat = repeat, isDistributed = isDistributed)
+        for (_ ← 1 to 3) {
+          val file = directory / "TEST-DELETE"
+          val orderKey = TestJobChainPath orderKey file.toString
+          eventBus.awaitingEvent[InfoLogEvent](_.codeOption contains MessageCode("SCHEDULER-981")) {
+            eventBus.awaitingKeyedEvent[OrderFinishedEvent](orderKey) {
+              touch(file)
+            }
+          }
+        }
+      }
+
+      "A file, not deleted by job chain, stays on blacklist" in {
+        val repeat = 1.s
+        val delay = repeat dividedBy 2
+        scheduler executeXml newJobChainElem(directory, agentUri = agentUri, JobPath("/test-dont-delete"), repeat, isDistributed = isDistributed)
+        val file = directory / "TEST-DONT-DELETE"
+        val orderKey = TestJobChainPath orderKey file.toString
+        eventBus.awaitingEvent[InfoLogEvent](_.codeOption contains MessageCode("SCHEDULER-981")) {
+          eventBus.awaitingKeyedEvent[OrderFinishedEvent](orderKey) {
+            touch(file)
+          }
+          val startedAgain = eventBus.keyedEventFuture[OrderTouchedEvent](orderKey)
+          assert(orderIsOnBlacklist(orderKey))
+          sleep(repeat + delay)
+          assert(orderIsOnBlacklist(orderKey))
+          assert(!startedAgain.isCompleted)
+          delete(file)
+        }
+        assert(!orderExists(orderKey))
+        eventBus.awaitingEvent[InfoLogEvent](_.codeOption contains MessageCode("SCHEDULER-981")) {
+          eventBus.awaitingKeyedEvent[OrderFinishedEvent](orderKey) {
+            val started = eventBus.keyedEventFuture[OrderTouchedEvent](orderKey)
+            assert(!started.isCompleted)
+            sleep(repeat + delay)
+            assert(!started.isCompleted)
+            touch(file)
+          }
+          assert(orderIsOnBlacklist(orderKey))
+          delete(file)
         }
       }
     }
   }
 
-  "A file, not deleted by job chain, stays on blacklist" in {
-    val repeat = 1.s
-    val delay = repeat dividedBy 2
-    scheduler executeXml newJobChainElem(directory, agentUri = agentUri, JobPath("/test-dont-delete"), repeat)
-    val file = directory / "TEST-DONT-DELETE"
-    val orderKey = TestJobChainPath orderKey file.toString
-    eventBus.awaitingEvent[InfoLogEvent](_.codeOption contains MessageCode("SCHEDULER-981")) {
-      eventBus.awaitingKeyedEvent[OrderFinishedEvent](orderKey) {
-        touch(file)
+  private def orderIsOnBlacklist(orderKey: OrderKey): Boolean =
+    if (jobChain(orderKey.jobChainPath).isDistributed)
+      transaction { implicit entityManager ⇒
+        val e = instance[HibernateOrderStore].fetch(orderKey)
+        e.isOnBlacklist
       }
-      val startedAgain = eventBus.keyedEventFuture[OrderTouchedEvent](orderKey)
-      assert(order(orderKey).isOnBlacklist)
-      sleep(repeat + delay)
-      assert(order(orderKey).isOnBlacklist)
-      assert(!startedAgain.isCompleted)
-      delete(file)
-    }
-    assert(!orderExists(orderKey))
-    eventBus.awaitingKeyedEvent[OrderFinishedEvent](orderKey) {
-      val started = eventBus.keyedEventFuture[OrderTouchedEvent](orderKey)
-      assert(!started.isCompleted)
-      sleep(repeat + delay)
-      assert(!started.isCompleted)
-      touch(file)
-    }
-    assert(order(orderKey).isOnBlacklist)
-  }
+    else
+      order(orderKey).isOnBlacklist
 }
 
 private object JS1300IT {
   private val TestJobChainPath = JobChainPath("/test")
 
-  private def newJobChainElem(directory: Path, agentUri: String, jobPath: JobPath, repeat: Duration): xml.Elem =
-    <job_chain name={TestJobChainPath.withoutStartingSlash}>
+  private def newJobChainElem(directory: Path, agentUri: String, jobPath: JobPath, repeat: Duration, isDistributed: Boolean): xml.Elem =
+    <job_chain name={TestJobChainPath.withoutStartingSlash} distributed={isDistributed.toString}>
       <file_order_source directory={directory.toString} remote_scheduler={agentUri} repeat={repeat.getSeconds.toString}/>
       <job_chain_node state="100" job={jobPath.string}/>
       <job_chain_node.end state="END"/>
