9e005ea0f93f7a0c380293e5251a7f3cce5900dc
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-08 15:15:59 +0200

    JS-1371 Spooler.create_xslt_stylesheet is not supported

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1291/TestApiJob.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1291/TestApiJob.scala
@@ -1,5 +1,6 @@
 package com.sos.scheduler.engine.tests.jira.js1291
 
+import java.io.File
 import java.nio.file.Paths
 import org.scalatest.Matchers._
 
@@ -13,6 +14,9 @@ final class TestApiJob extends sos.spooler.Job_impl {
     checkUnsupportedMethod("sos.spooler.Spooler.include_path") { spooler.include_path }
     checkUnsupportedMethod("sos.spooler.Spooler.ini_path") { spooler.ini_path }
     checkUnsupportedMethod("sos.spooler.Spooler.log_dir") { spooler.log_dir }
+    checkUnsupportedMethod("sos.spooler.Spooler.create_xslt_stylesheet") { spooler.create_xslt_stylesheet() }
+    checkUnsupportedMethod("sos.spooler.Spooler.create_xslt_stylesheet") { spooler.create_xslt_stylesheet("x") }
+    checkUnsupportedMethod("sos.spooler.Spooler.create_xslt_stylesheet") { spooler.create_xslt_stylesheet(new File("x")) }
     checkUnsupportedMethod("sos.spooler.Task.create_subprocess") { spooler_task.create_subprocess() }
     checkUnsupportedMethod("sos.spooler.Task.create_subprocess") { spooler_task.create_subprocess("x") }
     checkUnsupportedMethod("sos.spooler.Task.create_subprocess") { spooler_task.create_subprocess(Array("x")) }
