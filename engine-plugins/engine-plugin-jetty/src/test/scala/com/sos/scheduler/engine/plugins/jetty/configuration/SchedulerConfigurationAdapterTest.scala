83485bdd496cfc9078fd267e9667faed2b1c3a3b
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-12 18:09:09 +0200

    Revert "ScalaXMLEventReader: XmlException extracted and prepared for PublicException"
    This reverts commit a8f0ff4b14567e5a9087aac1b9c7563511636c42.

--- engine-plugins/engine-plugin-jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/configuration/SchedulerConfigurationAdapterTest.scala
+++ engine-plugins/engine-plugin-jetty/src/test/scala/com/sos/scheduler/engine/plugins/jetty/configuration/SchedulerConfigurationAdapterTest.scala
@@ -1,7 +1,7 @@
 package com.sos.scheduler.engine.plugins.jetty.configuration
 
 import com.sos.scheduler.engine.common.scalautil.SideEffect._
-import com.sos.scheduler.engine.common.scalautil.xmls.XmlException
+import com.sos.scheduler.engine.common.scalautil.xmls.ScalaXMLEventReader.XmlException
 import com.sos.scheduler.engine.common.xml.XmlUtils.loadXml
 import com.sos.scheduler.engine.kernel.scheduler.SchedulerConfiguration
 import com.sos.scheduler.engine.plugins.jetty.configuration.JettyConfiguration.{FixedTcpPortNumber, LazyRandomTcpPortNumber, WarEntry}
