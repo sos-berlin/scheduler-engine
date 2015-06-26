a5b6ec99c59c5dbe6ad63411c194176f57b42655
Joacim Zschimmer <jogit@zschimmer.com>
2015-06-26 08:41:05 +0200

    JS-1386 Tunnel: StartThread and StartSeparateProcess prepared for tunnel

--- engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/responses/StartProcessResponse.scala
+++ engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/responses/StartProcessResponse.scala
@@ -1,8 +1,12 @@
 package com.sos.scheduler.engine.agent.data.responses
 
 import com.sos.scheduler.engine.agent.data.AgentProcessId
+import com.sos.scheduler.engine.tunnel.data.TunnelId
 
 /**
  * @author Joacim Zschimmer
  */
-final case class StartProcessResponse(processId: AgentProcessId) extends Response
+final case class StartProcessResponse(
+  processId: AgentProcessId,
+  tunnelIdWithPasswordOption: Option[TunnelId.WithPassword])
+extends Response
