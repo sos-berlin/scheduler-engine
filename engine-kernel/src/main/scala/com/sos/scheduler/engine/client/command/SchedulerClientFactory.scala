54d0d72fa32bf6fb1ad3972a94193e2214f5e5c8
Joacim Zschimmer <jogit@zschimmer.com>
2015-07-03 01:05:03 +0200

    JS-1386 SchedulerClientFactory with content type application/xml and correct decoding

--- engine-kernel/src/main/scala/com/sos/scheduler/engine/client/command/SchedulerClientFactory.scala
+++ engine-kernel/src/main/scala/com/sos/scheduler/engine/client/command/SchedulerClientFactory.scala
@@ -5,12 +5,14 @@ import com.sos.scheduler.engine.client.command.RemoteSchedulers.checkResponseFor
 import com.sos.scheduler.engine.client.command.SchedulerClientFactory._
 import com.sos.scheduler.engine.common.scalautil.Logger
 import com.sos.scheduler.engine.common.scalautil.xmls.{SafeXML, StringSource}
+import com.sos.scheduler.engine.common.sprayutils.XmlString
 import java.io.ByteArrayInputStream
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.control.NonFatal
 import spray.client.pipelining._
+import spray.http.HttpHeaders.Accept
 import spray.http.MediaTypes._
 import spray.http.Uri.Path
 import spray.http.{HttpEntity, HttpRequest, Uri}
@@ -34,12 +36,13 @@ final class SchedulerClientFactory @Inject private(implicit actorSystem: ActorSy
     case _ ⇒ throw new IllegalArgumentException(s"Invalid JobScheduler URL: $baseUri")
   }
 
-  private val pipeline: HttpRequest ⇒ Future[String] =
+  private val pipeline: HttpRequest ⇒ Future[XmlString] =
     // JettyPlugin dekomprimiert Request nicht: encode(Gzip) ~>
+    addHeader(Accept(`application/xml`, `text/xml`)) ~>
       sendReceive ~>
       decode(Deflate) ~>
       decode(Gzip) ~>
-      unmarshal[String]
+      unmarshal[XmlString]
 
   def apply(baseUri: Uri): SchedulerCommandClient = new SchedulerCommandClient {
     def executeXml(xmlBytes: Array[Byte]): Future[String] =
@@ -56,12 +59,12 @@ final class SchedulerClientFactory @Inject private(implicit actorSystem: ActorSy
 
     def uncheckedExecuteXml(xmlBytes: Array[Byte]): Future[String] = {
       logger.debug(s"POST ${commandUri(baseUri)} ${try SafeXML.load(new ByteArrayInputStream(xmlBytes)) catch { case NonFatal(t) ⇒ t }})}")
-      pipeline(Post(commandUri(baseUri), XmlBytes(xmlBytes))(XmlBytesMarshaller))
+      pipeline(Post(commandUri(baseUri), XmlBytes(xmlBytes))(XmlBytesMarshaller)) map { _.string }
     }
 
     def uncheckedExecute(elem: xml.Elem): Future[String] = {
       logger.debug(s"POST ${commandUri(baseUri)} $elem")
-      pipeline(Post(commandUri(baseUri), elem))
+      pipeline(Post(commandUri(baseUri), elem)) map { _.string }
     }
   }
 }
