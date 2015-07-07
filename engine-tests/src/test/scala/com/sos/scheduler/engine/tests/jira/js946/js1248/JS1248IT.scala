8258c598536ff5df600bdf99dc605c7d5ebf3405
Joacim Zschimmer <jogit@zschimmer.com>
2015-07-07 14:37:30 +0200

    JS-1248 Simplified test case - not failing

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js946/js1248/JS1248IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js946/js1248/JS1248IT.scala
@@ -5,10 +5,11 @@ import com.sos.scheduler.engine.common.time.ScalaTime._
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
 import com.sos.scheduler.engine.data.order.OrderFinishedEvent
 import com.sos.scheduler.engine.data.xmlcommands.OrderCommand
+import com.sos.scheduler.engine.kernel.settings.CppSettingName
 import com.sos.scheduler.engine.test.EventBusTestFutures.implicits.RichEventBus
 import com.sos.scheduler.engine.test.SchedulerTestUtils._
+import com.sos.scheduler.engine.test.configuration.TestConfiguration
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
-import com.sos.scheduler.engine.tests.jira.js946.js1248.JS1248IT._
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
@@ -22,15 +23,24 @@ import scala.concurrent.Future
 @RunWith(classOf[JUnitRunner])
 final class JS1248IT extends FreeSpec with ScalaSchedulerTest {
 
+  override protected lazy val testConfiguration = TestConfiguration(getClass,
+    mainArguments = List("-tcp-port=3333"),
+    cppSettings = Map(CppSettingName.htmlDir → """C:\sos\all\joc\joc-html-cockpit\target\classes"""))
+
+  "15 orders, a job with 2 tasks and a process class with 3 process" in {
+    if (isUnix) pending
+    runOrders(JobChainPath("/test"), 15)
+  }
+
   "15 orders and a process class with 5 processes" in {
     if (isUnix) pending
-    val allFinished = Future.sequence(for (orderKey ← TestOrderKeys) yield eventBus.keyedEventFuture[OrderFinishedEvent](orderKey))
-    for (orderKey ← TestOrderKeys) scheduler executeXml OrderCommand(orderKey)
+    runOrders(JobChainPath("/01_JobChainA"), 15)
+  }
+
+  private def runOrders(jobChainPath: JobChainPath, n: Int): Unit = {
+    val orderKeys = for (i ← 1 to n) yield jobChainPath orderKey s"$i"
+    val allFinished = Future.sequence(for (orderKey ← orderKeys) yield eventBus.keyedEventFuture[OrderFinishedEvent](orderKey))
+    for (orderKey ← orderKeys) scheduler executeXml OrderCommand(orderKey)
     awaitSuccess(allFinished)(300.s)
   }
-}
-
-private object JS1248IT {
-  private val TestJobChainPath = JobChainPath("/01_JobChainA")
-  private val TestOrderKeys = for (i ← 1 to 15) yield TestJobChainPath orderKey s"$i"
 }
