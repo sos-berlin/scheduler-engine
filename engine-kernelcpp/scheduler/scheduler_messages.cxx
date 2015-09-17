c24e2a214d3c844a99fa88aa6e037afdd3c8caab
Joacim Zschimmer <jogit@zschimmer.com>
2015-09-17 13:15:03 +0200

    JS-1481 <job_chain process_class> process class limit may terminate idling task

--- engine-kernelcpp/scheduler/scheduler_messages.cxx
+++ engine-kernelcpp/scheduler/scheduler_messages.cxx
@@ -361,7 +361,7 @@ Message_code_text scheduler_messages[] =
     { "SCHEDULER-946", "setback(): order has been set back $1 times, until $2" },
     { "SCHEDULER-947", "setback(): order has been set back $1 times. This is more than $2 - the maximum for this job" },
     { "SCHEDULER-948", "Process $1 started" },
-    { "SCHEDULER-949", "Execution is deferred a process of process class '$1' becomes available" },
+    { "SCHEDULER-949", "Execution is deferred until a process of process class '$1' becomes available" },
     { "SCHEDULER-950", "Scheduler has been registered" },
     { "SCHEDULER-951", "Daylight saving time has begun" },
     { "SCHEDULER-952", "Daylight saving time has ended, now it's standard time" },
