b70c25a1e3d46c03168b94340166d3f6e7297c1a
Joacim Zschimmer <jogit@zschimmer.com>
2015-09-28 12:39:56 +0200

    agent-gui source files ignored

--- engine-agent-gui/src/main/resources/com/sos/scheduler/engine/agent/gui/frontend/filters/percentage.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='../_all.ts' />
-(function (angular) {
-    angular.module('agent')
-        .filter('percentage', ['$filter', function ($filter) {
-            return (input, decimals) => $filter('number')(input * 100, decimals) + '%'
-        }])
-}(angular))
