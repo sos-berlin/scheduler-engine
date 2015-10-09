02d3f7ac01ec2c7ebc7d9fe0e3ce83d6971372af
Joacim Zschimmer <jogit@zschimmer.com>
2015-10-09 09:22:31 +0200

    JS-1031 Test improved

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1031/JS1031IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1031/JS1031IT.scala
@@ -1,5 +1,6 @@
 package com.sos.scheduler.engine.tests.jira.js1031
 
+import com.sos.scheduler.engine.common.scalautil.Logger
 import com.sos.scheduler.engine.common.time.ScalaJoda._
 import com.sos.scheduler.engine.data.order.{OrderKey, OrderTouchedEvent}
 import com.sos.scheduler.engine.data.schedule.SchedulePath
@@ -8,6 +9,7 @@ import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import com.sos.scheduler.engine.tests.jira.js1031.JS1031IT._
 import org.joda.time.DateTimeZone
 import org.joda.time.Instant.now
+import org.joda.time.Instant
 import org.joda.time.format.ISODateTimeFormat
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
@@ -21,20 +23,22 @@ final class JS1031IT extends FreeSpec with ScalaSchedulerTest {
 
   override def onBeforeSchedulerActivation() = eventPipe
 
-  "Missing test.schedule.xml should prevent order start" in {
+  "Missing test.schedule.xml prevents order start" in {
     assert(eventPipe.queued[OrderTouchedEvent].isEmpty)
-    val at = now() + 5.s
-    writeConfigurationFile(TestSchedulePath,
-      <schedule>
-        <at at={ISODateTimeFormat.dateHourMinuteSecond withZone DateTimeZone.getDefault print at}/>
-      </schedule>)
-    sleep(2.s)
+    val at = new Instant((now() + 5.s).getMillis / 1000 * 1000)
+    val scheduleElem = <schedule><at at={ISODateTimeFormat.dateHourMinuteSecond withZone DateTimeZone.getDefault print at}/></schedule>
+    logger.info(s"$scheduleElem")
+    writeConfigurationFile(TestSchedulePath, scheduleElem)
+    logger.info("sleep")
+    sleep(at - 1.s - now())
+    logger.info("Order should not yet be touched")
     assert(eventPipe.queued[OrderTouchedEvent].isEmpty)
-    eventPipe.nextKeyed[OrderTouchedEvent](TestOrderKey, timeout = 5.s)
+    eventPipe.nextKeyed[OrderTouchedEvent](TestOrderKey, timeout = at + 1.s - now)
   }
 }
 
 private object JS1031IT {
+  private val logger = Logger(getClass)
   private val TestOrderKey = OrderKey("/test", "1")
   private val TestSchedulePath = SchedulePath("/test")
 }
