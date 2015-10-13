3e5d8115018b0cc94205ff430b07607f1c08cd07
Joacim Zschimmer <jogit@zschimmer.com>
2015-10-13 23:11:05 +0200

    JS-1492 Test, not failing: JobScheduler hangs if an order job has an end time in its current run time period and the task of the job needs more time

---
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1492/JS1492IT.scala
@@ -0,0 +1,68 @@
+package com.sos.scheduler.engine.tests.jira.js1492
+
+import com.sos.scheduler.engine.common.scalautil.AutoClosing.autoClosing
+import com.sos.scheduler.engine.common.scalautil.Logger
+import com.sos.scheduler.engine.common.system.OperatingSystem.isWindows
+import com.sos.scheduler.engine.common.utils.FreeTcpPortFinder.findRandomFreeTcpPort
+import com.sos.scheduler.engine.data.job.JobPath
+import com.sos.scheduler.engine.common.time.ScalaJoda._
+import com.sos.scheduler.engine.test.SchedulerTestUtils._
+import com.sos.scheduler.engine.test.configuration.TestConfiguration
+import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
+import com.sos.scheduler.engine.tests.jira.js1492.JS1492IT._
+import java.io.OutputStreamWriter
+import java.net.Socket
+import java.nio.charset.StandardCharsets.UTF_8
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.{DateTimeZone, Instant}
+import org.joda.time.Instant.now
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * JS-1492 JobScheduler hangs if an order job has an end time in its current run time period and the task of the job needs more time.
+ *
+ * @author Joacim Zschimmer
+ */
+@RunWith(classOf[JUnitRunner])
+final class JS1492IT extends FreeSpec with ScalaSchedulerTest {
+
+  private lazy val tcpPort = findRandomFreeTcpPort()
+  override protected lazy val testConfiguration = TestConfiguration(getClass, mainArguments = List(s"-tcp-port=$tcpPort"))
+
+  "JS-1492" in {
+    val periodEnd = now() + 3.s
+    val expectedTaskEnd = now() + 5.s
+    writeConfigurationFile(TestJobPath,
+      <job>
+        <script language="shell">{
+          if (isWindows) "ping -n 6 127.0.0.1 >nul" else "sleep 5"
+        }</script>
+        <run_time>
+          <period end={DateTimeFormat forPattern "HH:mm:ss" withZone DateTimeZone.getDefault print periodEnd}/>
+        </run_time>
+      </job>)
+    val run = runJobFuture(TestJobPath)
+    sleep(periodEnd + 100.ms - now())
+    checkTcp()
+    assert(now() < expectedTaskEnd - 1.s)
+    val result = awaitSuccess(run.result)
+    assert(now() > expectedTaskEnd)
+    assert(result.duration >= 5.s)
+  }
+
+  private def checkTcp(): Unit =
+    autoClosing(new Socket("127.0.0.1", tcpPort)) { socket ⇒
+      val writer = new OutputStreamWriter(socket.getOutputStream, UTF_8)
+      writer.write("<show_state/>")
+      writer.flush()
+      assert(socket.getInputStream.read() == '<')
+      logger.info("TCP works")
+    }
+}
+
+private object JS1492IT {
+  private val logger = Logger(getClass)
+  private val TestJobPath = JobPath("/test")
+}
