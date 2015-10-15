3172ac6c30cd4b3c6502e4ff5d2938c152ba2d4f
Joacim Zschimmer <jogit@zschimmer.com>
2015-10-15 10:55:39 +0200

    JS-1191 Order.last_error

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1191/JS1191IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1191/JS1191IT.scala
@@ -1,14 +1,20 @@
 package com.sos.scheduler.engine.tests.jira.js1191
 
+import com.google.common.base.Strings.nullToEmpty
 import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits._
 import com.sos.scheduler.engine.data.filebased.{FileBasedAddedEvent, FileBasedRemovedEvent}
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
-import com.sos.scheduler.engine.data.order.{OrderFinishedEvent, OrderState, OrderStateChangedEvent}
+import com.sos.scheduler.engine.data.order._
+import com.sos.scheduler.engine.kernel.persistence.hibernate.HibernateOrderStepStore
+import com.sos.scheduler.engine.kernel.persistence.hibernate.RichEntityManager.toRichEntityManager
+import com.sos.scheduler.engine.kernel.persistence.hibernate.ScalaHibernate._
+import com.sos.scheduler.engine.persistence.entities.OrderStepEntity
 import com.sos.scheduler.engine.test.EventBusTestFutures.implicits.RichEventBus
 import com.sos.scheduler.engine.test.SchedulerTestUtils._
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import com.sos.scheduler.engine.tests.jira.js1191.JS1191IT._
 import java.nio.file.{Files, Paths}
+import javax.persistence.EntityManagerFactory
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
@@ -22,10 +28,20 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 final class JS1191IT extends FreeSpec with ScalaSchedulerTest {
 
+  private implicit lazy val entityManager = instance[EntityManagerFactory]
+  private implicit lazy val orderStepStore = instance[HibernateOrderStepStore]
+
   "Order.last_error" in {
     controller.toleratingErrorLogEvent(_ ⇒ true) {
-      val result = runOrder(JobChainPath("/test") orderKey "1")
+      val result = runOrder(FirstOrderKey)
       assert(result.state == OrderState("END"))
+    }
+  }
+
+  "Database" in {
+    transaction { implicit entityManager ⇒
+      for (entity ← entityManager.fetchSeq[OrderStepEntity]("select e from OrderStepEntity e"))
+        TestJob.settingFor(entity.state).check(nullToEmpty(entity.errorText))
     }
   }
 
@@ -53,4 +69,5 @@ final class JS1191IT extends FreeSpec with ScalaSchedulerTest {
 
 private object JS1191IT {
   private val TestJobChainPath = JobChainPath("/test")
+  private val FirstOrderKey = TestJobChainPath orderKey "1"
 }
--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1191/TestJob.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1191/TestJob.scala
@@ -10,37 +10,40 @@ import scala.xml.XML
 final class TestJob extends sos.spooler.Job_impl {
 
   override def spooler_process() = {
-    for (previousState ← order.job_chain.states sliding 2 collectFirst { case Array(previous, state) if state == order.state ⇒ previous })
-      settingFor(previousState).check()
+    val order = spooler_task.order
+    for (previousState ← order.job_chain.states sliding 2 collectFirst { case Array(previous, state) if state == order.state ⇒ previous }) {
+      settingFor(previousState).check(order.last_error)
+    }
     for (orderShow ← Some(spooler execute_xml <show_order job_chain={order.job_chain.path} order={order.id}/>.toString))
       assert((XML.loadString(orderShow) \ "answer" \ "order" \ "@last_error").text == order.last_error)
-    settingFor(order.state).spooler_process()
+    settingFor(order.state).spooler_process(this)
   }
-
-  private def settingFor(orderState: String) = orderState match {
-    case o if o startsWith "NO-ERROR-" ⇒ Setting(
-      () ⇒ true,
-      () ⇒ assert(order.last_error.isEmpty))
-    case "SHELL" ⇒ Setting(
-      () ⇒ throw new NotImplementedError,
-      () ⇒ assert(order.last_error == "SCHEDULER-280  Process terminated with exit code 7 (0x7)"))
-    case "LOG-ERROR-TRUE" ⇒ Setting(
-      () ⇒ { spooler_log.error("ERROR LOG LINE, RETURN TRUE"); true },
-      () ⇒ assert(order.last_error.isEmpty))
-    case "LOG-ERROR-FALSE" ⇒ Setting(
-      () ⇒ { spooler_log.error("ERROR LOG LINE AND RETURNING FALSE"); false },
-      () ⇒ assert(order.last_error == s"SCHEDULER-140  Task logged error: ERROR LOG LINE AND RETURNING FALSE"))
-    case "EXCEPTION" ⇒ Setting(
-      () ⇒ throw new RuntimeException("EXCEPTION THROWN"),
-      () ⇒ assert(order.last_error contains "EXCEPTION THROWN"))
-    case "LAST" ⇒ Setting(
-      () ⇒ true,
-      () ⇒ ())
-  }
-
-  def order = spooler_task.order
 }
 
-private object TestJob {
-  private case class Setting(spooler_process: () ⇒ Boolean, check: () ⇒ Unit)
+private[js1191] object TestJob {
+  private[js1191] case class Setting(spooler_process: sos.spooler.Job_impl ⇒ Boolean, check: String ⇒ Unit)
+
+  private[js1191] def settingFor(orderState: String) = orderState match {
+    case o if o startsWith "NO-ERROR-" ⇒ Setting(
+      job ⇒ true,
+      lastError ⇒ assert(lastError.isEmpty))
+    case o if o startsWith "NO-ERROR-FALSE" ⇒ Setting(
+      job ⇒ false,
+      lastError ⇒ assert(lastError.isEmpty))
+    case "SHELL" ⇒ Setting(
+      job ⇒ throw new NotImplementedError,
+      lastError ⇒ assert(lastError == "SCHEDULER-280  Process terminated with exit code 7 (0x7)"))
+    case "LOG-ERROR-TRUE" ⇒ Setting(
+      job ⇒ { job.spooler_log.error("ERROR LOG LINE, RETURN TRUE"); true },
+      lastError ⇒ assert(lastError.isEmpty))
+    case "LOG-ERROR-FALSE" ⇒ Setting(
+      job ⇒ { job.spooler_log.error("ERROR LOG LINE AND RETURNING FALSE"); false },
+      lastError ⇒ assert(lastError == s"SCHEDULER-140  Task logged error: ERROR LOG LINE AND RETURNING FALSE"))
+    case "EXCEPTION" ⇒ Setting(
+      job ⇒ throw new RuntimeException("EXCEPTION THROWN"),
+      lastError ⇒ assert(lastError contains "EXCEPTION THROWN"))
+    case "LAST" ⇒ Setting(
+      job ⇒ true,
+      lastError ⇒ assert(lastError.isEmpty))
+  }
 }
