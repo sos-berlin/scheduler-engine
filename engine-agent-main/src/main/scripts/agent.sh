9a3f80d57237f2978b271c52344889ca92f5309b
Joacim Zschimmer <jogit@zschimmer.com>
2015-11-18 12:55:10 +0100

    JS-1523 agent.sh -rmx-port=... -intellij-port=...

--- engine-agent-main/src/main/scripts/agent.sh
+++ engine-agent-main/src/main/scripts/agent.sh
@@ -1,6 +1,32 @@
 #! /bin/bash
 set -e
 
+declare -a javaOptions
+declare -a agentOptions
+
+for arg in "$@"; do
+    case $arg in
+        -rmx-port=*)
+            port=${arg/-rmx-port=/}
+            javaOptions=(
+                "-Dcom.sun.management.jmxremote"
+                "-Dcom.sun.management.jmxremote.ssl=false"
+                "-Dcom.sun.management.jmxremote.authenticate=false"
+                "-Dcom.sun.management.jmxremote.port=$port")
+            shift
+            ;;
+        -intellij-port=*)
+            port=${arg/-intellij-port=/}
+            javaOptions=("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$port")
+            shift
+            ;;
+        *)
+            agentOptions+=("$arg")
+            shift
+            ;;
+    esac
+done
+
 if [ -z "$SCHEDULER_AGENT_HOME" ]; then :
     SCHEDULER_AGENT_HOME=$(cd "$(dirname "$0")/.."; pwd)
 fi
@@ -12,4 +38,5 @@ else
     logbackConfig="com/sos/scheduler/engine/agent/main/logback.xml"
 fi
 
-exec "$java" -classpath "$jarDir/*" -Dlogback.configurationFile="$logbackConfig" com.sos.scheduler.engine.agent.main.AgentMain "$@"
+echo "$java" "${javaOptions[@]}" -classpath "$jarDir/*" -Dlogback.configurationFile="$logbackConfig" com.sos.scheduler.engine.agent.main.AgentMain "${agentOptions[@]}"
+exec "$java" "${javaOptions[@]}" -classpath "$jarDir/*" -Dlogback.configurationFile="$logbackConfig" com.sos.scheduler.engine.agent.main.AgentMain "${agentOptions[@]}"
