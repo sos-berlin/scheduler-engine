487522d0065be05941625c61712ca8f5d97a6550
Joacim Zschimmer <jogit@zschimmer.com>
2015-12-01 12:35:01 +0100

    JS-1523 AlarmClock default idleTimeout = None

--- engine-http-server/src/test/scala/com/sos/scheduler/engine/http/server/heartbeat/HeartbeatTest.scala
+++ engine-http-server/src/test/scala/com/sos/scheduler/engine/http/server/heartbeat/HeartbeatTest.scala
@@ -41,7 +41,7 @@ final class HeartbeatTest extends FreeSpec with BeforeAndAfterAll {
   private implicit val askTimeout = AskTimeout
   private implicit lazy val actorSystem = ActorSystem("TEST")
   import actorSystem.dispatcher
-  private val alarmClock = new AlarmClock(1.ms)
+  private val alarmClock = new AlarmClock(1.ms, idleTimeout = Some(1.s))
   private lazy val heartbeatService = new HeartbeatService(alarmClock)
   private implicit val dataJsonFormat = Data.jsonFormat
   private lazy val (baseUri, webService) = startWebServer(heartbeatService)
