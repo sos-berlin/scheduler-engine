cfdc7e43186a476646d8a309e84fa44f898add5c
Joacim Zschimmer <jogit@zschimmer.com>
2015-12-03 10:35:46 +0100

    JS-1523 clean-up

--- engine-http-client/src/main/scala/com/sos/scheduler/engine/http/client/heartbeat/HeartbeatRequestHeaders.scala
+++ engine-http-client/src/main/scala/com/sos/scheduler/engine/http/client/heartbeat/HeartbeatRequestHeaders.scala
@@ -1,5 +1,6 @@
 package com.sos.scheduler.engine.http.client.heartbeat
 
+import com.sos.scheduler.engine.common.time.ScalaTime._
 import com.sos.scheduler.engine.http.client.common.{OwnHttpHeader, OwnHttpHeaderCompanion}
 import java.time.Duration
 
@@ -8,8 +9,7 @@ import java.time.Duration
   */
 object HeartbeatRequestHeaders {
 
-  private val DurationRegex = "[0-9.A-Za-z]+".r
-  private val TimesRegex = s"($DurationRegex) +($DurationRegex)".r
+  private val TimesRegex = s"($Iso8601DurationRegex) +($Iso8601DurationRegex)".r
 
   private def parseTimes(string: String) = string match {
     case TimesRegex(duration, timeout) ⇒ HttpHeartbeatTiming(Duration parse duration, Duration parse timeout)
@@ -30,7 +30,7 @@ object HeartbeatRequestHeaders {
     def value = s"${heartbeatId.string} ${timing.period} ${timing.timeout}"
   }
   object `X-JobScheduler-Heartbeat-Continue` extends OwnHttpHeaderCompanion {
-    private val ValueRegex = s"(${HeartbeatId.Regex}) +($DurationRegex +$DurationRegex)".r
+    private val ValueRegex = s"(${HeartbeatId.Regex}) +($Iso8601DurationRegex +$Iso8601DurationRegex)".r
 
     object Value {
       def unapply(value: String): Option[(HeartbeatId, HttpHeartbeatTiming)] =
