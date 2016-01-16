ed7d8f17b970d34e58bb0f02a19f676739670ee0
Joacim Zschimmer <jogit@zschimmer.com>
2016-01-16 09:30:27 +0100

    JS-1574 CrashKillScript with master's TaskId and job name

--- engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/ProcessKillScript.scala
+++ engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/ProcessKillScript.scala
@@ -1,6 +1,7 @@
 package com.sos.scheduler.engine.agent.data
 
 import com.sos.scheduler.engine.data.base.IsString
+import com.sos.scheduler.engine.data.job.{JobPath, TaskId}
 import java.nio.file.{Path, Paths}
 import scala.collection.immutable
 
@@ -9,6 +10,9 @@ import scala.collection.immutable
   */
 final case class ProcessKillScript(file: Path) extends IsString {
   def string = file.toString
+
+  def toCommandArguments(id: AgentTaskId, jobPath: JobPath, taskId: TaskId): immutable.Seq[String] =
+    toCommandArguments(id) ++ List(s"-kill-master-task-id=${taskId.string}", s"-job-name=${jobPath.name}")
 
   def toCommandArguments(id: AgentTaskId): immutable.Seq[String] = List(file.toString, s"-kill-agent-task-id=${id.string}")
 }
