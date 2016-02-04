705ccb284876b62cd8b1518dc353f2a26126f0bc
Joacim Zschimmer <jogit@zschimmer.com>
2016-02-04 16:32:18 +0100

    JS-1582 Test adapted for release/1.10

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1582/JS1582IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1582/JS1582IT.scala
@@ -1,14 +1,13 @@
 package com.sos.scheduler.engine.tests.jira.js1582
 
-import com.sos.scheduler.engine.common.time.ScalaJoda._
+import com.sos.scheduler.engine.common.scalautil.Futures.implicits.SuccessFuture
+import com.sos.scheduler.engine.common.time.ScalaTime._
 import com.sos.scheduler.engine.data.job.{JobPath, TaskStartedEvent}
 import com.sos.scheduler.engine.test.EventBusTestFutures.implicits.RichEventBus
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
 
 /**
   * @author Joacim Zschimmer
@@ -26,7 +25,7 @@ final class JS1582IT extends FreeSpec with ScalaSchedulerTest {
   }
 
   "min_tasks not allowed for shell jobs" in {
-    Await.result(apiStarted, 10.seconds)
+    apiStarted await 10.s
     sleep(2.s)
     if (shellStarted.isCompleted) fail(shellStarted.value.get.toString)
   }
