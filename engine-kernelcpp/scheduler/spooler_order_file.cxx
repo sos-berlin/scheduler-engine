71d8aee1aa48c0afbcdf8b2be563d8fbedd231dd
Joacim Zschimmer <jogit@zschimmer.com>
2016-03-30 09:11:37 +0200

    JS-1609 FIXED method call in constructor

--- engine-kernelcpp/scheduler/spooler_order_file.cxx
+++ engine-kernelcpp/scheduler/spooler_order_file.cxx
@@ -247,7 +247,7 @@ struct Directory_file_order_source : Directory_file_order_source_interface, Depe
 
 struct File_order_sink_job : Internal_job {
     File_order_sink_job(Scheduler* scheduler) :
-        Internal_job(scheduler, file_order_sink_job_path.without_slash(), new_internal_module(scheduler, log(), "FileOrderSink"))
+        Internal_job(scheduler, file_order_sink_job_path.without_slash(), new_internal_module(scheduler, (Prefix_log*)NULL, "FileOrderSink"))
     {
         set_idle_timeout(Duration(0));
     }
