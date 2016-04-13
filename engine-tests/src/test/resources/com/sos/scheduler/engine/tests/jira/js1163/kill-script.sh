5ef0bf329aa05996d5929b18f613d518b7372d0f
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-13 22:41:45 +0200

    JS-1609 Test kill-script.sh improved for Solaris

--- engine-tests/src/test/resources/com/sos/scheduler/engine/tests/jira/js1163/kill-script.sh
+++ engine-tests/src/test/resources/com/sos/scheduler/engine/tests/jira/js1163/kill-script.sh
@@ -9,6 +9,10 @@ for arg in "$@"; do
             agentTaskId="${arg#*=}"
             shift
             ;;
+        -pid=*)
+            pid="${arg#*=}"
+            shift
+            ;;
         *)
             echo "Unknown option: $arg"
             exit 101
@@ -16,16 +20,3 @@ for arg in "$@"; do
     esac
 done
 [ -n "$agentTaskId" ] || { echo Missing argument -kill-agent-task-id=; exit 102; }
-
-ps=$(mktemp)
-ps -ef >$ps
-count=$(grep --count --fixed-strings -- " -agent-task-id=$agentTaskId" <$ps || kill $$)
-rm $ps
-[ $count -eq 1 ] || {
-    echo "Exactly one process with -agent-task-id=$agentTaskId is expected, not: $count"
-    if [ $count -eq 0 ]; then
-        exit 100
-    else
-        exit $count
-    fi
-}
