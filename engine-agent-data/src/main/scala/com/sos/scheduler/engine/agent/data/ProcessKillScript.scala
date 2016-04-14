a1961e56b68120872e81fda7b076e46cfda8fe50
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-14 16:22:11 +0200

    JS-1574 Refactored kill script handling

--- engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/ProcessKillScript.scala
+++ engine-agent-data/src/main/scala/com/sos/scheduler/engine/agent/data/ProcessKillScript.scala
@@ -1,6 +1,7 @@
 package com.sos.scheduler.engine.agent.data
 
 import com.sos.scheduler.engine.base.generic.IsString
+import com.sos.scheduler.engine.common.process.Processes.Pid
 import com.sos.scheduler.engine.data.job.{JobPath, TaskId}
 import java.nio.file.{Path, Paths}
 import scala.collection.immutable
@@ -11,10 +12,11 @@ import scala.collection.immutable
 final case class ProcessKillScript(file: Path) extends IsString {
   def string = file.toString
 
-  def toCommandArguments(id: AgentTaskId, jobPath: JobPath, taskId: TaskId): immutable.Seq[String] =
-    toCommandArguments(id) ++ List(s"-master-task-id=${taskId.string}", s"-job=${jobPath.string}")
+  def toCommandArguments(id: AgentTaskId, pid: Option[Pid], jobPath: JobPath, taskId: TaskId): immutable.Seq[String] =
+    toCommandArguments(id, pid) :+ s"-master-task-id=${taskId.string}" :+ s"-job=${jobPath.string}"
 
-  def toCommandArguments(id: AgentTaskId): immutable.Seq[String] = List(file.toString, s"-kill-agent-task-id=${id.string}")
+  def toCommandArguments(id: AgentTaskId, pid: Option[Pid]): immutable.Seq[String] =
+    Vector(file.toString, s"-kill-agent-task-id=${id.string}") ++ (pid map { o ⇒ s"-pid=${o.string}" })
 }
 
 object ProcessKillScript extends IsString.HasJsonFormat[ProcessKillScript] {
