529e93636046d0132f0a8a73d030e02c31d4be05
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-14 14:41:54 +0200

    Refactoring: IsString moved from engine-data to engine-base

--- engine-kernel/src/main/scala/com/sos/scheduler/engine/kernel/filebased/SimpleFileBasedDetails.scala
+++ engine-kernel/src/main/scala/com/sos/scheduler/engine/kernel/filebased/SimpleFileBasedDetails.scala
@@ -1,8 +1,9 @@
 package com.sos.scheduler.engine.kernel.filebased
 
-import com.sos.scheduler.engine.data.base.JavaJsonFormats._
+import com.sos.scheduler.engine.base.generic.JavaJsonFormats
+import com.sos.scheduler.engine.base.generic.JavaJsonFormats._
 import com.sos.scheduler.engine.base.sprayjson.JavaTimeJsonFormats.implicits._
-import com.sos.scheduler.engine.data.filebased.{FileBasedState, FileBasedDetails, TypedPath}
+import com.sos.scheduler.engine.data.filebased.{FileBasedDetails, FileBasedState, TypedPath}
 import java.io.File
 import java.time.Instant
 import spray.json.DefaultJsonProtocol._
