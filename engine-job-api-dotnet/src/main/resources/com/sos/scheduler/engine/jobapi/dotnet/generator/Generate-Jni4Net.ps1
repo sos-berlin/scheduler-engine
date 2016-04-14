71338ea1019a9e97a8e108f11aa35109bc4280a4
SOS-RE <robert.ehrlich@sos-berlin.com>
2016-04-14 11:02:06 +0200

    JS-1595 Generator script - error handling, extract version from snapshot, remove copied files on error

--- engine-job-api-dotnet/src/main/resources/com/sos/scheduler/engine/jobapi/dotnet/generator/Generate-Jni4Net.ps1
+++ engine-job-api-dotnet/src/main/resources/com/sos/scheduler/engine/jobapi/dotnet/generator/Generate-Jni4Net.ps1
@@ -67,15 +67,17 @@ $env:APP_PATH                   = Split-Path $MyInvocation.MyCommand.Path
 # Script
 # ----------------------------------------------------------------------
 $Script:ProxyAssemblyBasename   = "com.sos-berlin.jobscheduler.dotnet.job-api.proxy" # This name is constant and can't be changed - the name defines the assembly name and is referenced by the another .dll files.
+$Script:ProxyOutputNamePrefix   = "com.sos-berlin.jobscheduler."
 $Script:AdapterAssemblyBasename = "com.sos-berlin.jobscheduler.dotnet.adapter"       # 
 $Script:ProxyGenDirectoryName   = "proxygen" 
 $Script:Jni4NetDlls             = @("jni4net.n-0.8.8.0.dll","jni4net.n.w32.v40-0.8.8.0.dll", "jni4net.n.w64.v40-0.8.8.0.dll")
 $Script:Jni4NetReferenceDll     = Join-Path $Script:ProxyGenDirectoryName "jni4net.n-0.8.8.0.dll"
 $Script:JDKBinDirectory         = Join-Path $Global:JDKDirectory "bin"
+$Script:CopiedFiles             = @()
 $Script:ErrorExitCode           = 99
 $Script:TempDirectory           = $null
 $Script:InputApiJar             = $null
-$Script:InputApiJarVersion      = $null
+$Script:ApiJarVersion           = $null
 $Script:TempApiJar              = $null
 $Script:OutDirProxyDll          = $null
 $Script:OutDirProxyJar          = $null
@@ -103,146 +105,228 @@ function Init([string] $outDirProxyDll,[string] $outDirProxyJar,[string] $dotnet
     $Script:DotnetAdapterSourceDir = $dotnetAdapterSourceDirectory
 }
 function CreateTempDirectory(){
-    $tempDirName            = "dotnet-proxy-"+[System.Guid]::NewGuid().ToString();
-    $tempDirPath            = Join-Path $env:Temp $tempDirName
-    $Script:TempDirectory   = New-Item -Type Directory -Path $tempDirPath -ea Stop
+    try
+    {
+        $tempDirName            = "dotnet-proxy-"+[System.Guid]::NewGuid().ToString();
+        $tempDirPath            = Join-Path $env:Temp $tempDirName
+        $Script:TempDirectory   = New-Item -Type Directory -Path $tempDirPath -ea Stop
+    }
+    catch{
+        throw "[CreateTempDirectory] $($_.Exception.ToString())"
+    }
 }
 function RemoveTempDirectory(){
-    Set-Location $env:APP_PATH
+    try
+    {
+        Set-Location $env:APP_PATH
     
-    if(Test-Path($Script:TempDirectory.Fullname)){
-        Remove-Item -path $Script:TempDirectory.Fullname -Recurse -Force -ea Stop
+        if(Test-Path($Script:TempDirectory.Fullname)){
+            Remove-Item -path $Script:TempDirectory.Fullname -Recurse -Force -ea Stop
+        }
+    }
+    catch{
+        throw "[RemoveTempDirectory] $($_.Exception.ToString())"
     }
 }
 function CreateNewApiJarForProxy($jobApiJar){
-    $Script:InputApiJar = Get-Item $jobApiJar
+    try
+    {
+        $Script:InputApiJar = Get-Item $jobApiJar
     
-    SetApiJarVersion
+        SetApiJarVersion
     
-    Set-Location $Script:TempDirectory
+        Set-Location $Script:TempDirectory
     
-    [Array]$arguments   = "xvf",$jobApiJar
-    ExecuteCommand "jar" $arguments;
+        [Array]$arguments   = "xvf",$jobApiJar
+        ExecuteCommand "jar" $arguments;
     
-    Remove-Item "com" -Recurse -Force -ea Stop
-    Remove-Item "META-INF" -Recurse -Force -ea Stop
-    Remove-Item "sos\spooler\jobs" -Recurse -Force -ea Stop
-    Remove-Item "sos\spooler\Spooler_program.class" -Force -ea Stop
+        if(Test-Path("com")){
+            Remove-Item "com" -Recurse -Force -ea Stop
+        }
+        if(Test-Path("META-INF")){
+            Remove-Item "META-INF" -Recurse -Force -ea Stop
+        }
+        if(Test-Path("sos\spooler\jobs")){
+            Remove-Item "sos\spooler\jobs" -Recurse -Force -ea Stop
+        }
+        if(Test-Path("sos\spooler\Spooler_program.class")){
+            Remove-Item "sos\spooler\Spooler_program.class" -Force -ea Stop
+        }
     
-    $name = $Script:ProxyAssemblyBasename+".jar"
-    [Array]$arguments = "cvf",$name,"sos\spooler"
-    ExecuteCommand "jar" $arguments;
+        $name = $Script:ProxyAssemblyBasename+".jar"
+        [Array]$arguments = "cvf",$name,"sos\spooler"
+        ExecuteCommand "jar" $arguments;
     
-    Remove-Item "sos" -Recurse -Force -ea Stop
+        Remove-Item "sos" -Recurse -Force -ea Stop
     
-    $Script:TempApiJar  = Get-Item $name
+        $Script:TempApiJar  = Get-Item $name
+    }
+    catch{
+        throw "[CreateNewApiJarForProxy] $($_.Exception.ToString())"
+    }
 }
 
 function GenerateProxy(){
-    Set-Location $env:APP_PATH
+    try
+    {
+        Set-Location $env:APP_PATH
     
-    $build              = Join-Path $Script:TempDirectory.Fullname "build"
-    $command            = Join-Path $Script:ProxyGenDirectoryName "proxygen.exe" 
-    [Array]$arguments   = """$Script:TempApiJar""","-wd","""$build"""
-    ExecuteCommand $command $arguments;
+        $build              = Join-Path $Script:TempDirectory.Fullname "build"
+        $command            = Join-Path $Script:ProxyGenDirectoryName "proxygen.exe" 
+        [Array]$arguments   = """$Script:TempApiJar""","-wd","""$build"""
+        ExecuteCommand $command $arguments;
     
-    Set-Location $build
-    $buildCommand       = Join-Path $build "build.cmd"
-    ExecuteCommand $buildCommand $null;
+        Set-Location $build
+        $buildCommand       = Join-Path $build "build.cmd"
+        ExecuteCommand $buildCommand $null;
         
-    CopyGeneratedProxyFiles $build
+        CopyGeneratedProxyFiles $build
+    }
+    catch{
+        throw "[GenerateProxy] $($_.Exception.ToString())"
+    }
 }
 
 function CopyGeneratedProxyFiles([string] $dir){
-    Set-Location $env:APP_PATH
+    try
+    {
+        Set-Location $env:APP_PATH
     
-    $generatedTempDllName   = $Script:TempApiJar.Basename+".j4n.dll"
-    $generatedTempJarName   = $Script:TempApiJar.Basename+".j4n.jar"
+        $generatedTempDllName   = $Script:TempApiJar.Basename+".j4n.dll"
+        $generatedTempJarName   = $Script:TempApiJar.Basename+".j4n.jar"
         
-    $Script:ProxyDll    = Join-Path $dir $generatedTempDllName
-    $Script:ProxyJar    = Join-Path $dir $generatedTempJarName
+        $Script:ProxyDll    = Join-Path $dir $generatedTempDllName
+        $Script:ProxyJar    = Join-Path $dir $generatedTempJarName
         
-    $targetDllName      = RemoveInputApiJarVersion $Script:InputApiJar.Basename
-    $targetDllName     += ".j4n"
-    $targetDllName      = AddInputApiJarVersion $targetDllName "dll"
+        $targetDllName      = RemoveInputApiJarVersion $Script:InputApiJar.Basename
+        $targetDllName     += ".j4n"
+        $targetDllName      = AddInputApiJarVersion $targetDllName "dll"
+        if(!($targetDllName.StartsWith($Script:ProxyOutputNamePrefix))){
+            $targetDllName = $Script:ProxyOutputNamePrefix+$targetDllName
+        }
     
-    $targetJarName      = RemoveInputApiJarVersion $Script:InputApiJar.Basename
-    $targetJarName     += ".j4n"
-    $targetJarName      = AddInputApiJarVersion $targetJarName "jar"
+        $targetJarName      = RemoveInputApiJarVersion $Script:InputApiJar.Basename
+        $targetJarName     += ".j4n"
+        $targetJarName      = AddInputApiJarVersion $targetJarName "jar"
+        if(!($targetJarName.StartsWith($Script:ProxyOutputNamePrefix))){
+            $targetJarName = $Script:ProxyOutputNamePrefix+$targetJarName
+        }
     
-    $targetDll          = Join-Path $Script:OutDirProxyDll $targetDllName
-    $targetJar          = Join-Path $Script:OutDirProxyJar $targetJarName
+        $targetDll          = Join-Path $Script:OutDirProxyDll $targetDllName
+        $targetJar          = Join-Path $Script:OutDirProxyJar $targetJarName
     
-    if(Test-Path($targetDll)){
-        Remove-Item $targetDll -ea Stop
+        if(Test-Path($targetDll)){
+            Remove-Item $targetDll -ea Stop
+        }
+        if(Test-Path($targetJar)){
+            Remove-Item $targetJar -ea Stop
+        }
+        
+        Copy-Item $Script:ProxyDll $targetDll -ea Stop
+        $Script:CopiedFiles += $targetDll
+        Copy-Item $Script:ProxyJar $targetJar -ea Stop
+        $Script:CopiedFiles += $targetJar
     }
-    if(Test-Path($targetJar)){
-        Remove-Item $targetJar -ea Stop
+    catch{
+        throw "[CopyGeneratedProxyFiles] $($_.Exception.ToString())"
     }
-        
-    Copy-Item $Script:ProxyDll $targetDll -ea Stop
-    Copy-Item $Script:ProxyJar $targetJar -ea Stop
 }
 
 function SetApiJarVersion(){
-    $arr = $Script:InputApiJar.Basename.Split("-")
-    $vers = $arr[$arr.length-1];
-    if ($vers.Replace(".","") -match "^[\d\.]+$"){
-       $Script:InputApiJarVersion = $vers
+    try
+    {
+        $arr  = $Script:InputApiJar.Basename.Split("-")
+        $last = $arr[$arr.length-1];
+        $add  = "";
+        if($last.ToLower().equals("snapshot")){
+            $vers   = $arr[$arr.length-2];
+            $add    = "-"+$last;
+        }
+        else{
+            $vers   = $arr[$arr.length-1];
+        }
+        if ($vers.Replace(".","") -match "^[\d\.]+$"){
+            $Script:ApiJarVersion = $vers+$add
+        }
+    }
+    catch{
+        throw "[SetApiJarVersion] $($_.Exception.ToString())"
     }
 }
 
 function GenerateJobSchedulerAdapterDll($proxyDll){
-    $adapterName    = $Script:AdapterAssemblyBasename+".dll"
-    $adapter        = Join-Path $Script:OutDirProxyDll $adapterName
-    $powershellRef  = [PsObject].Assembly.Location
+    try{
+        $adapterName    = $Script:AdapterAssemblyBasename+".dll"
+        $adapter        = Join-Path $Script:OutDirProxyDll $adapterName
+        $powershellRef  = [PsObject].Assembly.Location
     
-    if(Test-Path($adapter)){
-        Remove-Item $adapter -ea Stop
+        if(Test-Path($adapter)){
+            Remove-Item $adapter -ea Stop
+        }
+    
+        $command            = "csc" 
+        [Array]$arguments   = "/nologo","/warn:0","/t:library","/out:$adapter","/recurse:""$Script:DotnetAdapterSourceDir\*.cs""","/reference:$powershellRef;$Script:Jni4NetReferenceDll;""$Script:ProxyDll"""
+        ExecuteCommand $command $arguments
+        $Script:CopiedFiles += $adapter
+        
+        if(![string]::IsNullOrEmpty($Script:ApiJarVersion)) {   
+            $adapterName    = AddInputApiJarVersion $Script:AdapterAssemblyBasename "dll"
+            $adapterNewPath = Join-Path $Script:OutDirProxyDll $adapterName
+            if(Test-Path($adapterNewPath)){
+                Remove-Item $adapterNewPath -ea Stop
+            }
+            Move-Item $adapter $adapterNewPath -ea Stop
+            $Script:CopiedFiles += $adapterNewPath
+        }
     }
-    
-    $command            = "csc" 
-    [Array]$arguments   = "/nologo","/warn:0","/t:library","/out:$adapter","/recurse:""$Script:DotnetAdapterSourceDir\*.cs""","/reference:$powershellRef;$Script:Jni4NetReferenceDll;""$Script:ProxyDll"""
-    ExecuteCommand $command $arguments
-    
-    if(![string]::IsNullOrEmpty($Script:InputApiJarVersion)) {   
-        $adapterName    = AddInputApiJarVersion $Script:AdapterAssemblyBasename "dll"
-        $adapterNewPath = Join-Path $Script:OutDirProxyDll $adapterName
-        Move-Item $adapter $adapterNewPath -ea Stop
-    }    
+    catch{
+        throw "[GenerateJobSchedulerAdapterDll] $($_.Exception.ToString())"
+    }
 }
 
 function AddInputApiJarVersion([string] $basename,[string] $extension){
-    if([string]::IsNullOrEmpty($Script:InputApiJarVersion)) {
+    if([string]::IsNullOrEmpty($Script:ApiJarVersion)) {
         return $basename+"."+$extension
     }
     else{
-        return $basename+"-"+$Script:InputApiJarVersion+"."+$extension
+        return $basename+"-"+$Script:ApiJarVersion+"."+$extension
     }
 }
 
 function RemoveInputApiJarVersion([string] $basename){
-    if([string]::IsNullOrEmpty($Script:InputApiJarVersion)) {
+    if([string]::IsNullOrEmpty($Script:ApiJarVersion)) {
         return $basename
     }
     else{
-        return $basename.Replace("-"+$Script:InputApiJarVersion,"")
+        return $basename.Replace("-"+$Script:ApiJarVersion,"")
     }
 }
 
 function CopyJni4NetDlls(){
-    $Script:Jni4NetDlls |foreach {
-        $oldDll = Join-Path $Script:OutDirProxyDll $_
-        if(Test-Path($oldDll)){
-            Remove-Item $oldDll -ea Stop
+    try{
+        $Script:Jni4NetDlls |foreach {
+            $oldDll = Join-Path $Script:OutDirProxyDll $_
+            $dll        = Join-Path $Script:ProxyGenDirectoryName $_
+            $dllCopied  = Join-Path $Script:OutDirProxyDll $_
+            if(Test-Path($oldDll)){
+                Remove-Item $oldDll -ea Stop
+            }
+            if(Test-Path($dllCopied)){
+                Remove-Item $dllCopied -ea Stop
+            }
+            
+            Copy-Item $dll $dllCopied -ea Stop
+            $Script:CopiedFiles += $dllCopied
         }
-        $dll = Join-Path $Script:ProxyGenDirectoryName $_
-        Copy-Item $dll $Script:OutDirProxyDll -ea Stop
+    }
+    catch{
+        throw "[CopyJni4NetDlls] $($_.Exception.ToString())"
     }
 }
 
 function ExecuteCommand([string] $command, [Array]$arguments){
-    try{
+    try
+    {
         if($arguments -ne $null){
             $process = Start-Process $command -NoNewWindow -ArgumentList $arguments -Wait -PassThru 
         }
@@ -259,10 +343,22 @@ function ExecuteCommand([string] $command, [Array]$arguments){
         }
     }
     catch{
-        throw "Command $command ends with error $($_.Exception.ToString())"
+        throw "[ExecuteCommand] Command $command ends with error $($_.Exception.ToString())"
     }
 }
-
+function RemoveCopiedFiles(){
+    try
+    {
+        $Script:CopiedFiles |foreach {
+            if(Test-Path($_)){
+                Remove-Item $_ -ea Stop
+            }
+        }
+    }
+    catch{
+        throw "[RemoveCopiedFiles] $($_.Exception.ToString())"
+    }
+}
 # ----------------------------------------------------------------------
 # Main
 # ----------------------------------------------------------------------
@@ -276,6 +372,13 @@ try{
 }
 catch{
     Write-Error "Error occurred: $($_.Exception.ToString())"
+    try
+    {
+        RemoveCopiedFiles
+    }
+    catch{
+        Write-Error "Error occurred on remove copied files : $($_.Exception.ToString())"
+    }
     exit $Script:ErrorExitCode
 }
 finally{
