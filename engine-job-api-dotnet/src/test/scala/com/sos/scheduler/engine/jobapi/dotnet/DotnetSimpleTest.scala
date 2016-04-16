64fac2bdcfb48b9637fa2f5711fad1d96aa5d99c
Joacim Zschimmer <jogit@zschimmer.com>
2016-04-16 17:01:17 +0200

    JS-1595 DotnetSimpleTest: spooler_process reads always spooler_task.order

--- engine-job-api-dotnet/src/test/scala/com/sos/scheduler/engine/jobapi/dotnet/DotnetSimpleTest.scala
+++ engine-job-api-dotnet/src/test/scala/com/sos/scheduler/engine/jobapi/dotnet/DotnetSimpleTest.scala
@@ -1,7 +1,6 @@
 package com.sos.scheduler.engine.jobapi.dotnet
 
 import com.sos.scheduler.engine.common.system.FileUtils.temporaryDirectory
-import com.sos.scheduler.engine.jobapi.dotnet.DotnetSimpleTest._
 import com.sos.scheduler.engine.jobapi.dotnet.api.{DotnetModuleReference, TaskContext}
 import org.junit.runner.RunWith
 import org.mockito.Mockito._
@@ -24,15 +23,19 @@ final class DotnetSimpleTest extends FreeSpec with BeforeAndAfterAll {
   }
 
   "Error in PowerShell script is detected" in {
+    val spoolerTaskInvoker, orderInvoker = mock[sos.spooler.Invoker]
+    val order = new sos.spooler.Order(orderInvoker)
+    when(spoolerTaskInvoker.call("<order", Array())).thenReturn(order, null)
     val error = "POWERSHELL-ERROR"
-    val job = instanceFactory.newInstance(
-      classOf[sos.spooler.Job_impl],
-      TaskContext(
-        new sos.spooler.Log(null/*not used*/),
-        new sos.spooler.Task(null/*not used*/),
-        new sos.spooler.Job(null/*not used*/),
-        new sos.spooler.Spooler(null/*not used*/)),
-      DotnetModuleReference.Powershell(s"""function spooler_process() { throw "$error" }"""))
+    val taskContext = TaskContext(
+      new sos.spooler.Log(null/*not used*/),
+      new sos.spooler.Task(spoolerTaskInvoker),
+      new sos.spooler.Job(null/*not used*/),
+      new sos.spooler.Spooler(null/*not used*/))
+    val job = instanceFactory.newInstance(classOf[sos.spooler.Job_impl], taskContext, DotnetModuleReference.Powershell(s"""
+      function spooler_process() {
+      throw "$error"
+    }"""))
     val e = intercept[Exception] {
       job.spooler_process()
     }
@@ -46,24 +49,24 @@ final class DotnetSimpleTest extends FreeSpec with BeforeAndAfterAll {
     when(spoolerTaskInvoker.call("<order", Array())).thenReturn(order, null)
     when(orderInvoker.call("<params", Array())).thenReturn(variableSet, null)
     when(paramsInvoker.call("<value", Array("TEST"))).thenReturn("HELLO", null)
-    val job = instanceFactory.newInstance(
-      classOf[sos.spooler.Job_impl],
-      TaskContext(
-        new sos.spooler.Log(spoolerLogInvoker),
-        new sos.spooler.Task(spoolerTaskInvoker),
-        new sos.spooler.Job(null/*not used*/),
-        new sos.spooler.Spooler(null/*not used*/)),
-      DotnetModuleReference.Powershell(PowershellScript))
+    val taskContext = TaskContext(
+      new sos.spooler.Log(spoolerLogInvoker),
+      new sos.spooler.Task(spoolerTaskInvoker),
+      new sos.spooler.Job(null/*not used*/),
+      new sos.spooler.Spooler(null/*not used*/))
+    val job = instanceFactory.newInstance(classOf[sos.spooler.Job_impl], taskContext, DotnetModuleReference.Powershell("""
+      function spooler_process() {
+        $value = $spooler_task.order.params.value("TEST")
+        $spooler_log.log(0, $value)
+        return true
+      }"""))
     job.spooler_process()
+    verify(spoolerTaskInvoker).call("<order", Array())
+    verify(orderInvoker).call("<params", Array())
+    verify(paramsInvoker).call("<value", Array("TEST"))
     verify(spoolerLogInvoker).call("log", Array(0: Integer, "HELLO"))
   }
 }
 
 private object DotnetSimpleTest {
-  private val PowershellScript = """
-    function spooler_process() {
-      $value = $spooler_task.order.params.value("TEST")
-      $spooler_log.log(0, $value)
-      return true
-    }"""
 }
