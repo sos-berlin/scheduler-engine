6a2a5c03e1b77db2f826daad2d130d7f0ed24911
Joacim Zschimmer <jogit@zschimmer.com>
2016-05-03 19:20:51 +0200

    JS-1595 .Net assembly description

--- engine-taskserver-dotnet/src/main/dotnet/adapter/AssemblyInfo.cs
+++ engine-taskserver-dotnet/src/main/dotnet/adapter/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("com.sos-berlin.engine.engine-job-api-dotnet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JobScheduler API for .Net jobs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SOS Berlin")]
 [assembly: AssemblyProduct("com.sos-berlin.engine.engine-job-api-dotnet")]
 [assembly: AssemblyCopyright("Copyright ©2016")]
 [assembly: AssemblyTrademark("")]
