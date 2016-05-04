01d25649366be1cd073e8df1a34b64e1625137c4
Joacim Zschimmer <jogit@zschimmer.com>
2016-05-04 18:36:23 +0200

    JS-1623 NamedInvocables renamed as NamedIDispatches

--- engine-taskserver-moduleapi/src/main/scala/com/sos/scheduler/engine/taskserver/moduleapi/NamedIDispatches.scala
+++ engine-taskserver-moduleapi/src/main/scala/com/sos/scheduler/engine/taskserver/moduleapi/NamedIDispatches.scala
@@ -1,26 +1,26 @@
 package com.sos.scheduler.engine.taskserver.moduleapi
 
-import com.sos.scheduler.engine.minicom.idispatch.Invocable
+import com.sos.scheduler.engine.minicom.idispatch.IDispatch
 
 /**
   * @author Joacim Zschimmer
   */
-trait NamedInvocables {
-  def spoolerLog: Invocable
-  def spoolerTask: Invocable
-  def spoolerJob: Invocable
-  def spooler: Invocable
+trait NamedIDispatches {
+  def spoolerLog: IDispatch
+  def spoolerTask: IDispatch
+  def spoolerJob: IDispatch
+  def spooler: IDispatch
 
   /**
-    * Invocable by name.
+    * IDispatch by name.
     *
     * @param name "spooler_log", "spooler_task", "spooler_job" or "spooler"
     * @return `spoolerLog`, `spoolerTask`, `spoolerJob` or `spooler`
     */
-  def apply(name: String): Invocable
+  def apply(name: String): IDispatch
 }
 
-object NamedInvocables {
+object NamedIDispatches {
   val SpoolerLogName = "spooler_log"
   val SpoolerTaskName = "spooler_task"
   val SpoolerJobName = "spooler_job"
