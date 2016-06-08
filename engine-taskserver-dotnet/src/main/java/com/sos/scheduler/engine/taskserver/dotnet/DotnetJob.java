16a604200fe6884770cdeaa9dd73f6414dd121ec
SOS-RE <robert.ehrlich@sos-berlin.com>
2016-06-08 16:03:25 +0200

    JS-1623 - Refactoring. DonetJob and DotnetMonitor provide the original exception

--- engine-taskserver-dotnet/src/main/java/com/sos/scheduler/engine/taskserver/dotnet/DotnetJob.java
+++ engine-taskserver-dotnet/src/main/java/com/sos/scheduler/engine/taskserver/dotnet/DotnetJob.java
@@ -3,83 +3,49 @@ package com.sos.scheduler.engine.taskserver.dotnet;
 import com.sos.scheduler.engine.taskserver.dotnet.api.TaskContext;
 
 public final class DotnetJob extends sos.spooler.Job_impl {
-	private final static String LOG_PREFIX = "[.NET module]";
-	private final DotnetApiImpl apiImpl;
+    private final DotnetApiImpl apiImpl;
 
-	DotnetJob(TaskContext taskContext, DotnetApiImpl dotnetApiImpl)
-			throws Exception {
-		spooler_log = taskContext.spoolerLog();
-		spooler_task = taskContext.spoolerTask();
-		spooler_job = taskContext.spoolerJob();
-		spooler = taskContext.spooler();
-		this.apiImpl = dotnetApiImpl;
-	}
+    DotnetJob(TaskContext taskContext, DotnetApiImpl dotnetApiImpl)
+            throws Exception {
+        spooler_log = taskContext.spoolerLog();
+        spooler_task = taskContext.spoolerTask();
+        spooler_job = taskContext.spoolerJob();
+        spooler = taskContext.spooler();
+        this.apiImpl = dotnetApiImpl;
+    }
 
-	@Override
-	public boolean spooler_init() throws Exception {
-		try {
-			return apiImpl.spooler_init();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public boolean spooler_init() throws Exception {
+        return apiImpl.spooler_init();
+    }
 
-	@Override
-	public void spooler_exit() throws Exception {
-		try {
-			apiImpl.spooler_exit();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public void spooler_exit() throws Exception {
+        apiImpl.spooler_exit();
+    }
 
-	@Override
-	public boolean spooler_open() throws Exception {
-		try {
-			return apiImpl.spooler_open();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public boolean spooler_open() throws Exception {
+        return apiImpl.spooler_open();
+    }
 
-	@Override
-	public void spooler_close() throws Exception {
-		try {
-			apiImpl.spooler_close();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public void spooler_close() throws Exception {
+        apiImpl.spooler_close();
+    }
 
-	@Override
-	public boolean spooler_process() throws Exception {
-		try {
-			return apiImpl.spooler_process();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public boolean spooler_process() throws Exception {
+        return apiImpl.spooler_process();
+    }
 
-	@Override
-	public void spooler_on_error() throws Exception {
-		try {
-			apiImpl.spooler_on_error();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public void spooler_on_error() throws Exception {
+        apiImpl.spooler_on_error();
+    }
 
-	@Override
-	public void spooler_on_success() throws Exception {
-		try {
-			apiImpl.spooler_on_success();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
-
-	private String getLogMessage(String msg) {
-		return String.format("%s %s", LOG_PREFIX, msg);
-	}
-
+    @Override
+    public void spooler_on_success() throws Exception {
+        apiImpl.spooler_on_success();
+    }
 }
--- engine-taskserver-dotnet/src/main/java/com/sos/scheduler/engine/taskserver/dotnet/DotnetMonitor.java
+++ engine-taskserver-dotnet/src/main/java/com/sos/scheduler/engine/taskserver/dotnet/DotnetMonitor.java
@@ -1,54 +1,31 @@
 package com.sos.scheduler.engine.taskserver.dotnet;
 
-import com.sos.scheduler.engine.taskserver.dotnet.api.TaskContext;
-
 public final class DotnetMonitor extends sos.spooler.Monitor_impl {
-	private final static String LOG_PREFIX = "[.NET module]";
-	private final DotnetApiImpl apiImpl;
+    private final DotnetApiImpl apiImpl;
 
-	DotnetMonitor(DotnetApiImpl dotnetApiImpl)
-			throws Exception {
-		this.apiImpl = dotnetApiImpl;
-	}
+    DotnetMonitor(DotnetApiImpl dotnetApiImpl)
+            throws Exception {
+        this.apiImpl = dotnetApiImpl;
+    }
 
-	@Override
-	public boolean spooler_task_before() throws Exception {
-		try {
-			return apiImpl.spooler_task_before();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public boolean spooler_task_before() throws Exception {
+        return apiImpl.spooler_task_before();
+    }
 
-	@Override
-	public void spooler_task_after() throws Exception {
-		try {
-			apiImpl.spooler_task_after();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public void spooler_task_after() throws Exception {
+        apiImpl.spooler_task_after();
+    }
 
-	@Override
-	public boolean spooler_process_before() throws Exception {
-		try {
-			return apiImpl.spooler_process_before();
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
+    @Override
+    public boolean spooler_process_before() throws Exception {
+        return apiImpl.spooler_process_before();
+    }
 
-	@Override
-	public boolean spooler_process_after(boolean spooler_process_result)
-			throws Exception {
-		try {
-			return apiImpl.spooler_process_after(spooler_process_result);
-		} catch (Exception ex) {
-			throw new Exception(getLogMessage(ex.toString()));
-		}
-	}
-
-	private String getLogMessage(String msg) {
-		return String.format("%s %s", LOG_PREFIX, msg);
-	}
+    @Override
+    public boolean spooler_process_after(boolean spooler_process_result)
+            throws Exception {
+        return apiImpl.spooler_process_after(spooler_process_result);
+    }
 }
