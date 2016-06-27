61ae0d752c52facf12f51358ab5c5fb88bc5e894
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-27 13:09:39 +0200

    JS-1635 FIX: warn_if_longer_than issues SCHEUDLER-712 immediately for shell jobs, too.

---
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1635/JS1635IT.scala
@@ -0,0 +1,37 @@
+package com.sos.scheduler.engine.tests.jira.js1635
+
+import com.sos.scheduler.engine.common.scalautil.Futures.implicits._
+import com.sos.scheduler.engine.common.time.ScalaTime._
+import com.sos.scheduler.engine.data.job.JobPath
+import com.sos.scheduler.engine.data.log.WarningLogEvent
+import com.sos.scheduler.engine.data.message.MessageCode
+import com.sos.scheduler.engine.test.EventBusTestFutures.implicits.RichEventBus
+import com.sos.scheduler.engine.test.SchedulerTestUtils._
+import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
+import java.time.Instant.now
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * JS-1635 warn_if_longer_than should work for shell jobs.
+  *
+  * @author Joacim Zschimmer
+  */
+@RunWith(classOf[JUnitRunner])
+final class JS1635IT extends FreeSpec with ScalaSchedulerTest {
+
+  "warn_if_longer_than" in {
+    val jobPaths = List(JobPath("/shell"), JobPath("/shell-with-monitor"), JobPath("/api"))
+    var runs: List[TaskRun] = null
+    val t = now
+    eventBus.awaitingEvent[WarningLogEvent](_.codeOption contains MessageCode("SCHEDULER-712")) {
+      runs = for (jobPath ← jobPaths) yield startJob(jobPath)
+    }
+    val duration = now - t
+    assert(duration >= 5.s && duration <= 9.s)
+    assert(runs forall { o ⇒ !o.ended.isCompleted })
+    val results = runs map { _.result } await TestTimeout
+    assert(results forall { o ⇒ o.duration >= 10.s })
+  }
+}
