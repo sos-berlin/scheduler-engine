b361d1c326b132a139296e13f2c9866d4673553a
Joacim Zschimmer <jogit@zschimmer.com>
2016-06-30 12:49:50 +0200

    Removed StringValue

---
+++ engine-kernel/src/main/scala/com/sos/scheduler/engine/kernel/scheduler/SchedulerInstanceId.scala
@@ -0,0 +1,8 @@
+package com.sos.scheduler.engine.kernel.scheduler
+
+import com.sos.scheduler.engine.base.generic.IsString
+
+/**
+  * @author Joacim Zschimmer
+  */
+final case class SchedulerInstanceId(string: String) extends IsString
