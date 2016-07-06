15d634cb3cfedea1227fcd1ca77d315870e64157
Joacim Zschimmer <jogit@zschimmer.com>
2016-07-06 13:06:59 +0200

    JS-1644 Test, not confirming: Call of spooler_log() api after spooler.log().start_new_file() triggers scheduler.log rotate

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1455/JS1455IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1455/JS1455IT.scala
@@ -5,35 +5,52 @@ import com.sos.scheduler.engine.data.job.JobPath
 import com.sos.scheduler.engine.test.SchedulerTestUtils._
 import com.sos.scheduler.engine.test.configuration.TestConfiguration
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
+import java.nio.file.Files.exists
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
 
 /**
- * JS-1455 spooler.log.start_new_file should keep log categories, at least "scheduler".
- *
- * @author Joacim Zschimmer
- */
+  * JS-1455 spooler.log.start_new_file should keep log categories, at least "scheduler".
+  * <br>
+  * JS-1644 Call of spooler_log() api after spooler.log().start_new_file() triggers scheduler.log rotate.
+  *
+  * @author Joacim Zschimmer
+  */
 @RunWith(classOf[JUnitRunner])
 final class JS1455IT extends FreeSpec with ScalaSchedulerTest {
 
   override protected lazy val testConfiguration = TestConfiguration(getClass, logCategories = "scheduler.wait")
 
-  private val testCommand = "<params.get/>"
+  private val firstCommand = "<params.get/>"
+  private val secondCommand = "<params/>"
+  private lazy val schedulerOldLogFile = testEnvironment.logDirectory / "scheduler-old.log"
 
   "spooler.log contains log category {scheduler}" in {
-    scheduler executeXml testCommand
-    assert(schedulerLogAsString contains testCommand)
+    scheduler executeXml firstCommand
+    assert(schedulerLogAsString contains firstCommand)
     checkSchedulerLog()
   }
 
   "spooler.log.start_new_file should keep log category {scheduler}" in {
     runJobAndWaitForEnd(JobPath("/start_new_file"))
-    scheduler executeXml <params/>  // Logs the command
+    scheduler executeXml secondCommand // Logs the command
     withClue("New file should not contain previous lines:") {
-      assert(!(schedulerLogAsString contains testCommand))
+      assert(!(schedulerLogAsString contains firstCommand))
     }
+    assert(schedulerLogAsString contains secondCommand)
     checkSchedulerLog()
+  }
+
+  "JS-1644 spooler_log.info() in next job does not start new scheduler.log" in {
+    runJobAndWaitForEnd(JobPath("/log"))
+    withClue("After next job with spooler_log.info(), scheduler.log should keep its content: ") {
+      assert(schedulerLogAsString contains secondCommand)
+    }
+  }
+
+  "No scheduler-old.log exists" in {
+    assert(!exists(schedulerOldLogFile))
   }
 
   // For manual testing. To be fast, remove sleep(1) from log.cxx.
