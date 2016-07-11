7ef10379e9dd34ebf891907193868c5cd0adbce0
Joacim Zschimmer <jogit@zschimmer.com>
2016-07-11 13:15:45 +0200

    ExtraRoute

--- engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/WebServiceActor.scala
+++ engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/WebServiceActor.scala
@@ -2,16 +2,20 @@ package com.sos.scheduler.engine.plugins.newwebservice
 
 import com.google.inject.Injector
 import com.sos.scheduler.engine.common.guice.GuiceImplicits._
+import com.sos.scheduler.engine.common.scalautil.Logger
 import com.sos.scheduler.engine.kernel.DirectSchedulerClient
 import com.sos.scheduler.engine.kernel.filebased.FileBasedSubsystem
 import com.sos.scheduler.engine.kernel.scheduler.SchedulerConfiguration
+import com.sos.scheduler.engine.plugins.newwebservice.WebServiceActor._
 import com.sos.scheduler.engine.plugins.newwebservice.configuration.NewWebServicePluginConfiguration
 import com.sos.scheduler.engine.plugins.newwebservice.routes.WebServices
 import javax.inject.Inject
+import scala.collection.JavaConversions._
 import spray.routing._
 
 final class WebServiceActor @Inject private(
   protected val configuration: NewWebServicePluginConfiguration,
+  extraRoutes: java.util.Set[ExtraRoute],
   injector: Injector)
 extends HttpServiceActor with WebServices {
 
@@ -20,5 +24,14 @@ extends HttpServiceActor with WebServices {
   protected lazy val client = injector.instance[DirectSchedulerClient]
   protected lazy val fileBasedSubsystemRegister = injector.instance[FileBasedSubsystem.Register]
 
-  override def receive = runRoute(route)
+  override def receive = {
+    for (o ← extraRoutes) logger.info(s"Using ${o.getClass.getName}")
+    var extras: Vector[Route] = extraRoutes.toVector map { _.route }
+    val routes = route +: extras
+    runRoute(routes reduce { _ ~ _ })
+  }
+}
+
+object WebServiceActor {
+  private val logger = Logger(getClass)
 }
