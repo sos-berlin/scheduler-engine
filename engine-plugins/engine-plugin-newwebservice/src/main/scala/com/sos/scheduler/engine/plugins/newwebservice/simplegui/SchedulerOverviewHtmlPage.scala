12bb7a3e3771f211e21098673ae392c9c3fd2377
Joacim Zschimmer <jogit@zschimmer.com>
2016-08-11 13:22:29 +0200

    SchedulerOverview with SystemInformation

--- engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/simplegui/SchedulerOverviewHtmlPage.scala
+++ engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/simplegui/SchedulerOverviewHtmlPage.scala
@@ -19,10 +19,14 @@ final class SchedulerOverviewHtmlPage private(
 extends SchedulerHtmlPage {
 
   protected val schedulerOverview = response.content
+  import webServiceContext.uris
   import schedulerOverview._
+
+  override protected def cssLinks = super.cssLinks :+ uris / "api/frontend/schedulerOverview/overview.css"
 
   def wholePage =
     htmlPage(
+      systemInformationHtml,
       p(marginBottom := "30px")(
         s"Started at ",
         instantWithDurationToHtml(startedAt),
@@ -41,6 +45,38 @@ extends SchedulerHtmlPage {
           span(cls := "input-group-btn")
             (button(`type` := "submit", cls := "btn btn-primary")(
               "Execute")))))
+
+//  private def cpuAndRamGraphics =
+//    div(width := 170.px)(
+//      "progressbar".tag[String](cls := "progress-bar-info", "max".attr := 1, "animate".attr := false, margin := 0, fontSize := 11.px)(
+//        system.mxBeans.get("operatingSystem") flatMap { _.asInstanceOf[Map[String, Any]].get("systemCpuLoad") } getOrElse "")
+//        //span(color.white, whiteSpace.nowrap)(system.mxBeans.operatingSystem.systemCpuLoad | percentage:0 "CPU"),
+//      "progress".tag[String]("max".attr := 1, margin := "5px 0 0 0", fontSize := 11.px, "animate".attr := false)(
+//        "bar".tag[String](1 - freePhysicalMemoryRatio)(
+//          span(color.white)((totalPhysicalMemorySize - freePhysicalMemorySize) / (1024*1024*1024) | number:1 "GiB")()
+//        bar(`type` := 'info')(freePhysicalMemoryRatio)(
+//          span(color.black)((freePhysicalMemorySize / (1024*1024*1024) | number:1) "GiB"))),
+//      div(cls := "smallFont", margin := 0, color := 0xa0a0a0)(
+//        (totalPhysicalMemorySize / (1024*1024*1024) | number:1 " GiB RAM ∙ "),
+//        span(color := 0x08000)(freePhysicalMemorySize / (1024*1024*1024) | number:1 "GiB free")
+
+  private def systemInformationHtml =
+    div(cls := "ContentBox SystemInformation")(
+//      div(float.right, marginTop := 1.px, marginLeft := 10.px)(
+//        cpuAndRamGraphics),
+      div(/*float.right*/)(
+        div(cls := "Hostname")(
+          system.hostname),
+        for (o ← system.distribution) yield
+          div(o),
+        div(
+          java.systemProperties.getOrElse("os.name", "") + " " +
+            java.systemProperties.getOrElse("os.version", "") + " ∙ " +
+            java.systemProperties.getOrElse("os.arch", "")),
+        div(
+          "Java " + java.systemProperties.getOrElse("java.version", "") + " " +
+            java.systemProperties.getOrElse("java.vendor", ""))),
+      div(clear.both))
 }
 
 object SchedulerOverviewHtmlPage {
