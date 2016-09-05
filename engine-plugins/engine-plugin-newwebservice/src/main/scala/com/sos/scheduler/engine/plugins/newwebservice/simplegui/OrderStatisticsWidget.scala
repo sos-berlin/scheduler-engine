ed7f605dcab42dbbb03db889f6562889d71c0f69
Joacim Zschimmer <jogit@zschimmer.com>
2016-09-05 21:00:39 +0200

    OrderStatistics html

--- engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/simplegui/OrderStatisticsWidget.scala
+++ engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/simplegui/OrderStatisticsWidget.scala
@@ -1,6 +1,5 @@
 package com.sos.scheduler.engine.plugins.newwebservice.simplegui
 
-import com.sos.scheduler.engine.plugins.newwebservice.html.HtmlPage.joinHtml
 import scalatags.Text.all._
 import scalatags.text.Frag
 
@@ -9,26 +8,32 @@ import scalatags.text.Frag
   */
 object OrderStatisticsWidget {
 
-  private val fieldNames = List(
-    "total",
-    "notPlanned",
-    "planned",
-    "pending",
-    "running",
-    "inTask",
-    "inProcess",
-    "setback",
-    "suspended",
-    "blacklisted",
-    "permanent",
-    "fileOrder")
+  private val fieldGroups = List(
+    List(
+      "total",
+      "notPlanned",
+      "planned",
+      "pending"),
+    List(
+      "running",
+      "inTask",
+      "inProcess"),
+    List(
+      "setback",
+      "suspended",
+      "blacklisted"),
+    List(
+      "permanent",
+      "fileOrder"))
 
   val html: Frag =
     div(id := "OrderStatistics")(marginBottom := 1.em)(
-      for (name ← fieldNames) yield
-        div(id := s"order-$name-field", cls := "OrderStatistics-field", float.left, width := 15.ex, marginTop := 2.px, marginRight := 1.ex, whiteSpace.nowrap)(
-          s"$name:\u2009",
-          span(id := s"order-$name-value")("\u2007\u2007\u2007\u2007")),
+      for (nameGroup ← fieldGroups) yield
+        div(cls := "OrderStatistics-fieldGroup")(
+          for (name ← nameGroup) yield
+            div(id := s"order-$name-field", cls := "OrderStatistics-field")(
+              s"$name:\u2009",
+              span(id := s"order-$name-value")("\u2007\u2007\u2007\u2007"))),
       " ",
       span(id := "OrderStatistics-refresh", cls := "glyphicon glyphicon-refresh", float.left, position.relative, top := 2.px, marginRight := 8.px),
       div(clear.left))
