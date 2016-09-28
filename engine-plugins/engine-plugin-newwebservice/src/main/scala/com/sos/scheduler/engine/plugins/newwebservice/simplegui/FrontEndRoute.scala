76b072d497df5ff66031c5c257d4adb5aef95558
Joacim Zschimmer <jogit@zschimmer.com>
2016-09-28 11:11:04 +0200

    typo

--- engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/simplegui/FrontEndRoute.scala
+++ engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/simplegui/FrontEndRoute.scala
@@ -26,7 +26,7 @@ trait FrontEndRoute extends WebjarsRoute {
 }
 
 object FrontEndRoute {
-  /** For quicker response, we asume an installation of a changed JobScheduler version takes more than a minute. */
+  /** For quicker response, we assume an installation of a changed JobScheduler version takes more than a minute. */
   private val ShortTimeCaching = `Cache-Control`(`max-age`(60))
   private val FrontendResourceDirectory = JavaResource("com/sos/scheduler/engine/plugins/newwebservice/simplegui/frontend")
 }
