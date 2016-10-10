ba4b07f62e5697033b54c7ef66b3cec9e17943a3
Joacim Zschimmer <jogit@zschimmer.com>
2016-10-10 10:08:11 +0200

    JS-1642 NewWebServicePlugin no long works with JettyPlugin

--- engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/NewWebServicePlugin.scala
+++ engine-plugins/engine-plugin-newwebservice/src/main/scala/com/sos/scheduler/engine/plugins/newwebservice/NewWebServicePlugin.scala
@@ -4,12 +4,10 @@ import akka.actor.ActorSystem
 import com.google.inject.{AbstractModule, Injector, Provides}
 import com.sos.scheduler.engine.common.scalautil.Closers.implicits.RichClosersCloser
 import com.sos.scheduler.engine.common.scalautil.Futures.implicits.SuccessFuture
-import com.sos.scheduler.engine.common.scalautil.SideEffect._
 import com.sos.scheduler.engine.common.time.ScalaTime._
 import com.sos.scheduler.engine.common.xml.XmlUtils.toXml
 import com.sos.scheduler.engine.kernel.plugin.{Plugin, PluginSubsystem, Plugins, UseGuiceModule}
 import com.sos.scheduler.engine.kernel.scheduler.SchedulerConfiguration
-import com.sos.scheduler.engine.plugins.jetty.{JettyPlugin, JettyPluginExtension}
 import com.sos.scheduler.engine.plugins.newwebservice.configuration.NewWebServicePluginConfiguration
 import java.net.InetSocketAddress
 import javax.inject.{Inject, Named, Singleton}
@@ -38,19 +36,7 @@ extends Plugin {
     def newWebServicePluginConfiguration: NewWebServicePluginConfiguration = conf
   })
 
-  override def onPrepare() =
-    pluginSubsystem.pluginOptionByClass(classOf[JettyPlugin]) match {
-      case Some(jettyPlugin) ⇒ runWithJetty(jettyPlugin)
-      case None ⇒ runWithSprayWebServer()
-    }
-
-  private def runWithJetty(jettyPlugin: JettyPlugin): Unit =
-    jettyPlugin.addExtension(
-      JettyPluginExtension(
-        modifyServletContextHandler = servletContextHandler ⇒ {
-          servletContextHandler.addEventListener(new SprayServletContextInitializer(myInjector))
-          servletContextHandler.addServlet(classOf[spray.servlet.Servlet30ConnectorServlet], "/*") sideEffect { _.setAsyncSupported(true) }
-        }))
+  override def onPrepare() = runWithSprayWebServer()
 
   private def runWithSprayWebServer(): Unit = {
     val httpPort = schedulerConfiguration.httpPortOption getOrElse { throw new IllegalArgumentException("Missing -http-port=") }
