87aeb26a84690349351759d7570e5982c31ab157
Joacim Zschimmer <jogit@zschimmer.com>
2016-10-31 09:48:01 +0100

    JS-1671 Refactoring: TunnelSecret becomes SecretString

--- engine-common/src/test/scala/com/sos/scheduler/engine/common/auth/SecretStringGeneratorTest.scala
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/auth/SecretStringGeneratorTest.scala
@@ -1,10 +1,11 @@
-package com.sos.scheduler.engine.tunnel.data
+package com.sos.scheduler.engine.common.auth
 
+import com.sos.scheduler.engine.base.generic.SecretString
+import com.sos.scheduler.engine.common.auth.SecretStringGenerator._
+import com.sos.scheduler.engine.common.auth.SecretStringGeneratorTest._
 import com.sos.scheduler.engine.common.scalautil.Logger
 import com.sos.scheduler.engine.common.time.ScalaTime._
 import com.sos.scheduler.engine.common.time.Stopwatch.measureTime
-import com.sos.scheduler.engine.tunnel.data.TunnelToken._
-import com.sos.scheduler.engine.tunnel.data.TunnelTokenTest._
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
@@ -13,36 +14,36 @@ import org.scalatest.junit.JUnitRunner
  * @author Joacim Zschimmer
  */
 @RunWith(classOf[JUnitRunner])
-final class TunnelTokenTest extends FreeSpec {
+final class SecretStringGeneratorTest extends FreeSpec {
 
-  "newSecret returns different secrets" in {
-    logger.debug(newSecret().string)
+  "newSecretString returns different secrets" in {
+    logger.debug(newSecretString().string)
     val n = 100000
-    val secrets = for (_ ← 1 to n) yield newSecret()
+    val secrets = for (_ ← 1 to n) yield newSecretString()
     assert(secrets.distinct.size == n)
     assert(secrets.map(_.string).distinct.size == n)
     assert(secrets.distinct == secrets)
   }
 
-  "newSecret returns restricted character set" in {
+  "newSecretString returns restricted character set" in {
     for (_ ← 1 to 1000) {
-      val secretString = newSecret().string
+      val secretString = newSecretString().string
       assert(secretString forall ExpectedCharacters)
       assert(secretString.size == SecretSize)
     }
   }
 
-  "newSecret is fast" in {
-    val result = measureTime(10000, "newSecret") { newSecret() }
+  "newSecretString is fast" in {
+    val result = measureTime(10000, "newSecretString") { newSecretString() }
     assert(result.singleDuration < 1.ms)
   }
 
-  "Secret.toString does not show secret" in {
-    assert(TunnelToken.Secret("secret").toString == "Secret(...)")
+  "SecretString.toString does not show secret" in {
+    assert(SecretString("secret").toString == "SecretString")
   }
 }
 
-private object TunnelTokenTest {
+private object SecretStringGeneratorTest {
   private val logger = Logger(getClass)
   private val ExpectedCharacters = Set('-', '_') ++ ('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9')
   private val SecretSize = 24   // = 28/3*4
--- engine-tunnel-data/src/main/scala/com/sos/scheduler/engine/tunnel/data/TunnelToken.scala
+++ engine-tunnel-data/src/main/scala/com/sos/scheduler/engine/tunnel/data/TunnelToken.scala
@@ -1,29 +1,24 @@
 package com.sos.scheduler.engine.tunnel.data
 
-import com.sos.scheduler.engine.base.generic.IsString
-import com.sos.scheduler.engine.tunnel.data.TunnelToken._
-import java.security.SecureRandom
-import java.util.Base64
+import com.sos.scheduler.engine.base.generic.{IsString, SecretString}
+import com.sos.scheduler.engine.common.auth.SecretStringGenerator
 import spray.json.DefaultJsonProtocol._
 
 /**
  * @author Joacim Zschimmer
  */
-final case class TunnelToken(id: TunnelId, secret: Secret) {
+final case class TunnelToken(id: TunnelId, secret: SecretString) {
   override def toString = id.toString
 }
 
 object TunnelToken {
-  implicit val MyJsonFormat = jsonFormat2(apply)
-  private val ByteCount = 18  // 144 bits (a UUID has 128 bits). For base64, a multiple of 3 bytes is good.
-  private val toUrlBase64 = Base64.getUrlEncoder.encodeToString _
-  private val random = new SecureRandom
 
-  private[tunnel] def newSecret() = Secret({
-    val bytes = new Array[Byte](ByteCount)
-    random.nextBytes(bytes)
-    toUrlBase64(bytes) stripSuffix "="
-  })
+  implicit val jsonFormat = {
+    implicit val x = SecretString.implicits.jsonFormat
+    jsonFormat2(apply)
+  }
+
+  private[tunnel] def newSecret() = SecretStringGenerator.newSecretString()
 
   final case class Secret(string: String) extends IsString {
     override def toString = "Secret(...)"
