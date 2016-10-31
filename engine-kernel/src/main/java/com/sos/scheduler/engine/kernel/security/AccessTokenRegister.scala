3495f3815859653b0eb315ca48ca96bee9d6b1e1
Joacim Zschimmer <jogit@zschimmer.com>
2016-10-31 22:00:45 +0100

    JS-1671 AccessTokenRegister

---
+++ engine-kernel/src/main/java/com/sos/scheduler/engine/kernel/security/AccessTokenRegister.scala
@@ -0,0 +1,34 @@
+package com.sos.scheduler.engine.kernel.security
+
+import com.sos.scheduler.engine.base.generic.SecretString
+import com.sos.scheduler.engine.common.auth.SecretStringGenerator.newSecretString
+import com.sos.scheduler.engine.common.auth.UserId
+import javax.inject.{Inject, Singleton}
+import scala.collection.JavaConversions._
+
+/**
+  * @author Joacim Zschimmer
+  */
+@Singleton
+final class AccessTokenRegister @Inject()() {
+
+  private val register = new java.util.concurrent.ConcurrentHashMap[SecretString, UserId] withDefault {
+    _ ⇒ throw new NoSuchElementException("Invalid access token")
+  }
+
+  private[security] def size = register.size
+
+  def newAccessTokenForUser(userId: UserId): SecretString = {
+    val accessToken = newSecretString()
+    register += accessToken → userId
+    accessToken
+  }
+
+  def remove(accessToken: SecretString): Unit = {
+    register -= accessToken
+  }
+
+  val validate: PartialFunction[SecretString, UserId] = register
+
+  override def toString = s"AccessTokenRegister(${register.size} access tokens)"
+}
---
+++ engine-kernel/src/test/scala/com/sos/scheduler/engine/kernel/security/AccessTokenRegisterTest.scala
@@ -0,0 +1,34 @@
+package com.sos.scheduler.engine.kernel.security
+
+import com.sos.scheduler.engine.base.generic.SecretString
+import com.sos.scheduler.engine.common.auth.UserId
+import org.junit.runner.RunWith
+import org.scalatest.FreeSpec
+import org.scalatest.junit.JUnitRunner
+
+/**
+  * @author Joacim Zschimmer
+  */
+@RunWith(classOf[JUnitRunner])
+final class AccessTokenRegisterTest extends FreeSpec {
+
+  private val register = new AccessTokenRegister
+
+  "AccessTokeRegister" in {
+    assert(register.size == 0)
+    val a = register.newAccessTokenForUser(UserId("A"))
+    val b = register.newAccessTokenForUser(UserId("B"))
+    assert(a != b)
+    assert(register.size == 2)
+    assert(register.validate.lift(a) == Some(UserId("A")))
+    assert(register.validate.lift(b) == Some(UserId("B")))
+    assert(register.validate.lift(SecretString("UNKNOWN")) == None)
+
+    register.remove(a)
+    assert(register.validate.lift(a) == None)
+
+    register.remove(b)
+    assert(register.validate.lift(b) == None)
+    assert(register.size == 0)
+  }
+}
