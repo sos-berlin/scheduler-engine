f861507d14294f1dbf1080c4e4007b94e4867fa5
Joacim Zschimmer <jogit@zschimmer.com>
2017-01-10 12:10:28 +0100

    CustomEvent

---
+++ engine-kernel/src/main/scala/com/sos/scheduler/engine/eventbus/JavaEventSubscription.scala
@@ -0,0 +1,13 @@
+package com.sos.scheduler.engine.eventbus
+
+import com.sos.scheduler.engine.data.event.{AnyKeyedEvent, Event, KeyedEvent}
+import java.util.function.Consumer
+
+/**
+  * @author Joacim Zschimmer
+  */
+final class JavaEventSubscription[E <: Event](val eventClass: Class[E], handleEvent: Consumer[KeyedEvent[E]])
+extends EventSubscription {
+
+  def handleEvent(e: AnyKeyedEvent): Unit = handleEvent.accept(e.asInstanceOf[KeyedEvent[E]])
+}
---
+++ engine-kernel/src/test/java/com/sos/scheduler/engine/kernel/javatest/VariablesCustomEventJavaIT.java
@@ -0,0 +1,36 @@
+package com.sos.scheduler.engine.kernel.javatest;
+
+import com.sos.scheduler.engine.data.event.KeyedEvent;
+import com.sos.scheduler.engine.data.event.custom.VariablesCustomEvent;
+import com.sos.scheduler.engine.eventbus.JavaEventSubscription;
+import com.sos.scheduler.engine.eventbus.SchedulerEventBus;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Joacim Zschimmer
+ */
+public class VariablesCustomEventJavaIT {
+
+    @Test
+    public void test() throws ExecutionException, InterruptedException, TimeoutException {
+        SchedulerEventBus eventBus = new SchedulerEventBus();
+        CompletableFuture<KeyedEvent<VariablesCustomEvent>> future = new CompletableFuture<>();
+        eventBus.subscribeHot(new JavaEventSubscription<>(VariablesCustomEvent.class, future::complete));  // Not unsubscribed in this test.
+        KeyedEvent<VariablesCustomEvent> keyedEvent = newKeyedEvent();
+        eventBus.publishJava(keyedEvent);
+        assertEquals(future.get(0, TimeUnit.SECONDS), keyedEvent);
+    }
+
+    private KeyedEvent<VariablesCustomEvent> newKeyedEvent() {
+        Map<String,String> variables = new HashMap<>();
+        variables.put("test-key", "test-variable");
+        return VariablesCustomEvent.keyed("KEY", variables);
+    }
+}
