f08fc9c4924431ba031725328ae082cb4b83d043
Joacim Zschimmer <jogit@zschimmer.com>
2017-01-15 18:19:26 +0100

    mavenSettings.h

--- engine-kernelcpp/generated/mavenSettings.h
+++ engine-kernelcpp/generated/mavenSettings.h
@@ -12,8 +12,8 @@
 
 // Die numerische Dateiversion einer Windows-Datei (bei uns VER_PRODUCTVERSION) hat vier Zahlen
 
-#define VER_PRODUCTVERSION       1,11,0
-#define VER_PRODUCTVERSION_STR  "1.11.0-SNAPSHOT" VER_PRODUCTVERSION_TAIL
+#define VER_PRODUCTVERSION       1,12,0
+#define VER_PRODUCTVERSION_STR  "1.12.0-SNAPSHOT" VER_PRODUCTVERSION_TAIL
 
 #ifdef _DEBUG
 #   define VER_PRODUCTVERSION_DEBUG " (Debug)"
