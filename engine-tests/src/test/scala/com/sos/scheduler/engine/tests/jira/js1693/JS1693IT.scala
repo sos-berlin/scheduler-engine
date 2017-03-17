20194744cdc848749d00c1d295ff6a5b83d53e7b
Joacim Zschimmer <jogit@zschimmer.com>
2017-03-17 13:28:30 +0100

    JS-1693 <order><params value="$environmentvariable"> is substituted for permanent orders, too

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1693/JS1693IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1693/JS1693IT.scala
@@ -1,6 +1,6 @@
 package com.sos.scheduler.engine.tests.jira.js1693
 
-import com.sos.scheduler.engine.common.system.OperatingSystem.isWindows
+import com.sos.jobscheduler.common.system.OperatingSystem.isWindows
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
 import com.sos.scheduler.engine.test.SchedulerTestUtils._
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
