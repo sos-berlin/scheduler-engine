4e8c943983c02251526ded8c06063bb11dfe2449
Joacim Zschimmer <jogit@zschimmer.com>
2017-03-23 13:39:05 +0100

    JS-1693 Environment variables in order <params>: Test improved

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1693/JS1693IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1693/JS1693IT.scala
@@ -1,12 +1,17 @@
 package com.sos.scheduler.engine.tests.jira.js1693
 
-import com.sos.scheduler.engine.common.system.OperatingSystem.isWindows
+import com.sos.scheduler.engine.common.scalautil.Futures.implicits._
+import com.sos.scheduler.engine.data.event.KeyedEvent
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
+import com.sos.scheduler.engine.data.order.OrderFinished
+import com.sos.scheduler.engine.data.xmlcommands.OrderCommand
 import com.sos.scheduler.engine.test.SchedulerTestUtils._
 import com.sos.scheduler.engine.test.scalatest.ScalaSchedulerTest
+import com.sos.scheduler.engine.tests.jira.js1693.JS1693IT._
 import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.junit.JUnitRunner
+import scala.concurrent.Promise
 
 /**
   * @author Joacim Zschimmer
@@ -14,10 +19,31 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 final class JS1693IT extends FreeSpec with ScalaSchedulerTest {
 
-  if (false)  // For some reason, the environment variables PATH or Path are not available in every case.
-  "Environment variables in order variables" in {
-    val variables = runOrder(JobChainPath("/test") orderKey "1").variables
-    println(variables)
-    assert(variables("path") == "/" + sys.env.getOrElse("PATH", sys.env("Path")) + "/")
+  private val permanentOrderFinished = Promise[Map[String, String]]()
+
+  override protected def onBeforeSchedulerActivation() = {
+    eventBus.onHot[OrderFinished] {
+      case KeyedEvent(PermanentOrderKey, _: OrderFinished) ⇒
+        permanentOrderFinished.success(orderDetailed(PermanentOrderKey).variables)
+    }
+    super.onBeforeSchedulerActivation()
   }
+
+  "Environment variables in permanent order" in {
+    val variables = permanentOrderFinished.future await TestTimeout
+    orderLog(PermanentOrderKey)
+    val expected = sys.env.getOrElse("PATH", sys.env("Path"))
+    assert(variables("testpath") == s"/$expected/")
+    assert(variables("included") == s"|$expected|")
+  }
+
+  "Environment variables in ad-hoc order" in {
+    val variables = runOrder(OrderCommand(JobChainPath("/test") orderKey "Ad-hoc", parameters = Map("testpath" → "-$PATH-"))).variables
+    val expected = sys.env.getOrElse("PATH", sys.env("Path"))
+    assert(variables("testpath") == s"-$expected-")
+  }
+}
+
+private object JS1693IT {
+  private val PermanentOrderKey = JobChainPath("/test") orderKey "1"
 }
