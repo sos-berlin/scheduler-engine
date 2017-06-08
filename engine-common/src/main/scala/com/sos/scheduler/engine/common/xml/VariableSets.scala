205f6fa2f4111b6abe0b9e56b74fab46103a52b9
Joacim Zschimmer <jogit@zschimmer.com>
2017-06-08 10:00:56 +0200

    JS-1716 Scala parses and generates <variables> with hex_value correctly

--- engine-common/src/main/scala/com/sos/scheduler/engine/common/xml/VariableSets.scala
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/xml/VariableSets.scala
@@ -2,6 +2,7 @@ package com.sos.scheduler.engine.common.xml
 
 import com.sos.scheduler.engine.common.scalautil.xmls.ScalaXMLEventReader
 import javax.xml.stream.XMLEventReader
+import scala.collection.immutable.IndexedSeq
 import scala.xml.{TopScope, UnprefixedAttribute}
 
 /**
@@ -22,8 +23,13 @@ object VariableSets {
       xml.Elem(
         prefix = null,
         label = subelementName,
-        attributes = new UnprefixedAttribute("name", xml.Text(k), new UnprefixedAttribute("value", xml.Text(v), xml.Null)),
-        scope = TopScope,
+        attributes = new UnprefixedAttribute("name", xml.Text(k),
+          if (v exists { c ⇒ !hex.isLatin1(c) })
+            new UnprefixedAttribute("hex_value", xml.Text(hex.bytesToHex(k, v)), xml.Null)
+          else
+            new UnprefixedAttribute("value", xml.Text(v), xml.Null)
+        ),
+          scope = TopScope,
         minimizeEmpty = true)
     }
     xml.Elem(null: String, elementName, xml.Null, TopScope, children.isEmpty, children.toSeq: _*)
@@ -41,8 +47,56 @@ object VariableSets {
     parseElement(myGroupName) {
       attributeMap.ignore("count")
       parseEachRepeatingElement(elementName) {
-        attributeMap("name") → attributeMap.getOrElse("value", "")
+        attributeMap("name") →
+          (attributeMap.get("value") orElse (attributeMap.get("hex_value") map hex.hexToBytes) getOrElse "")
       }
     } .toMap
+  }
+
+  private object hex {
+    val isLatin1: IndexedSeq[Boolean] = Vector(
+    //  0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0,     // 00  nur \t \n \r
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     // 10
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // 20
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // 30
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // 40
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // 50
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // 60
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,     // 70  \x7f nicht
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     // 80
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     // 90
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // a0
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // b0
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // c0
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // d0
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,     // e0
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1      // f0
+    ) map { _ != 0 }
+
+    def bytesToHex(key: String, value: String): String =
+      (value map { c ⇒
+        val i = c.toInt
+        if (c < 0 || c >= 0x100) throw new IllegalArgumentException(f"Variable '$key' contains a non-8-bit character: '$c' U+$i%04X")
+        f"$i%02x"
+      }).mkString
+
+    def hexToInt(c: Char) =
+      if (c >= '0' && c <= '9')
+        c - '0'
+      else if (c >= 'a' && c <= 'f')
+        c - ('a' + 10 - 1)
+      else
+        throwInvalidHexValue()
+
+
+    def hexToBytes(value: String): String = {
+      if(value.length % 2 != 0) throwInvalidHexValue()
+      (for (i ← 0 until value.length by 2) yield
+        ((hexToInt(value(i)) << 4) | hexToInt(value(i + 1))).toChar
+      ).mkString
+    }
+
+    private def throwInvalidHexValue() = throw new IllegalArgumentException("""Invalid <variable "hex_value"=...>""")
   }
 }
--- engine-common/src/test/scala/com/sos/scheduler/engine/common/xml/VariableSetsTest.scala
+++ engine-common/src/test/scala/com/sos/scheduler/engine/common/xml/VariableSetsTest.scala
@@ -15,9 +15,9 @@ final class VariableSetsTest extends FreeSpec {
     val x =
       <anything>
         <variable name="A" value="a"/>
-        <variable name="B" value="b"/>
+        <variable name="B" hex_value="620180"/>
       </anything>
-    assert(parseXml(x.toString()) == Map("A" → "a", "B" → "b"))
+    assert(parseXml(x.toString()) == Map("A" → "a", "B" → "b\u0001\u0080"))
   }
 
   "toXmlElem" in {
@@ -27,12 +27,24 @@ final class VariableSetsTest extends FreeSpec {
   }
 
   "toXmlElem o parseXml" in {
-    val m = Map("A" → "B", "b" → "b")
-    assert(parseXml(toXmlElem(Map("A" → "B", "b" → "b")).toString()) == m)
+    val m = Map("A" → "AA", "b" → "b\u0001\u0080")
+    val x = toXmlElem(m)
+    assert(x == <sos.spooler.variable_set><variable name="A" value="AA"/><variable name="b" hex_value="620180"/></sos.spooler.variable_set>)
+    assert(parseXml(x.toString()) == m)
   }
 
   "toParamsXmlElem" in {
     assert(toParamsXmlElem(Map("A" → "a")) == <params><param name="A" value="a"/></params>)
     assert(toParamsXmlElem(Map()) == <params/>)
+  }
+
+  "Invalid hex_value is rejected" in {
+    parseXml(<anything><variable name="B" hex_value=""/></anything>.toString())
+    intercept[RuntimeException] {
+      parseXml(<anything><variable name="B" hex_value="1g"/></anything>.toString())
+    }
+    intercept[RuntimeException] {
+      parseXml(<anything><variable name="B" hex_value="1"/></anything>.toString())
+    }
   }
 }
