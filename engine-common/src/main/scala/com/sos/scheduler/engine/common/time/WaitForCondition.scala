1f739b8d95e2d24e4e9d8ca84c138f06192ebf91
Joacim Zschimmer <jogit@zschimmer.com>
2017-08-04 07:57:33 +0200

    WaitForCondition.retryUntil

--- engine-common/src/main/scala/com/sos/scheduler/engine/common/time/WaitForCondition.scala
+++ engine-common/src/main/scala/com/sos/scheduler/engine/common/time/WaitForCondition.scala
@@ -2,10 +2,27 @@ package com.sos.scheduler.engine.common.time
 
 import com.sos.scheduler.engine.common.time.ScalaTime._
 import java.lang.System.currentTimeMillis
-import java.time.Instant.now
 import java.time.Duration
+import java.time.Instant.now
+import scala.util.control.NonFatal
+import scala.util.{Success, Try}
 
 object WaitForCondition {
+
+  def retryUntil[A](timeout: Duration, step: Duration)(body: ⇒ A): A = {
+    val until = now + timeout
+    try body
+    catch { case NonFatal(t) ⇒
+      while (now < until) {
+        sleep(step)
+        Try { body } match {
+          case Success(result) ⇒ return result
+          case _ ⇒
+        }
+      }
+      throw t
+    }
+  }
 
   /** Wartet längstens t.timeout in Schritten von t.step, bis condition wahr wird.
     * condition wird bei t.timeout > 0 wenigsten zweimal aufgerufen: am Anfang und am Ende.
@@ -17,22 +34,20 @@ object WaitForCondition {
     * condition wird bei t.timeout > 0 wenigsten zweimal aufgerufen: am Anfang und am Ende.
     * @return letztes Ergebnis von condition */
   def waitForCondition(t: TimeoutWithSteps)(condition: ⇒ Boolean) =
-    waitAtInstantsFor(t.toMillisInstantIterator(now()))(condition)
+    waitAtInstantsFor(t.toMillisInstantIterator(now))(condition)
 
   /** Wartet bis zu den relative Zeitpunkten, bis condition wahr wird.
     * Die relative Zeitpunkt gelten ab jetzt (Instant.now).
     * condition wird am Anfang und am Ende geprüft.
     * @return letztes Ergebnis von condition */
-  def waitFromNowFor(relativeInstants: TraversableOnce[Long])(condition: ⇒ Boolean) = {
-    val nw = now()
-    waitAtInstantsFor(relativeInstants map nw.toEpochMilli.+)(condition)
-  }
+  def waitFromNowFor(relativeInstants: TraversableOnce[Long])(condition: ⇒ Boolean) =
+    waitAtInstantsFor(relativeInstants map now.toEpochMilli.+)(condition)
 
   /** Wartet bis zu den Zeitpunkten, bis condition wahr wird.
     * condition wird am Anfang und am Ende geprüft.
     * @return letztes Ergebnis von condition */
   def waitAtInstantsFor(instants: TraversableOnce[Long])(condition: ⇒ Boolean) =
-    realTimeIterator(instants) exists {_ ⇒ condition}
+    realTimeIterator(instants) exists { _ ⇒ condition }
 
   /** Ein Iterator, der bei next() (oder hasNext) auf den nächsten Zeitpunkt wartet.
     * Wenn aufeinanderfolgende Zeitpunkte schon erreicht sind, kehrt der Iterator trotzdem jedesmal zurück.
