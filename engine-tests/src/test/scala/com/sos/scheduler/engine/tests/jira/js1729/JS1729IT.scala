24d29b8e08564d66c4b3ee7dd7b0e59cc8b07936
Joacim Zschimmer <jogit@zschimmer.com>
2017-09-01 09:45:56 +0200

    JS-1729 Test adapted to v1.11

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1729/JS1729IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1729/JS1729IT.scala
@@ -13,7 +13,7 @@ final class JS1729IT extends FreeSpec with ScalaSchedulerTest {
 
   "<show_calendar> returns all spaces in order ID" in {
     val result = scheduler.executeXml(<show_calendar before="2031-01-01T00:00:00" what="orders"/>)
-    val orderId = (result.answer \ "calendar" \ "at" \ "@order").text
+    val orderId = (result.answer \ "calendar" \ "period" \ "@order").text
     assert(orderId == "two  spaces")
   }
 }
