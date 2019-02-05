2b5119903d16f999636bc7687e9883143a54edb5
Joacim Zschimmer <jogit@zschimmer.com>
2019-02-05 08:34:20 +0100

    engine-playground deleted

--- engine-playground/engine-playground-zschimmer/src/main/scala/com/sos/scheduler/engine/playground/zschimmer/IntervalTimer.scala
+++
@@ -1,9 +0,0 @@
-package com.sos.scheduler.engine.playground.zschimmer
-
-
-class IntervalTimer(override val intervalMs: Long, override val startTime: Long = System.currentTimeMillis)
-extends SimpleIntervalTimer(intervalMs, startTime) {
-    def number() = numberOfMs(System.currentTimeMillis)
-    def nextMs(): Long = nextMs(System.currentTimeMillis)
-    def msUntilNextInterval(): Long = msUntilNextInterval(System.currentTimeMillis)
-}
