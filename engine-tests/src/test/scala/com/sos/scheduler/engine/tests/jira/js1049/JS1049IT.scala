3fcb4bc3a0433ec59d463f8ddc863fd2378d2a64
Joacim Zschimmer <jogit@zschimmer.com>
2020-10-30 15:08:06 +0100

    JS-1901 Test: Umlauts in UTF-8 encoded permanent order XML

--- engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1049/JS1049IT.scala
+++ engine-tests/src/test/scala/com/sos/scheduler/engine/tests/jira/js1049/JS1049IT.scala
@@ -1,9 +1,10 @@
 package com.sos.scheduler.engine.tests.jira.js1049
 
 import com.sos.scheduler.engine.common.scalautil.FileUtils.implicits.RichFile
+import com.sos.scheduler.engine.common.scalautil.xmls.ScalaXmls.implicits.RichXmlFile
 import com.sos.scheduler.engine.data.job.{JobDescription, JobPath}
 import com.sos.scheduler.engine.data.jobchain.JobChainPath
-import com.sos.scheduler.engine.data.order.OrderFinished
+import com.sos.scheduler.engine.data.order.{OrderFinished, OrderId, OrderStepEnded, OrderSuspended}
 import com.sos.scheduler.engine.data.xmlcommands.OrderCommand
 import com.sos.scheduler.engine.kernel.scheduler.SchedulerConstants.schedulerEncoding
 import com.sos.scheduler.engine.kernel.variable.SchedulerVariableSet
@@ -17,6 +18,7 @@ import org.junit.runner.RunWith
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
 import org.scalatest.junit.JUnitRunner
+import com.sos.scheduler.engine.test.SchedulerTestUtils._
 
 @RunWith(classOf[JUnitRunner])
 final class JS1049IT extends FreeSpec with ScalaSchedulerTest {
@@ -68,10 +70,40 @@ final class JS1049IT extends FreeSpec with ScalaSchedulerTest {
       scheduler executeXml OrderCommand(XmlPayloadOrderKey)
     }
   }
+
+  "JS-1901 permanent order parameter with umlaut in XML" - {
+    "ISO-8859-1 (1)" in {
+      runOrderEncodingTest(1, OrderId("ISO-8859-1"))
+    }
+
+    "ISO-8859-1 (2)" in {
+      runOrderEncodingTest(2, OrderId("ISO-8859-1"))
+    }
+
+    "UTF-8 (1)" in {
+      runOrderEncodingTest(3, OrderId("UTF-8"))
+    }
+
+    "UTF-8 (2)" in {
+      runOrderEncodingTest(4, OrderId("UTF-8"))
+    }
+  }
+
+  private def runOrderEncodingTest(runNumber: Int, orderId: OrderId): Unit = {
+    val orderKey = JobChainPath("/test") orderKey orderId
+    eventBus.awaiting[OrderSuspended.type](orderKey) {
+      scheduler executeXml <modify_order job_chain="/test" order={orderKey.id.string} at="now"/>
+    }
+    assert(orderLog(orderKey) contains s"#$runNumber PARAMETER=TEST-${orderId.string}-ÄÖÜ")
+    eventBus.awaiting[OrderFinished](orderKey) {
+      scheduler executeXml <modify_order job_chain="/test" order={orderKey.id.string} suspended="false"/>
+    }
+  }
 }
 
 
-object JS1049IT {
+object JS1049IT
+{
   private case class Include(filename: String, encoding: Charset, content: String) {
     override def toString = s"$filename $encoding"
   }
